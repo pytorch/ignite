32d8cfbe4eaa2978e368bf4c89d73e2268e92858
Alykhan Tejani <alykhan.tejani@gmail.com>
2017-11-24 16:54:52 +0000

    remove avg loss logic from trainer + add history lists

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -50,36 +50,10 @@ def test_adding_handler_for_non_existent_event_throws_error():
     trainer.add_event_listener(event_name, lambda x: x)
 
 
-def test_update_function_returning_none_throws_error():
-  trainer = Trainer([1], MagicMock(return_value=None), MagicMock(), MagicMock())
-
-  with raises(ValueError):
-    trainer.run()
-
-
-def test_validation_function_returning_none_throws_error():
-  trainer = Trainer([1], MagicMock(return_value=1), [1], MagicMock(return_value=None))
-
-  with raises(ValueError):
-    trainer.run()
-
-
-def test_empty_trainingdata_iterable_throws_error():
-  trainer = Trainer(MagicMock(), MagicMock(return_value=1), MagicMock(), MagicMock())
-
-  with raises(ValueError):
-    trainer.run()
-
-
-def test_empty_validationdata_iterable_throws_error():
-  trainer = Trainer([1], MagicMock(return_value=1), MagicMock(), MagicMock())
-
-  with raises(ValueError):
-    trainer.run()
-
-
 def test_exception_handler_called_on_error():
-  trainer = Trainer([1], MagicMock(return_value=None), MagicMock(), MagicMock())
+  training_update_function = MagicMock(side_effect=ValueError())
+
+  trainer = Trainer([1], training_update_function, MagicMock(), MagicMock())
   exception_handler = MagicMock()
   trainer.add_event_listener(TrainingEvents.EXCEPTION_RAISED, exception_handler)
 
@@ -321,19 +295,20 @@ def test_terminate_after_training_iteration_skips_validation_run():
   assert trainer.validate.call_count == 0
 
 
-def _create_mock_data_loader_manager(epochs, batches_per_epoch):
+def _create_mock_data_loader(epochs, batches_per_epoch):
   batches = [MagicMock()] * batches_per_epoch
   data_loader_manager = MagicMock()
   batch_iterators = [iter(batches) for _ in range(epochs)]
 
   data_loader_manager.__iter__.side_effect = batch_iterators
+
   return data_loader_manager
 
 
 def test_training_iteration_events_are_fired():
   max_epochs = 5
   num_batches = 3
-  training_data = _create_mock_data_loader_manager(max_epochs, num_batches)
+  training_data = _create_mock_data_loader(max_epochs, num_batches)
 
   trainer = Trainer(training_data=training_data,
                     validation_data=MagicMock(),
@@ -366,7 +341,7 @@ def test_training_iteration_events_are_fired():
 def test_validation_iteration_events_are_fired():
   max_epochs = 5
   num_batches = 3
-  validation_data = _create_mock_data_loader_manager(max_epochs, num_batches)
+  validation_data = _create_mock_data_loader(max_epochs, num_batches)
 
   trainer = Trainer(training_data=[None],
                     validation_data=validation_data,
@@ -399,7 +374,7 @@ def test_validation_iteration_events_are_fired():
 def test_validation_iteration_events_are_fired_when_validate_is_called_explicitly():
   max_epochs = 5
   num_batches = 3
-  validation_data = _create_mock_data_loader_manager(max_epochs, num_batches)
+  validation_data = _create_mock_data_loader(max_epochs, num_batches)
 
   trainer = Trainer(training_data=[None],
                     validation_data=validation_data,
@@ -423,150 +398,3 @@ def test_validation_iteration_events_are_fired_when_validate_is_called_explicitl
 
   assert iteration_started.call_count == num_batches
   assert iteration_complete.call_count == num_batches
-
-
-@mark.parametrize("training_update_losses_per_batch, avg_loss_per_epoch, batches_per_epoch",
-                  [([1, 2, 3, 4], [np.array([1.5]), np.array([3.5])], 2),
-                   ([(1, 5), (2, 6), (3, 7), (4, 8)], [np.array([1.5, 5.5]), np.array([3.5, 7.5])],
-                    2)])
-def test_avg_training_loss_per_epoch_updates_correctly(training_update_losses_per_batch,
-                                                       avg_loss_per_epoch,
-                                                       batches_per_epoch):
-  max_epochs = len(training_update_losses_per_batch) // batches_per_epoch
-  training_data = _create_mock_data_loader_manager(max_epochs, batches_per_epoch)
-
-  trainer = Trainer(training_data=training_data,
-                    validation_data=MagicMock(),
-                    training_update_function=MagicMock(
-                        side_effect=training_update_losses_per_batch),
-                    validation_inference_function=MagicMock())
-
-  trainer.run(max_epochs=max_epochs, validate_every_epoch=False)
-
-  assert np.array_equal(trainer.avg_training_loss_per_epoch, avg_loss_per_epoch)
-  assert np.array_equal(trainer.best_training_loss, np.min(avg_loss_per_epoch, axis=0))
-
-
-@mark.parametrize("validation_update_losses_per_batch, avg_loss_per_epoch, batches_per_epoch",
-                  [([1, 2, 3, 4], [np.array([1.5]), np.array([3.5])], 2),
-                   ([(1, 2), (2, 1), (3, 4), (4, 5)], [(1.5, 1.5), (3.5, 4.5)], 2)])
-def test_avg_validation_loss_per_epoch_updates_correctly(validation_update_losses_per_batch,
-                                                         avg_loss_per_epoch,
-                                                         batches_per_epoch):
-  max_epochs = len(validation_update_losses_per_batch) // batches_per_epoch
-
-  validation_data_loader_manager = _create_mock_data_loader_manager(max_epochs, batches_per_epoch)
-
-  trainer = Trainer(training_data=[1],
-                    validation_data=validation_data_loader_manager,
-                    training_update_function=MagicMock(return_value=1),
-                    validation_inference_function=MagicMock(
-      side_effect=validation_update_losses_per_batch))
-
-  trainer.run(max_epochs=max_epochs)
-
-  assert np.array_equal(trainer.avg_validation_loss, avg_loss_per_epoch)
-  assert np.array_equal(trainer.best_validation_loss, min(avg_loss_per_epoch, key=lambda a: a[0]))
-
-
-@mark.parametrize(
-    "validation_update_losses_per_batch, batches_per_epoch, expected_number_of_updates",
-    [([7, 6, 6, 6, 6, 7, 5, 5], 2, 3),
-     ([(7, 2), (6, 1), (6, 1), (6, 0), (6, 0), (7, 1), (5, 0), (5, 0)], 2, 3),
-        (
-        [(7, 3, 2), (6, 2, 1), (6, 2, 1), (6, 2, 0), (6, 1, 0), (7, 0, 1), (5, 1, 0), (5, 0, 0)], 2,
-        3)])
-def test_best_loss_updates_if_validation_loss_reduces(validation_update_losses_per_batch,
-                                                      batches_per_epoch,
-                                                      expected_number_of_updates):
-  max_epochs = len(validation_update_losses_per_batch) // batches_per_epoch
-
-  validation_data_loader_manager = _create_mock_data_loader_manager(max_epochs, batches_per_epoch)
-
-  training_data = [1]
-  training_update_function = MagicMock()
-  training_data = _create_mock_data_loader_manager(max_epochs, batches_per_epoch)
-  training_update_function = MagicMock(side_effect=validation_update_losses_per_batch)
-
-  def check_loss_update(trainer):
-    best_validation_loss = trainer.best_validation_loss
-    last_validation_loss = trainer.avg_validation_loss[-1]
-    assert (best_validation_loss == last_validation_loss).all()
-
-  trainer = Trainer(training_data=training_data,
-                    validation_data=validation_data_loader_manager,
-                    training_update_function=training_update_function,
-                    validation_inference_function=MagicMock(
-                        side_effect=validation_update_losses_per_batch))
-
-  trainer.add_event_listener(TrainingEvents.BEST_LOSS_UPDATED, check_loss_update)
-
-  counter = MagicMock()
-  trainer.add_event_listener(TrainingEvents.BEST_LOSS_UPDATED, counter)
-
-  trainer.run(max_epochs=max_epochs)
-
-  assert counter.call_count == expected_number_of_updates
-
-
-@mark.parametrize("validate_every_epoch", [True, False])
-@mark.parametrize("training_update_loss_per_batch, batches_per_epoch, expected_number_of_updates",
-                  [([7, 6, 6, 6, 6, 7, 5, 5], 2, 3),
-                   ([(7, 2), (6, 1), (6, 1), (6, 0), (6, 0), (7, 1), (5, 0), (5, 0)], 2, 3),
-                   ([(7, 3, 2), (6, 2, 1), (6, 2, 1), (6, 2, 0), (6, 1, 0), (7, 0, 1), (5, 1, 0),
-                     (5, 0, 0)], 2, 3)])
-def test_best_loss_updates_if_no_validation_loss_and_training_loss_reduces(
-        training_update_loss_per_batch,
-        batches_per_epoch,
-        expected_number_of_updates,
-        validate_every_epoch):
-  max_epochs = len(training_update_loss_per_batch) // batches_per_epoch
-
-  training_data = _create_mock_data_loader_manager(max_epochs, batches_per_epoch)
-
-  def check_loss_update(trainer):
-    best_training_loss = trainer.best_training_loss
-    last_training_loss = trainer.avg_training_loss_per_epoch[-1]
-    assert (best_training_loss == last_training_loss).all()
-
-  trainer = Trainer(training_data=training_data,
-                    training_update_function=MagicMock(side_effect=training_update_loss_per_batch))
-
-  trainer.add_event_listener(TrainingEvents.BEST_LOSS_UPDATED, check_loss_update)
-
-  counter = MagicMock()
-  trainer.add_event_listener(TrainingEvents.BEST_LOSS_UPDATED, counter)
-
-  trainer.run(max_epochs=max_epochs, validate_every_epoch=validate_every_epoch)
-
-  assert counter.call_count == expected_number_of_updates
-
-
-@mark.parametrize("validate_every_epoch", [True, False])
-@mark.parametrize("num_training_batches", [1, 5])
-def test_best_loss_updates_if_validate_is_called_irregularly(num_training_batches,
-                                                             validate_every_epoch):
-  max_epochs = 10
-  num_validation_batches = 2
-
-  def update(*args):
-    return np.random.randn()
-
-  def validate(*args):
-    return np.random.randn()
-
-  def validate_or_not(trainer):
-    if trainer.current_iteration % 3 == 0:
-      trainer.validate()
-
-  trainer = Trainer(
-      training_data=[None] * num_training_batches,
-      training_update_function=update,
-      validation_data=[None] * num_validation_batches,
-      validation_inference_function=validate)
-
-  trainer.add_event_listener(TrainingEvents.TRAINING_ITERATION_COMPLETED, validate_or_not)
-
-  trainer.run(max_epochs=max_epochs, validate_every_epoch=validate_every_epoch)
-
-  assert np.isclose(np.min(trainer.avg_training_loss_per_epoch), trainer.best_training_loss)
