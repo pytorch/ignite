85d48d91b05e801943893480b42cb9519e1d3fbd
Jason Kriss <jasonkriss@gmail.com>
2017-12-28 14:32:38 -0800

    introduce Supervised wrapper

---
+++ ignite/wrappers/supervised.py
@@ -0,0 +1,58 @@
+from torch.autograd import Variable
+
+class Supervised(object):
+    """
+    Provides training and validation update functions for standard supervised models.
+
+    Args:
+        model (torch.nn.Module): the model to train
+        optimizer (torch.optim.Optimizer): the optimizer to use
+        loss_fn (torch.nn loss function): the loss function to use
+        cuda (bool, optional): whether or not to transfer batch to GPU (default: False)
+
+    """
+    def __init__(self, model, optimizer, loss_fn, cuda=False):
+        self._model = model
+        self._optimizer = optimizer
+        self._loss_fn = loss_fn
+        self._cuda = cuda
+
+    def update(self, batch):
+        """
+        Training update function.
+
+        Args:
+            batch (tuple): the model input and target
+
+        Returns:
+            float: the batch loss
+        """
+        self._model.train()
+        self._optimizer.zero_grad()
+        x, y = self._prepare_batch(batch)
+        y_pred = self._model(x)
+        loss = self._loss_fn(y_pred, y)
+        loss.backward()
+        self._optimizer.step()
+        return loss.data[0]
+
+    def predict(self, batch):
+        """
+        Validation update function.
+
+        Args:
+            batch (tuple): the model input and target
+
+        Returns:
+            tuple: the prediction and the target
+        """
+        self._model.eval()
+        x, y = self._prepare_batch(batch)
+        y_pred = self._model(x)
+        return y_pred, y
+
+    def _prepare_batch(self, batch):
+        x, y = batch
+        if self._cuda:
+            x, y = x.cuda(), y.cuda()
+        return Variable(x), Variable(y)
---
+++ tests/ignite/wrappers/test_supervised.py
@@ -0,0 +1,47 @@
+import torch
+from torch.nn import Linear
+from torch.optim import SGD
+from torch.autograd import Variable
+from torch.nn.functional import mse_loss
+
+from ignite.wrappers import Supervised
+
+def _build_batch():
+    x = torch.FloatTensor([[1.0], [2.0]])
+    y = torch.FloatTensor([[3.0], [5.0]])
+    return x, y
+
+def _assert_almost_equal(a, b):
+    assert round(a - b, 5) == 0
+
+def test_update():
+    model = Linear(1, 1)
+    model.weight.data.zero_()
+    model.bias.data.zero_()
+    optimizer = SGD(model.parameters(), 0.1)
+    wrapper = Supervised(model, optimizer, mse_loss)
+
+    _assert_almost_equal(model.weight.data[0, 0], 0.0)
+    _assert_almost_equal(model.bias.data[0], 0.0)
+
+    batch = _build_batch()
+    loss = wrapper.update(batch)
+
+    _assert_almost_equal(loss, 17.0)
+    _assert_almost_equal(model.weight.data[0, 0], 1.3)
+    _assert_almost_equal(model.bias.data[0], 0.8)
+
+def test_predict():
+    model = Linear(1, 1)
+    model.weight.data.zero_()
+    model.bias.data.zero_()
+    optimizer = SGD(model.parameters(), 0.1)
+    wrapper = Supervised(model, optimizer, mse_loss)
+
+    batch = _build_batch()
+    y_pred, y = wrapper.predict(batch)
+
+    _assert_almost_equal(y_pred.data[0, 0], 0.0)
+    _assert_almost_equal(y_pred.data[1, 0], 0.0)
+    _assert_almost_equal(y.data[0, 0], 3.0)
+    _assert_almost_equal(y.data[1, 0], 5.0)
