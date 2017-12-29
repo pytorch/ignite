fbf3db73c648e078a85af12035cc2004ee9f7529
Jason Kriss <jasonkriss@gmail.com>
2017-12-28 20:33:39 -0800

    use pytest.approx for comparing floats

--- tests/ignite/wrappers/test_supervised.py
+++ tests/ignite/wrappers/test_supervised.py
@@ -3,6 +3,7 @@ from torch.nn import Linear
 from torch.optim import SGD
 from torch.autograd import Variable
 from torch.nn.functional import mse_loss
+from pytest import approx
 
 from ignite.wrappers import Supervised
 
@@ -13,10 +14,6 @@ def _build_batch():
     return x, y
 
 
-def _assert_almost_equal(a, b):
-    assert round(a - b, 5) == 0
-
-
 def test_update():
     model = Linear(1, 1)
     model.weight.data.zero_()
@@ -24,15 +21,15 @@ def test_update():
     optimizer = SGD(model.parameters(), 0.1)
     wrapper = Supervised(model, optimizer, mse_loss)
 
-    _assert_almost_equal(model.weight.data[0, 0], 0.0)
-    _assert_almost_equal(model.bias.data[0], 0.0)
+    assert model.weight.data[0, 0] == approx(0.0)
+    assert model.bias.data[0] == approx(0.0)
 
     batch = _build_batch()
     loss = wrapper.update(batch)
 
-    _assert_almost_equal(loss, 17.0)
-    _assert_almost_equal(model.weight.data[0, 0], 1.3)
-    _assert_almost_equal(model.bias.data[0], 0.8)
+    assert loss == approx(17.0)
+    assert model.weight.data[0, 0] == approx(1.3)
+    assert model.bias.data[0] == approx(0.8)
 
 
 def test_predict():
@@ -45,7 +42,7 @@ def test_predict():
     batch = _build_batch()
     y_pred, y = wrapper.predict(batch)
 
-    _assert_almost_equal(y_pred[0, 0], 0.0)
-    _assert_almost_equal(y_pred[1, 0], 0.0)
-    _assert_almost_equal(y[0, 0], 3.0)
-    _assert_almost_equal(y[1, 0], 5.0)
+    assert y_pred[0, 0] == approx(0.0)
+    assert y_pred[1, 0] == approx(0.0)
+    assert y[0, 0] == approx(3.0)
+    assert y[1, 0] == approx(5.0)
