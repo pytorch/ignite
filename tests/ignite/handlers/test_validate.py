06153b81ca99c15945b17f7b516cc831c5971339
Jason Kriss <jasonkriss@gmail.com>
2018-01-04 09:10:53 -0800

    remove extraneous test code

--- tests/ignite/handlers/test_validate.py
+++ tests/ignite/handlers/test_validate.py
@@ -4,9 +4,6 @@ from ignite.handlers import Validate
 
 
 def test_validate_init():
-    Validate([], iteration_interval=1)
-    Validate([], epoch_interval=1)
-
     with raises(ValueError):
         Validate([], iteration_interval=1, epoch_interval=1)
 
