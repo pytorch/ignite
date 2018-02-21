162d628b637574fe9498034791e43e94ba07898d
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2018-02-21 16:59:47 +0100

    add timer tests

---
+++ tests/ignite/handlers/test_timing.py
@@ -0,0 +1,50 @@
+import time
+
+from ignite.engine import Events
+from ignite.evaluator import Evaluator
+from ignite.handlers import Timer
+from ignite.trainer import Trainer
+
+
+def test_timer():
+    sleep_t = 0.2
+    n_iter = 3
+
+    def _train_func(batch):
+        time.sleep(0.2)
+
+    def _test_func(batch):
+        time.sleep(0.2)
+
+    trainer = Trainer(_train_func)
+    tester = Evaluator(_test_func)
+
+    t_total = Timer()
+    t_batch = Timer(average=True)
+    t_train = Timer()
+
+    t_total.attach(trainer)
+    t_batch.attach(trainer,
+                   pause=Events.ITERATION_COMPLETED,
+                   resume=Events.ITERATION_STARTED,
+                   step=Events.ITERATION_COMPLETED)
+    t_train.attach(trainer,
+                   pause=Events.EPOCH_COMPLETED,
+                   resume=Events.EPOCH_STARTED)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def run_validation(trainer):
+        tester.run(range(n_iter))
+
+    # Run "training"
+    trainer.run(range(n_iter))
+
+    def _equal(lhs, rhs):
+        return round(lhs, 1) == round(rhs, 1)
+
+    assert _equal(t_total.value(), (2 * n_iter * sleep_t))
+    assert _equal(t_batch.value(), (sleep_t))
+    assert _equal(t_train.value(), (n_iter * sleep_t))
+
+    t_total.reset()
+    assert _equal(t_total.value(), 0.0)
