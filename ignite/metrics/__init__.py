ec83636d68462d1f8d9bd3c330f63ff8045b9973
Jason Kriss <jasonkriss@gmail.com>
2018-02-26 09:11:01 -0800

    add ignite.metrics

---
+++ ignite/metrics/__init__.py
@@ -0,0 +1,1 @@
+from metric import Metric
---
+++ ignite/metrics/metric.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
+from ignite.engine import Events
+
+
+class Metric(metaclass=ABCMeta):
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def update(self, output):
+        pass
+
+    @abstractmethod
+    def compute(self):
+        pass
+
+    def started(self, state):
+        self.reset()
+
+    def iteration_completed(self, state):
+        self.update(state.output)
+
+    def completed(self, state):
+        state.metrics.append(self.compute())
+
+    def attach(self, engine):
+        engine.add_event_handler(Events.STARTED, self.started)
+        engine.add_event_handler(Events.ITERATION_COMPLETED, self.iteration_completed)
+        engine.add_event_handler(Events.COMPLETED, self.completed)
