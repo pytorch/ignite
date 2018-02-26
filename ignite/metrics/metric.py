25c3507cb6d78bcf3c7de648c789d2845a846452
Jason Kriss <jasonkriss@gmail.com>
2018-02-26 09:20:01 -0800

    add docs for Metric

--- ignite/metrics/metric.py
+++ ignite/metrics/metric.py
@@ -4,19 +4,49 @@ from ignite.engine import Events
 
 
 class Metric(metaclass=ABCMeta):
+    """
+    Base class for all Metrics.
+
+    Metrics provide a way to compute various quantities of interest in an online
+    fashion without having to store the entire output history of a model.
+    """
     def __init__(self):
         self.reset()
 
     @abstractmethod
     def reset(self):
+        """
+        Resets the metric to to it's initial state.
+
+        This is called at the start of each evaluation run.
+        """
         pass
 
     @abstractmethod
     def update(self, output):
+        """
+        Updates the metric's state using the passed batch output.
+
+        This is called once for each batch of each evaluation run.
+
+        Args:
+            output: the is the output from the evaluator's process function
+        """
         pass
 
     @abstractmethod
     def compute(self):
+        """
+        Computes the metric based on it's accumulated state.
+
+        This is called at the end of each evaluation run.
+
+        Returns:
+            Any: the actual quantity of interest
+
+        Raises:
+            NotComputableError: raised when the metric cannot be computed
+        """
         pass
 
     def started(self, state):
