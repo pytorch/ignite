0f96e0b6a2884f576c0e0a3a284a1831a8104d59
Jason Kriss <jasonkriss@gmail.com>
2018-03-06 08:46:39 -0800

    add MeanAbsoluteError metric

--- ignite/metrics/__init__.py
+++ ignite/metrics/__init__.py
@@ -1,4 +1,5 @@
 from .binary_accuracy import BinaryAccuracy
 from .categorical_accuracy import CategoricalAccuracy
+from .mean_absolute_error import MeanAbsoluteError
 from .mean_squared_error import MeanSquaredError
 from .metric import Metric
