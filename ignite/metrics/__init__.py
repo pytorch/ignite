dc89a8bd1b22a343d279703ab4fe68752ba208d0
Jason Kriss <jasonkriss@gmail.com>
2018-03-07 10:14:24 -0800

    add MeanPairwiseDistance metric

--- ignite/metrics/__init__.py
+++ ignite/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .binary_accuracy import BinaryAccuracy
 from .categorical_accuracy import CategoricalAccuracy
 from .mean_absolute_error import MeanAbsoluteError
+from .mean_pairwise_distance import MeanPairwiseDistance
 from .mean_squared_error import MeanSquaredError
 from .metric import Metric
 from .root_mean_squared_error import RootMeanSquaredError
---
+++ ignite/metrics/mean_pairwise_distance.py
@@ -0,0 +1,34 @@
+from __future__ import division
+
+import torch
+from torch.nn.functional import pairwise_distance
+
+from .metric import Metric
+from ignite.exceptions import NotComputableError
+
+
+class MeanPairwiseDistance(Metric):
+    """
+    Calculates the mean pairwise distance.
+
+    `update` must receive output of the form (y_pred, y).
+    """
+    def __init__(self, p=2, eps=1e-6):
+        super(MeanPairwiseDistance, self).__init__()
+        self._p = p
+        self._eps = eps
+
+    def reset(self):
+        self._sum_of_distances = 0.0
+        self._num_examples = 0
+
+    def update(self, output):
+        y_pred, y = output
+        distances = pairwise_distance(y_pred, y, p=self._p, eps=self._eps)
+        self._sum_of_distances += torch.sum(distances)
+        self._num_examples += y.shape[0]
+
+    def compute(self):
+        if self._num_examples == 0:
+            raise NotComputableError('MeanAbsoluteError must have at least one example before it can be computed')
+        return self._sum_of_distances / self._num_examples
