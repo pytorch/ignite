dc6c95939853cfc8b428cd05f909e914568e499d
Jason Kriss <jasonkriss@gmail.com>
2018-03-15 11:13:15 -0700

    mark variables as volatile in Loss metric

--- ignite/metrics/loss.py
+++ ignite/metrics/loss.py
@@ -22,8 +22,8 @@ class Loss(Metric):
 
     def update(self, output):
         y_pred, y = output
-        loss_sum = self._loss_fn(Variable(y_pred), Variable(y), size_average=False).data[0]
-        self._sum += loss_sum
+        loss_sum = self._loss_fn(Variable(y_pred, volatile=True), Variable(y, volatile=True), size_average=False)
+        self._sum += loss_sum.data[0]
         self._num_examples += y.shape[0]
 
     def compute(self):
