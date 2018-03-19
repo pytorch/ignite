6889c9abfd748d8af9f3619e6d0f0222e477d980
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2018-03-19 16:10:35 -0400

    Update functions in Engine now receive (engine, batch), not just batch.

--- ignite/engines/engine.py
+++ ignite/engines/engine.py
@@ -120,7 +120,7 @@ class Engine(object):
                 self.state.batch = batch
                 self.state.iteration += 1
                 self._fire_event(Events.ITERATION_STARTED)
-                self.state.output = self._process_function(batch)
+                self.state.output = self._process_function(self, batch)
                 self._fire_event(Events.ITERATION_COMPLETED)
                 if self.should_terminate:
                     break
