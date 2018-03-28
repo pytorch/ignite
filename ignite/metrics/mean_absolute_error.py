50d6151f3465ad8d379ae001ebd16dd41955fa4b
Takeshi Kanmae <tkanmae@gmail.com>
2018-03-28 22:03:35 +0900

    Use absolute imports throughout (#137)

--- ignite/metrics/mean_absolute_error.py
+++ ignite/metrics/mean_absolute_error.py
@@ -2,8 +2,8 @@ from __future__ import division
 
 import torch
 
-from .metric import Metric
 from ignite.exceptions import NotComputableError
+from ignite.metrics.metric import Metric
 
 
 class MeanAbsoluteError(Metric):
