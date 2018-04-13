Precision/Recall Fixes (#140)
* use absolute imports

* guard against all incorrect predictions

* uncomment asserts

* warn and set precision/recall to 0.0 when a label has no examples

* add ability to reduce/average precision/recall across classes

* remove warning in Recall and Precision

* use torch.zeros_like to ensure Precision/Recall work on GPU

tests/ignite/metrics/test_recall.py
@@ -1,4 +1,3 @@
-from math import isnan
 from ignite.exceptions import NotComputableError
 from ignite.metrics import Recall
 import pytest
@@ -17,11 +16,13 @@ def test_compute():
     y_pred = torch.eye(4)
     y = torch.ones(4).type(torch.LongTensor)
     recall.update((y_pred, y))
+
     result = list(recall.compute())
-    assert isnan(result[0])
+
+    assert result[0] == 0.0
     assert result[1] == 0.25
-    assert isnan(result[2])
-    assert isnan(result[3])
+    assert result[2] == 0.0
+    assert result[3] == 0.0
 
     recall.reset()
     y_pred = torch.eye(2)
@@ -29,6 +30,31 @@ def test_compute():
     recall.update((y_pred, y))
     y = torch.zeros(2).type(torch.LongTensor)
     recall.update((y_pred, y))
+
     result = list(recall.compute())
+
     assert result[0] == 0.5
     assert result[1] == 0.5
+
+
+def test_compute_average():
+    recall = Recall(average=True)
+
+    y_pred = torch.eye(4)
+    y = torch.ones(4).type(torch.LongTensor)
+    recall.update((y_pred, y))
+
+    assert recall.compute() == 0.0625
+
+
+def test_compute_all_wrong():
+    recall = Recall()
+
+    y_pred = torch.FloatTensor([[1.0, 0.0], [1.0, 0.0]])
+    y = torch.ones(2).type(torch.LongTensor)
+    recall.update((y_pred, y))
+
+    result = list(recall.compute())
+
+    assert result[0] == 0.0
+    assert result[1] == 0.0