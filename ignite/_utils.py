2e8157f5ab533ea3c3fe98d9ee911da60489f698
Sean Adler <sean@tempoautomation.com>
2018-04-30 18:37:23 -0700

    Allow non-tensor arguments in 'convert_tensor' helper

--- ignite/_utils.py
+++ ignite/_utils.py
@@ -11,12 +11,22 @@ def _to_hours_mins_secs(time_taken):
     return hours, mins, secs
 
 
-def convert_tensor(tensor, requires_grad=False, device=None):
-    if device:
-        tensor = tensor.to(device=device)
-    if requires_grad != tensor.requires_grad:
-        tensor.requires_grad_(requires_grad)
-    return tensor
+def convert_tensor(input_, requires_grad=False, device=None):
+    if torch.is_tensor(input_):
+        if device:
+            input_ = input_.to(device=device)
+        if requires_grad != input_.requires_grad:
+            input_.requires_grad_(requires_grad)
+        return input_
+    elif isinstance(input_, string_classes):
+        return input_
+    elif isinstance(input_, collections.Mapping):
+        return {k: convert_tensor(sample, requires_grad=requires_grad, device=device) for k, sample in input_.items()}
+    elif isinstance(input_, collections.Sequence):
+        return [convert_tensor(sample, requires_grad=requires_grad, device=device) for sample in input_]
+    else:
+        raise TypeError(("input must contain tensors, numbers, dicts or lists; found {}"
+                         .format(type(input_[0]))))
 
 
 def to_onehot(indices, num_classes):
--- tests/ignite/test__utils.py
+++ tests/ignite/test__utils.py
@@ -5,6 +5,27 @@ from ignite._utils import convert_tensor, to_onehot
 
 
 def test_convert_tensor():
+    x = torch.Tensor([0.0])
+    tensor = convert_tensor(x)
+    assert torch.is_tensor(tensor)
+
+    x = (torch.Tensor([0.0]), torch.Tensor([0.0]))
+    list_ = convert_tensor(x)
+    assert isinstance(list_, list)
+    assert torch.is_tensor(list_[0])
+    assert torch.is_tensor(list_[1])
+
+    x = {'a': torch.Tensor([0.0]), 'b': torch.Tensor([0.0])}
+    dict_ = convert_tensor(x)
+    assert isinstance(dict_, dict)
+    assert torch.is_tensor(dict_['a'])
+    assert torch.is_tensor(dict_['b'])
+
+    assert convert_tensor('a') == 'a'
+
+    with pytest.raises(TypeError):
+        convert_tensor(12345)
+
     tensor = torch.Tensor([1])
     assert not tensor.requires_grad
     tensor = convert_tensor(tensor, requires_grad=True)
