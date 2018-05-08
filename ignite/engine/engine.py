67cd028bc1e74605a2d6515285562ea19da6bc9f
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2018-05-08 17:54:27 +0200

    revert exception handling fix

--- ignite/engine/engine.py
+++ ignite/engine/engine.py
@@ -161,9 +161,8 @@ class Engine(object):
         self.should_terminate = True
 
     def _run_once_on_dataset(self):
-        start_time = time.time()
-
         try:
+            start_time = time.time()
             for batch in self.state.dataloader:
                 self.state.batch = batch
                 self.state.iteration += 1
@@ -173,14 +172,12 @@ class Engine(object):
                 if self.should_terminate:
                     break
 
-        except BaseException as e:
-            self._logger.error("Current run is terminating due to exception: %s", str(e))
-            self._handle_exception(e)
-
-        finally:
             time_taken = time.time() - start_time
             hours, mins, secs = _to_hours_mins_secs(time_taken)
             return hours, mins, secs
+        except BaseException as e:
+            self._logger.error("Current run is terminating due to exception: %s", str(e))
+            self._handle_exception(e)
 
     def _handle_exception(self, e):
         if Events.EXCEPTION_RAISED in self._event_handlers:
