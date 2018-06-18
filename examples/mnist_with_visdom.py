27b8d9a682f952b9e8bd79fb041800e365398666
vfdev-5 <vfdev.5@gmail.com>
2018-06-18 22:57:46 +0200

    Update examples

--- examples/mnist_with_visdom.py
+++ examples/mnist_with_visdom.py
@@ -63,7 +63,6 @@ def run(train_batch_size, val_batch_size, epochs, lr, momentum, log_interval):
 
     if torch.cuda.is_available():
         device = 'cuda'
-        model = model.to(device)
 
     optimizer = SGD(model.parameters(), lr=lr, momentum=momentum)
     trainer = create_supervised_trainer(model, optimizer, F.nll_loss, device=device)
