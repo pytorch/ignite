c607abea84e42944ffc059312131963dcb08e2db
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-07-28 14:26:10 -0400

    Add add_event and tbptt trainer to contrib.

---
+++ ignite/contrib/engine.py
@@ -0,0 +1,110 @@
+# coding: utf-8
+
+from enum import Enum, auto
+import collections
+
+from ignite.contrib.events import add_events
+from ignite._utils import convert_tensor
+import ignite.engine
+
+
+class Tbptt_Events(Enum):
+    """Aditional tbptt events.
+
+    Additional events for truncated backpropagation throught time dedicated
+    trainer.
+    """
+
+    TIME_ITERATION_STARTED = auto()
+    TIME_ITERATION_COMPLETED = auto()
+
+
+def _prepare_tbptt_batch(batch, tbptt_step, dim=0, device=None):
+    """Prepare batch for tbptt trainer.
+
+    Batch come from the dataloader. It is split in chunks along the time
+    dimension and fed to the truncated backpropagation throught time trainer.
+    """
+    x, y = batch
+    x = convert_tensor(x, device=device)
+    y = convert_tensor(y, device=device)
+    return zip(x.split(tbptt_step, dim=dim), y.split(tbptt_step, dim=dim))
+
+
+def _detach_hidden(hidden):
+    """Cut backpropagation graph.
+
+    Auxillary function to cut the backpropagation graph by detaching the hidden
+    vector.
+    """
+    if isinstance(hidden, collections.Iterable):
+        return tuple(h.detach() for h in hidden)
+    else:
+        return hidden.detach()
+
+
+def create_supervised_tbptt_trainer(
+    model,
+    optimizer,
+    loss_fn,
+    tbtt_step,
+    dim=0,
+    device=None
+):
+    """
+    Factory function for creating a trainer for truncated backpropagation
+    through time supervised models.
+
+    Args:
+        model (`torch.nn.Module`): the model to train
+        optimizer (`torch.optim.Optimizer`): the optimizer to use
+        loss_fn (torch.nn loss function): the loss function to use
+        tbtt_step (int): the length of time chunks (last one may be smaller)
+        dim (int): axis representing the time dimension
+        device (str, optional): device type specification (default: None).
+            Applies to both model and batches.
+
+    Returns:
+        Engine: a trainer engine with supervised update function
+    """
+    # Add necessary events to the Engine core
+    add_events(Tbptt_Events)
+
+    if device:
+        model.to(device)
+
+    def _update(engine, batch):
+        loss_list = []
+        hidden = None
+
+        # Batches split in time chunks
+        batch_splits = _prepare_tbptt_batch(
+            batch, tbtt_step, dim=dim, device=device
+        )
+        for x_t, y_t in batch_splits:
+            # Fire event for start of iteration
+            engine._fire_event(ignite.engine.Events.TIME_ITERATION_STARTED)
+
+            # Forward, backward and
+            model.train()
+            optimizer.zero_grad()
+            if hidden is None:
+                y_pred_t, hidden = model(x_t)
+            else:
+                hidden = _detach_hidden(hidden)
+                y_pred_t, hidden = model(x_t)
+            loss_t = loss_fn(y_pred_t, y_t)
+            loss_t.backward()
+            optimizer.step()
+
+            # Setting state of engine for consistent behaviour
+            engine.state.output = loss_t.item()
+            loss_list.append(loss_t.item())
+
+            # Fire event for end of iteration
+            engine._fire_event(ignite.engine.Events.TIME_ITERATION_STARTED)
+
+            # return average loss over the time splits
+            return sum(loss_list) / len(loss_list)
+
+    return ignite.engine.Engine(_update)
