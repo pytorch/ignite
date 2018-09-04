ec158f8f4b158e42159a659435033431ccdbf6cd
miguel <miguel.varelaramos@braincreators.com>
2018-09-04 15:48:28 +0200

    added progress bar unit test

---
+++ tests/ignite/contrib/handlers/test_pbar.py
@@ -0,0 +1,22 @@
+from ignite.engine import Engine, Events
+from ignite.contrib.handlers import ProgressBar
+
+
+def test_with_engine(capsys):
+
+    def update_fn(engine, batch):
+        return {'a': 1, 'b': 2}
+
+    n_epochs = 2
+    loader = [1, 2]
+    engine = Engine(update_fn)
+    handler = ProgressBar(engine, loader)
+
+    engine.add_event_handler(Events.ITERATION_COMPLETED, handler)
+    engine.run(loader, max_epochs=n_epochs)
+
+    captured = capsys.readouterr()
+    out = str(captured.out).split('\n')
+    expected = 'Epoch {} | a={:.2e} | b={:.2e}'.format(engine.state.epoch, 1, 2)
+    assert len(out) == n_epochs + 1
+    assert out[-2] == expected
