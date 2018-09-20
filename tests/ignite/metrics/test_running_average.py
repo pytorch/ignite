ab6d26b0840244de95ecc9b140e4b868a8d2afd9
vfdev-5 <vfdev.5@gmail.com>
2018-09-20 23:17:40 +0200

    Move RunningAverage to metrics

--- tests/ignite/metrics/test_running_average.py
+++ tests/ignite/metrics/test_running_average.py
@@ -3,21 +3,23 @@ import numpy as np
 import torch
 
 from ignite.engine import Engine, Events
-from ignite.metrics import CategoricalAccuracy
-from ignite.handlers import RunningAverage
+from ignite.metrics import CategoricalAccuracy, RunningAverage
 
 import pytest
 
 
 def test_wrong_input_args():
     with pytest.raises(TypeError):
-        _ = RunningAverage([12, 34])
+        _ = RunningAverage(src=[12, 34])
 
     with pytest.raises(ValueError):
-        _ = RunningAverage("output", alpha=-1.0)
+        _ = RunningAverage(alpha=-1.0)
 
     with pytest.raises(ValueError):
-        _ = RunningAverage(CategoricalAccuracy(), output_transform=lambda x: x ** 2)
+        _ = RunningAverage(CategoricalAccuracy(), output_transform=lambda x: x[0])
+
+    with pytest.raises(ValueError):
+        _ = RunningAverage()
 
 
 def test_integration():
@@ -41,19 +43,19 @@ def test_integration():
     acc_metric = RunningAverage(CategoricalAccuracy(output_transform=lambda x: [x[1], x[2]]), alpha=alpha)
     acc_metric.attach(trainer, 'running_avg_accuracy')
 
-    avg_output = RunningAverage('output', output_transform=lambda x: x[0], alpha=alpha)
+    avg_output = RunningAverage(output_transform=lambda x: x[0], alpha=alpha)
     avg_output.attach(trainer, 'running_avg_output')
 
     running_avg_acc = [None]
 
-    @trainer.on(Events.ITERATION_COMPLETED)
-    def manual_running_avg_acc(engine):
+    @trainer.on(Events.ITERATION_COMPLETED, running_avg_acc)
+    def manual_running_avg_acc(engine, running_avg_acc):
         _, y_pred, y = engine.state.output
         indices = torch.max(y_pred, 1)[1]
         correct = torch.eq(indices, y).view(-1)
         num_correct = torch.sum(correct).item()
         num_examples = correct.shape[0]
-        batch_acc = num_correct / num_examples
+        batch_acc = num_correct * 1.0 / num_examples
         if running_avg_acc[0] is None:
             running_avg_acc[0] = batch_acc
         else:
@@ -83,7 +85,7 @@ def test_integration():
             "{} vs {}".format(engine.state.running_avg_output, engine.state.metrics['running_avg_output'])
 
     np.random.seed(10)
-    running_avg_acc = [None]
+    running_avg_acc[0] = None
     n_iters = 10
     batch_size = 10
     n_classes = 10
@@ -93,7 +95,7 @@ def test_integration():
     y_pred_batch_values = iter(np.random.rand(n_iters, batch_size, n_classes))
     trainer.run(data, max_epochs=1)
 
-    running_avg_acc = [None]
+    running_avg_acc[0] = None
     n_iters = 10
     batch_size = 10
     n_classes = 10
