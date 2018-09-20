5cadeb1b15589b201c5758f1ae868f448cb5e721
Anmol Joshi <anmolsjoshi@gmail.com>
2018-09-19 19:41:18 -0700

    added write_image option to convert parameters to images

--- ignite/contrib/handlers/tensorboardX.py
+++ ignite/contrib/handlers/tensorboardX.py
@@ -1,5 +1,7 @@
 from types import MethodType
 import torch
+from torchvision import utils
+import numpy as np
 from ignite.engine import Engine, Events
 from tensorboardX import SummaryWriter
 
@@ -21,6 +23,7 @@ class TensorBoardX(object):
             write_graph (`bool`): True, if graph should be recorded
             histogram_freq (`int`): frequency of epoch to record histogram of weights and gradients (if applicable)
             write_grads (`bool`): True, if gradients to model weights should be recorded
+            write_images (`bool`): True, if model weights should be written as images
 
         Returns:
             SummaryWriter: a TensorBoard summary report
@@ -37,7 +40,8 @@ class TensorBoardX(object):
                  validation_evaluator=None,
                  write_graph=False,
                  histogram_freq=0,
-                 write_grads=False):
+                 write_grads=False,
+                 write_images=False):
 
         self.log_dir = log_dir
         self.model = model
@@ -52,6 +56,7 @@ class TensorBoardX(object):
         self.write_graph = write_graph
         self.histogram_freq = histogram_freq
         self.write_grads = write_grads
+        self.write_images = write_images
 
     def _on_start(self, engine):
         """
@@ -120,6 +125,7 @@ class TensorBoardX(object):
     def _log_histograms(self, engine, model, add_name=False):
         if engine.state.epoch % self.histogram_freq == 0:
             for name, param in model.named_parameters():
+                name = name.replace('.', '/')
                 if add_name:
                     name = ''.join([model.__class__.__name__, '/', name])
 
@@ -132,10 +138,58 @@ class TensorBoardX(object):
                                                   param.grad.cpu().data.numpy().flatten(),
                                                   engine.state.epoch)
 
+    def _log_weight_image(self, engine, model):
+
+        if engine.state.epoch % self.histogram_freq == 0:
+
+            prefix = model.__class__.__name__ + '/'
+
+            for name, param in model.named_parameters():
+
+                name = ''.join([prefix, name.replace('.', '/')])
+
+                w_img = param.data
+                shape = list(w_img.size())
+
+                if len(shape) == 1:
+                    w_img = w_img.view(shape[0], 1, 1, 1)
+                    grid = self._vistensor(w_img,
+                                           ch=0,
+                                           allkernels=True,
+                                           padding=0,
+                                           nrow=int(shape[0]/16) + 1)
+                elif len(shape) == 2:
+                    if shape[0] > shape[1]:
+                        w_img = torch.transpose(w_img, 0, 1)
+                        shape = list(w_img.size())
+                    w_img = w_img.view(shape[0] * shape[1], 1, 1, 1)
+                    grid = self._vistensor(w_img,
+                                           ch=0,
+                                           allkernels=True,
+                                           nrow=shape[1],
+                                           padding=0)
+                elif len(shape) == 3:
+                    w_img = w_img.view(shape[0], 1, shape[1], shape[2])
+                    grid = self._vistensor(w_img,
+                                           ch=0,
+                                           allkernels=True,
+                                           nrow=int(shape[0]/4) + 1,
+                                           padding=1)
+                elif len(shape) == 4:
+                    grid = self._vistensor(w_img,
+                                           ch=0,
+                                           allkernels=True,
+                                           nrow=int(shape[0]*shape[1]/4) + 1,
+                                           padding=1)
+                else:
+                    continue
+
+                self.writer.add_image(name, grid.numpy(), global_step=engine.state.epoch)
+
     def _base_epoch_functions(self, engine):
         # Use engine.state.metrics
         if self.use_metrics:
-            self._log_metrics(engine)
+            self._log_metrics(engine=engine)
 
         # Train Evaluator
         if self.train_evaluator is not None:
@@ -150,35 +204,59 @@ class TensorBoardX(object):
 
         # Histogram of Weights and Gradients; in the case of multiple models, users can create custom functions
         if self.model is not None and self.histogram_freq > 0:
-            self._log_histograms(engine,
-                                 self.model,
+            self._log_histograms(engine=engine,
+                                 model=self.model,
                                  add_name=False)
+
+        if self.write_images and self.model is not None and self.histogram_freq > 0:
+            self._log_weight_image(engine=engine, model=self.model)
 
     def _base_iteration_functions(self, engine):
         # Use user created state attributes such as 'reward' for RL
         if self.state_keys is not None:
-            self._log_custom_state(engine)
+            self._log_custom_state(engine=engine)
 
         # Use engine.state.output for example mnist.py example
         if self.use_output:
-            self._log_engine_output(engine)
+            self._log_engine_output(engine=engine)
 
     def _on_complete(self, engine):
         self.writer.close()
 
-    def _difference(self):
+    def _call_children_function(self):
         functions = [getattr(self, x)
                      for x in dir(self) if (not x.startswith('_') and
                                             type(getattr(self, x)) == MethodType and
                                             x != 'attach')]
         return functions
 
+    def _vistensor(self, tensor, ch=0, allkernels=False, nrow=8, padding=1):
+        '''
+        vistensor: visuzlization tensor
+            @ch: visualization channel
+            @allkernels: visualization all tensors
+        '''
+
+        n, c, w, h = tensor.shape
+        if allkernels:
+            tensor = tensor.view(n * c, -1, w, h)
+        elif c != 3:
+            tensor = tensor[:, ch, :, :].unsqueeze(dim=1)
+
+        if w < 3 and h < 3:
+            padding = 0
+
+        rows = np.min((tensor.shape[0] // nrow + 1, 64))
+        grid = utils.make_grid(tensor.cpu().data, nrow=rows, normalize=True, padding=padding)
+
+        return grid
+
     def attach(self, engine):
         engine.add_event_handler(Events.STARTED, self._on_start)
         engine.add_event_handler(Events.ITERATION_COMPLETED, self._base_iteration_functions)
         engine.add_event_handler(Events.EPOCH_COMPLETED, self._base_epoch_functions)
 
-        child_functions = self._difference()
+        child_functions = self._call_children_function()
         for func in child_functions:
             engine.add_event_handler(Events.EPOCH_COMPLETED, func)
 
