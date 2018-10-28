386a94e541ab28996b3ee5fbd860c281f7983f36
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-10-28 12:55:37 -0400

    use pytest.approx

--- tests/ignite/contrib/metrics/test_mae.py
+++ tests/ignite/contrib/metrics/test_mae.py
@@ -1,8 +1,9 @@
 from ignite.contrib.metrics import MaximumAbsoluteError
 import torch
+from pytest import approx
 
 
-def test_maximum_absolute_error(tol=1e-6):
+def test_maximum_absolute_error():
     a = torch.tensor([0.0, 0.2, -0.8])
     b = torch.tensor([1.0, -2.0, 3.0])
     c = torch.tensor([0.0, 2.0, 0.0])
@@ -13,13 +14,13 @@ def test_maximum_absolute_error(tol=1e-6):
     m.reset()
 
     m.update((a, ground_truth))
-    assert abs(m.compute() - 0.8) < tol
+    assert m.compute() == approx(0.8)
 
     m.update((b, ground_truth))
-    assert abs(m.compute() - 3.0) < tol
+    assert m.compute() == approx(3.0)
 
     m.update((c, ground_truth))
-    assert abs(m.compute() - 3.0) < tol
+    assert m.compute() == approx(3.0)
 
     m.update((d, ground_truth))
-    assert abs(m.compute() - 5.0) < tol
+    assert m.compute() == approx(5.0)
