be538449c0ca2a994262def1ee0469c0118278b6
Anmol Joshi <anmolsjoshi@gmail.com>
2018-11-01 11:35:30 -0700

    fixed flake8

--- ignite/metrics/binary_accuracy.py
+++ ignite/metrics/binary_accuracy.py
@@ -9,6 +9,6 @@ class BinaryAccuracy(Accuracy):
     Note: This metric is deprecated in favor of Accuracy.
     """
     def __init__(self, *args, **kwargs):
-        warnings.warn("The use of metrics.BinaryAccuracy is deprecated, it will be " +
+        warnings.warn("The use of metrics.BinaryAccuracy is deprecated, it will be "
                       "removed in 0.1.2. Please use ignite.metrics.Accuracy instead.")
         super(Accuracy, self).__init__(*args, **kwargs)
