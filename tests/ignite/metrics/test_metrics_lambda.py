ae338748379194d4a269b9d9f3524a77434efe05
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-11-07 13:19:04 -0500

    test reset

--- tests/ignite/metrics/test_metrics_lambda.py
+++ tests/ignite/metrics/test_metrics_lambda.py
@@ -7,7 +7,7 @@ class ListGatherMetric(Metric):
             self.index = index
 
         def reset(self):
-            self.list_ = []
+            self.list_ = None
 
         def update(self, output):
             self.list_ = output
@@ -30,3 +30,19 @@ def test_metrics_lambda():
     m2_plus_2 = MetricsLambda(lambda x, y: x + y, m2, 2)
     m2_plus_2.update([1, 10, 100])
     assert m2_plus_2.compute() == 102
+
+
+def test_metrics_lambda_reset():
+    m0 = ListGatherMetric(0)
+    m1 = ListGatherMetric(1)
+    m2 = ListGatherMetric(2)
+    m0.update([1, 10, 100])
+    m1.update([1, 10, 100])
+    m2.update([1, 10, 100])
+
+    m = MetricsLambda(lambda x, y, z, t: 1, m0, m1, m2, 0)
+    m.reset()
+
+    assert m0.list_ is None
+    assert m1.list_ is None
+    assert m2.list_ is None
