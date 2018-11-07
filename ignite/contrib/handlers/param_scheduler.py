57102fdb19567cf62fc0e4b86d5feb7ecad528b1
Amit Aides <amitaid@il.ibm.com>
2018-11-07 06:20:04 +0200

    Applied comments modifications

--- ignite/contrib/handlers/param_scheduler.py
+++ ignite/contrib/handlers/param_scheduler.py
@@ -8,7 +8,7 @@ class ParamScheduler(object):
     training.
 
     Args:
-        optimizer (`torch.optim.Optimizer`/dict): the optimizer or parameters group to use
+        optimizer (`torch.optim.Optimizer` or dict): the optimizer or parameters group to use
         param_name (str): name of optimizer's parameter to update
         save_history (bool, optional): whether to log the parameter values
             (default=False)
@@ -54,7 +54,7 @@ class CyclicalScheduler(ParamScheduler):
     cycle of some size.
 
     Args:
-        optimizer (`torch.optim.Optimizer`/dict): the optimizer or parameters group to use
+        optimizer (`torch.optim.Optimizer` or dict): the optimizer or parameters group to use
         param_name (str): name of optimizer's parameter to update
         start_value (float): value at start of cycle
         end_value (float) : value at the middle of the cycle
@@ -101,7 +101,7 @@ class LinearCyclicalScheduler(CyclicalScheduler):
     adjusts it back to 'start_value' for a half-cycle.
 
     Args:
-        optimizer (`torch.optim.Optimizer`/dict): the optimizer or parameters group to use
+        optimizer (`torch.optim.Optimizer` or dict): the optimizer or parameters group to use
         param_name (str): name of optimizer's parameter to update
         start_value (float): value at start of cycle
         end_value (float) : value at the middle of the cycle
@@ -140,7 +140,7 @@ class CosineAnnealingScheduler(CyclicalScheduler):
     wave (as suggested in [Smith17]_).
 
     Args:
-        optimizer (`torch.optim.Optimizer`/dict): the optimizer or parameters group to use
+        optimizer (`torch.optim.Optimizer` or dict): the optimizer or parameters group to use
         param_name (str): name of optimizer's parameter to update
         start_value (float): value at start of cycle
         end_value (float) : value at the end of the cycle
@@ -187,7 +187,7 @@ class ConcatScheduler(ParamScheduler):
     switch to the next scheduler.
 
     Args:
-        optimizer (`torch.optim.Optimizer`/dict): the optimizer or parameters group to use
+        optimizer (`torch.optim.Optimizer` or dict): the optimizer or parameters group to use
         param_name (str): name of optimizer's parameter to update
         schedulers_list (list): List of three tuple of the order (scheduler_cls,
             scheduler_kwds, duration).
