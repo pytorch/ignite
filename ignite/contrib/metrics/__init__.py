5e63512f8ad4c6d555aae967c0f78a1bc1d2cef3
WeihuangXu <weihuangxu@hotmail.com>
2018-11-15 21:58:17 -0500

    change __init__

--- ignite/contrib/metrics/__init__.py
+++ ignite/contrib/metrics/__init__.py
@@ -1,4 +1,4 @@
 from ignite.contrib.metrics.average_precision import AveragePrecision
 from ignite.contrib.metrics.roc_auc import ROC_AUC
 from ignite.contrib.metrics.mae import MaximumAbsoluteError
-from ignite.contrib.metrics.MARE import MeanAbsoluteRelativeError
+from ignite.contrib.metrics.mean_absolute_relative_error import MeanAbsoluteRelativeError
