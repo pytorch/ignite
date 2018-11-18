cb26dbc01bd58044c692a2593d1fff9ab00332a6
Amit Aides <amitaid@il.ibm.com>
2018-11-18 14:07:29 +0200

    Wrote mock test files

--- tests/ignite/contrib/handlers/test_visdom_logger.py
+++ tests/ignite/contrib/handlers/test_visdom_logger.py
@@ -1,102 +1,92 @@
 # -*- coding: utf-8 -*-
 import json
+import numpy as np
 import os
 import pytest
+import tempfile
+from unittest.mock import call
+from unittest.mock import patch
 
 from ignite.contrib.handlers import VisdomLogger
 from ignite.engine import Engine
-import tempfile
 
 
-expected_log = [
-    [
-        'events',
-        {
-            'data': [
-                {
-                    'x': [0],
-                    'y': [1],
-                    'name': 'loss',
-                    'type': 'scatter',
-                    'mode': 'lines',
-                    'textposition': 'right',
-                    'marker': {
-                        'size': 10,
-                        'symbol': 'dot',
-                        'line': {'color': '#000000', 'width': 0.5}
-                    }
-                }
-            ],
-            'eid': 'main',
-            'layout': {
-                'showlegend': False,
-                'title': 'Training Loss',
-                'margin': {'l': 60, 'r': 60, 't': 60, 'b': 60},
-                'xaxis': {'title': 'Iteration'},
-                'yaxis': {'title': 'Loss'}
-            },
-            'opts': {
-                'title': 'Training Loss',
-                'xlabel': 'Iteration',
-                'ylabel': 'Loss',
-                'showlegend': False,
-                'markers': False,
-                'fillarea': False,
-                'mode': 'lines',
-                'markersymbol': 'dot',
-                'markersize': 10
-            }
-        }
-    ],
-    [
-        'update',
-        {
-            'data': [
-                {
-                    'x': [1],
-                    'y': [1],
-                    'name': 'loss',
-                    'type': 'scatter',
-                    'mode': 'lines',
-                    'textposition': 'right',
-                    'marker': {
-                        'size': 10,
-                        'symbol': 'dot',
-                        'line': {'color': '#000000', 'width': 0.5}
-                    }
-                }
-            ],
-            'eid': 'main',
-            'layout': {},
-            'opts': {
-                'title': 'Training Loss',
-                'xlabel': 'Iteration',
-                'ylabel': 'Loss',
-                'showlegend': False,
-                'markers': False,
-                'fillarea': False,
-                'mode': 'lines',
-                'markersymbol': 'dot',
-                'markersize': 10
-            },
-            'name': 'loss',
-            'append': True
-        }
-    ]
-]
-
-
-def update_fn(engine, batch):
+def process_function(engine, batch):
     a = 1
     engine.state.metrics['a'] = a
     return a
 
 
-def test_visdom():
+@patch('visdom.Visdom')
+def test_events(vis):
+    """Test calls to visdom logger."""
+
+    #
+    # Create replies to visdom calls.
+    #
+    vis.check_connection.return_value = True
+    vis.line.return_value = 1234
 
     n_epochs = 2
     loader = [1, 2]
-    engine = Engine(update_fn)
+    engine = Engine(process_function)
+
+    visdom_logger = VisdomLogger(
+        vis=vis
+    )
+
+    loss_win = visdom_logger.create_window(
+        window_title="Training Loss",
+        xlabel="Iteration",
+        ylabel="Loss"
+    )
+    loss_win.attach(
+        engine=engine,
+        output_transform=lambda x: {"loss": x},
+    )
+
+    engine.run(loader, max_epochs=n_epochs)
+
+    line_calls = [
+        call(
+            X=np.array([0]),
+            Y=np.array([1]),
+            env='main',
+            name='loss',
+            opts={
+                'title': 'Training Loss',
+                'xlabel': 'Iteration',
+                'ylabel': 'Loss',
+                'showlegend': False
+            },
+            update=None,
+            win=None
+        ),
+        call(
+            X=np.array([1]),
+            Y=np.array([1]),
+            env='main',
+            name='loss',
+            opts={
+                'title': 'Training Loss',
+                'xlabel': 'Iteration',
+                'ylabel': 'Loss',
+                'showlegend': False
+            },
+            update='append',
+            win=1234
+        )
+    ]
+
+    assert vis.line.call_args_list == line_calls
+
+
+def test_log_file():
+    """Test creation of the visdom log file."""
+
+    n_epochs = 2
+    loader = [1, 2]
+    engine = Engine(process_function)
 
     with tempfile.NamedTemporaryFile(delete=False) as f:
         log_filename = f.name
@@ -118,12 +108,13 @@ def test_visdom():
     with open(log_filename) as f:
         visdom_log = [json.loads(l.strip()) for l in f.readlines()]
 
-    #
-    # Not comparing window name.
-    #
-    for vl in visdom_log:
-        del vl[1]["win"]
-
     os.remove(log_filename)
 
-    assert visdom_log == expected_log
+    #
+    # Assert the events in the log file.
+    # The actual contents of the events are not checked as these depend on the
+    # installed visdom version.
+    #
+    assert len(visdom_log) == 2
+    assert visdom_log[0][0] == 'events'
+    assert visdom_log[1][0] == 'update'
