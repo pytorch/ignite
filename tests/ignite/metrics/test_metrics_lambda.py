e968238e68bb8611e1ef3720c6080f890b96f4eb
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-11-20 20:27:31 -0500

    integration test

--- tests/ignite/metrics/test_metrics_lambda.py
+++ tests/ignite/metrics/test_metrics_lambda.py
@@ -1,5 +1,10 @@
 from ignite.engine import Engine
-from ignite.metrics import Metric, MetricsLambda
+from ignite.metrics import Metric, MetricsLambda, Precision, Recall
+from pytest import approx
+from sklearn.metrics import precision_score, recall_score, f1_score
+import numpy as np
+from numpy.testing import assert_allclose
+import torch
 
 
 class ListGatherMetric(Metric):
@@ -63,3 +68,59 @@ def test_metrics_lambda_reset():
     assert m0.list_ is None
     assert m1.list_ is None
     assert m2.list_ is None
+
+
+def test_integration():
+    np.random.seed(1)
+
+    n_iters = 10
+    batch_size = 10
+    n_classes = 10
+
+    y_true = np.arange(0, n_iters * batch_size) % n_classes
+    y_pred = 0.2 * np.random.rand(n_iters * batch_size, n_classes)
+    for i in range(n_iters * batch_size):
+        if np.random.rand() > 0.4:
+            y_pred[i, y_true[i]] = 1.0
+        else:
+            j = np.random.randint(0, n_classes)
+            y_pred[i, j] = 0.7
+
+    y_true_batch_values = iter(y_true.reshape(n_iters, batch_size))
+    y_pred_batch_values = iter(y_pred.reshape(n_iters, batch_size, n_classes))
+
+
+    def update_fn(engine, batch):
+        y_true_batch = next(y_true_batch_values)
+        y_pred_batch = next(y_pred_batch_values)
+        return torch.from_numpy(y_pred_batch), torch.from_numpy(y_true_batch)
+
+
+    evaluator = Engine(update_fn)
+
+    precision = Precision(average=False)
+    recall = Recall(average=False)
+
+    def Fbeta(r, p, beta):
+        return torch.mean((1 + beta ** 2) * p * r / (beta ** 2 * p + r)).item()
+
+
+    F1 = MetricsLambda(Fbeta, recall, precision, 1)
+
+    precision.attach(evaluator, "precision")
+    recall.attach(evaluator, "recall")
+    F1.attach(evaluator, "f1")
+
+    data = list(range(n_iters))
+    state = evaluator.run(data, max_epochs=1)
+
+    precision_true = precision_score(y_true, np.argmax(y_pred, axis=-1), average=None)
+    recall_true = recall_score(y_true, np.argmax(y_pred, axis=-1), average=None)
+    f1_true = f1_score(y_true, np.argmax(y_pred, axis=-1), average='macro')
+
+    ignite_precision = state.metrics['precision'].numpy()
+    ignite_recall = state.metrics['recall'].numpy()
+
+    assert_allclose(precision_true, ignite_precision, err_msg="{} vs {}".format(precision_true, ignite_precision))
+    assert_allclose(recall_true, ignite_recall, err_msg="{} vs {}".format(recall_true, ignite_recall))
+    assert f1_true == approx(state.metrics['f1']), "{} vs {}".format(f1_true, state.metrics['f1'])
