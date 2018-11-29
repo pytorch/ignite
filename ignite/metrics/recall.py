Updated Precision and Recall to fix binary calculation, ensure that calculation type remains the same during training, calculate binary precision using a threshold function vs categorical.

ignite/metrics/recall.py
@@ -9,54 +9,47 @@ from ignite._utils import to_onehot
 
 class Recall(Metric):
     """
-    Calculates recall.
-
+    - | `threshold_function` is only needed for binary cases. Default is `torch.round(x)`. It is used to convert
+      | `y_pred` to 0's and 1's.
     - `update` must receive output of the form `(y_pred, y)`.
-
-    If `average` is True, returns the unweighted average across all classes.
+    - | For binary or multiclass cases, `y_pred` must be in the following shape (batch_size, num_categories, ...) or
+      | (batch_size, ...) and `y` must be in the following shape (batch_size, ...).
+    For binary or multiclass cases, if `average` is True, returns the unweighted average across all classes.
     Otherwise, returns a tensor with the recall for each class.
     """
-
-    def __init__(self, average=False, output_transform=lambda x: x):
-        super(Recall, self).__init__(output_transform)
+    def __init__(self, output_transform=lambda x: x, average=False, threshold_function=None):
         self._average = average
+        if threshold_function is not None:
+            if callable(threshold_function):
+                self._threshold = threshold_function
+            else:
+                raise ValueError("threshold_function must be a callable function.")
+        else:
+            self._threshold = torch.round
+        self._updated = False
+        super(Recall, self).__init__(output_transform=output_transform)
 
     def reset(self):
         self._actual = None
         self._true_positives = None
 
     def update(self, output):
-        y_pred, y = output
+        y_pred, y = self._check_shape(output)
+        self._check_type((y_pred, y))
+
         dtype = y_pred.type()
 
-        if not (y.ndimension() == y_pred.ndimension() or y.ndimension() + 1 == y_pred.ndimension()):
-            raise ValueError("y must have shape of (batch_size, ...) and y_pred "
-                             "must have shape of (batch_size, num_classes, ...) or (batch_size, ...).")
-
-        if y.ndimension() > 1 and y.shape[1] == 1:
-            y = y.squeeze(dim=1)
-
-        if y_pred.ndimension() > 1 and y_pred.shape[1] == 1:
-            y_pred = y_pred.squeeze(dim=1)
-
-        y_shape = y.shape
-        y_pred_shape = y_pred.shape
-
-        if y.ndimension() + 1 == y_pred.ndimension():
-            y_pred_shape = (y_pred_shape[0],) + y_pred_shape[2:]
-
-        if not (y_shape == y_pred_shape):
-            raise ValueError("y and y_pred must have compatible shapes.")
-
-        if y_pred.ndimension() == y.ndimension():
-            # Maps Binary Case to Categorical Case with 2 classes
-            y_pred = y_pred.unsqueeze(dim=1)
-            y_pred = torch.cat([1.0 - y_pred, y_pred], dim=1)
-
-        y = to_onehot(y.view(-1), num_classes=y_pred.size(1))
-        indices = torch.max(y_pred, dim=1)[1].view(-1)
-        y_pred = to_onehot(indices, num_classes=y_pred.size(1))
-
+        if self._type == 'binary':
+            y_pred = self._threshold(y_pred)
+            if not torch.equal(y, y **2):
+                raise ValueError("For binary cases, y must contain 0's and 1's only.")
+            if not torch.equal(y_pred, y_pred**2):
+                raise ValueError("For binary cases, y_pred must contain 0's and 1's only.")
+        else:
+            y = to_onehot(y.view(-1), num_classes=y_pred.size(1))
+            indices = torch.max(y_pred, dim=1)[1].view(-1)
+            y_pred = to_onehot(indices, num_classes=y_pred.size(1))
+        
         y_pred = y_pred.type(dtype)
         y = y.type(dtype)
 
@@ -83,3 +76,46 @@ class Recall(Metric):
             return result.mean().item()
         else:
             return result
+
+    def _check_shape(self, output):
+        y_pred, y = output
+
+        if not (y.ndimension() == y_pred.ndimension() or y.ndimension() + 1 == y_pred.ndimension()):
+            raise ValueError("y must have shape of (batch_size, ...) and y_pred must have "
+                             "shape of (batch_size, num_categories, ...) or (batch_size, ...).")
+
+        if y.ndimension() == 1 or y.shape[1] == 1:
+            # Binary Case, flattens y and num_classes is equal to 1.
+            y = y.squeeze(dim=1).view(-1) if (y.ndimension() > 1) else y.view(-1)
+
+        if y_pred.ndimension() == 1 or y_pred.shape[1] == 1:
+            # Binary Case, flattens y and num_classes is equal to 1.
+            y_pred = y_pred.squeeze(dim=1).view(-1) if (y_pred.ndimension() > 1) else y_pred.view(-1)
+
+        y_shape = y.shape
+        y_pred_shape = y_pred.shape
+
+        if y.ndimension() + 1 == y_pred.ndimension():
+            y_pred_shape = (y_pred_shape[0],) + y_pred_shape[2:]
+
+        if not (y_shape == y_pred_shape):
+            raise ValueError("y and y_pred must have compatible shapes.")
+
+        return y_pred, y
+
+    def _check_type(self, output):
+        y_pred, y = output
+
+        if y.ndimension() + 1 == y_pred.ndimension():
+            update_type = 'multiclass'
+        elif y_pred.shape == y.shape and y.ndimension() == 1:
+            update_type = 'binary'
+        else:
+            raise TypeError('Invalid shapes of y (shape={}) and y_pred (shape={}), check documentation'
+                            ' for expected shapes of y and y_pred.'.format(y.shape, y_pred.shape))
+        if not self._updated:
+            self._type = update_type
+            self._updated = True
+        else:
+            if self._type != update_type:
+                raise TypeError('update_type has changed from {} to {}.'.format(self._type, update_type))

tests/ignite/metrics/test_recall.py
@@ -65,41 +65,38 @@ def test_compute_all_wrong():
 def test_binary_vs_categorical():
     recall = Recall(average=True)
 
-    y_pred = torch.FloatTensor([0.9, 0.2])
-    y = torch.LongTensor([1, 0])
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
+    y_pred = torch.FloatTensor([0.9, 0.2, 0.3, 0.5, 0.6])
+    y = torch.LongTensor([1, 0, 1, 0, 0])
     recall.update((y_pred, y))
-    assert recall.compute() == pytest.approx(recall_score(y.data.numpy(), indices.data.numpy(), average='macro'))
-    assert recall.compute() == 1.0
+    np_y = y.numpy()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert recall.compute() == pytest.approx(recall_score(np_y, np_y_pred))
 
-    recall.reset()
-    y_pred = torch.FloatTensor([[0.1, 0.9], [0.8, 0.2]])
-    y = torch.LongTensor([1, 0])
+    recall = Recall(average=True)
+    y_pred = torch.FloatTensor([[0.1, 0.9], [0.8, 0.2], [0.25, 0.75], [0.95, 0.05], [0.54, 0.46]])
+    y = torch.LongTensor([1, 0, 0, 0, 1])
     indices = torch.max(y_pred, dim=1)[1]
     recall.update((y_pred, y))
-    assert recall.compute() == pytest.approx(recall_score(y.data.numpy(), indices.data.numpy(), average='macro'))
-    assert recall.compute() == 1.0
+    assert recall.compute() == pytest.approx(recall_score(y.numpy(), indices.numpy(), average='macro'))
 
 
 def test_binary_shapes():
     recall = Recall(average=True)
 
-    y = torch.LongTensor([1, 0])
-    y_pred = torch.FloatTensor([0.9, 0.2])
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
+    y = torch.LongTensor([1, 0, 1, 0, 0])
+    y_pred = torch.FloatTensor([0.9, 0.2, 0.7, 0.54, 0.29])
     recall.update((y_pred, y))
-    assert recall.compute() == pytest.approx(recall_score(y.data.numpy(), indices.data.numpy(), average='macro'))
-    assert recall.compute() == 1.0
+    np_y = y.numpy()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert recall.compute() == pytest.approx(recall_score(np_y, np_y_pred))
 
-    y = torch.LongTensor([[1], [0]])
-    y_pred = torch.FloatTensor([[0.9], [0.2]])
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
+    y = torch.LongTensor([[1], [0], [0], [1], [0]])
+    y_pred = torch.FloatTensor([[0.9], [0.2], [0.43], [0.56], [0.78]])
     recall.reset()
     recall.update((y_pred, y))
-    assert recall.compute() == pytest.approx(recall_score(y.data.numpy(), indices.data.numpy(), average='macro'))
-    assert recall.compute() == 1.0
+    np_y = y.numpy()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert recall.compute() == pytest.approx(recall_score(np_y, np_y_pred))
 
 
 def test_ner_example():
@@ -158,3 +155,40 @@ def test_sklearn_compute():
     recall_ig = [recall_ig[i] for i in y_pred_labels]
 
     assert all([a == pytest.approx(b) for a, b in zip(recall_sk, recall_ig)])
+
+
+def test_incorrect_threshold():
+    with pytest.raises(ValueError):
+        recall = Recall(threshold_function=2)
+
+    recall = Recall(threshold_function=lambda x: x + 2)
+
+    y_pred = torch.rand(4, 1)
+    y = torch.ones(4, 1).type(torch.LongTensor)
+
+    with pytest.raises(ValueError):
+        recall.update((y_pred, y))
+
+
+def test_incorrect_binary_y():
+    recall = Recall()
+
+    y_pred = torch.rand(4, 1)
+    y = 2 * torch.ones(4, 1).type(torch.LongTensor)
+
+    with pytest.raises(ValueError):
+        recall.update((y_pred, y))
+
+
+def test_incorrect_type():
+    recall = Recall()
+
+    y_pred = torch.softmax(torch.rand(4, 4), dim=1)
+    y = torch.ones(4).type(torch.LongTensor)
+    recall.update((y_pred, y))
+
+    y_pred = torch.rand(4, 1)
+    y = torch.ones(4).type(torch.LongTensor)
+
+    with pytest.raises(TypeError):
+        recall.update((y_pred, y))