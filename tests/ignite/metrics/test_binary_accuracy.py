6a60f9cd94ca5263ded19fe809e239dc6a318626
vfdev-5 <vfdev.5@gmail.com>
2018-12-07 23:03:06 +0100

    Change warning message 0.1.2 -> 0.2.0, remove tests from binary/categorical accuracy

--- tests/ignite/metrics/test_binary_accuracy.py
+++ tests/ignite/metrics/test_binary_accuracy.py
@@ -1,68 +1,7 @@
-from ignite.exceptions import NotComputableError
 from ignite.metrics import BinaryAccuracy
 import pytest
-import torch
-from sklearn.metrics import accuracy_score
 
 
-def test_zero_div():
+def test_warning():
     with pytest.warns(DeprecationWarning):
-        acc = BinaryAccuracy()
-
-    with pytest.raises(NotComputableError):
-        acc.compute()
-
-
-def test_compute():
-    with pytest.warns(DeprecationWarning):
-        acc = BinaryAccuracy()
-
-    y_pred = torch.sigmoid(torch.rand(4, 1))
-    y = torch.ones(4).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert acc._type == 'binary'
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(4))
-    y = torch.ones(4).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert acc._type == 'binary'
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_compute_batch_images():
-    with pytest.warns(DeprecationWarning):
-        acc = BinaryAccuracy()
-
-    y_pred = torch.sigmoid(torch.rand(1, 2, 2))
-    y = torch.ones(1, 2, 2).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert acc._type == 'binary'
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert acc._type == 'binary'
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 1, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert acc._type == 'binary'
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
+        BinaryAccuracy()
