Bug Related to Calculation of Binary Metrics (#349)
* Updated Precision and Recall to fix binary calculation, ensure that calculation type remains the same during training, calculate binary precision using a threshold function vs categorical.

* Fixed flake8 errors

* Updated handling of threshold_function.

* _classification_support.py -  Refactored checks into ClassificationSupport, precision/recall calculation into PrecisionRecallSupport.
precision.py and recall.py - use PrecisionRecallSupport

* Fixed flake8 errors, passing tests local

* Refactored Precision and Recall functions into _BasePrecisionRecallSupport (child of _BaseClassification). Refactored Accuracy into _BaseClassification.

* Removed .idea folder.

* Renamed precision.py to precision_recall.py which now contains _BasePrecisionRecallSupport, Precision, Recall.

* Added tests similar to Precision/Recall and vfdev5 modified tests for accuracy from #333

* Updated Precision and Recall with separate update functions

* Updated accuracy.py by removing threshold_function and adding categorical mapping.

* Updated precision_recall to remove threshold_function and add binary to categorical mapping.

* removed constructor from Accuracy

* Updated docstring, fixed bug in Accuracy, changed condition for binary to categorical mapping.

* Added warning for num_classes=2

* Added warning for num_classes=2, included tests in Precision/Recall with warning check, type check and binary multiclass case.

* Updated warning to print appropriate class name.

* Corrected warning format.

* Removed copied line from the docs

* Removed self._updated, track changing update type using self._type per comment

* Fixed docstring for Precision.

* Added DepreciationWarning to binary accuracy, categorical accuracy. Updated tests to catch warning, updated accuracy tests to assert _type.

* Fixed correct calculation for accuracy, separated precision_recall.py to precision.py and recall.

tests/ignite/metrics/test_accuracy.py
@@ -5,115 +5,277 @@ import torch
 from sklearn.metrics import accuracy_score
 
 
-def test_zero_div():
+torch.manual_seed(12)
+
+
+def test_no_update():
     acc = Accuracy()
     with pytest.raises(NotComputableError):
         acc.compute()
 
 
-def test_binary_compute():
+def test__check_shape():
     acc = Accuracy()
 
-    y_pred = torch.sigmoid(torch.rand(4, 1))
-    y = torch.ones(4).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
+    # Check squeezed dimensions
+    y_pred, y = acc._check_shape((torch.randint(0, 2, size=(10, 1, 5, 6)).type(torch.LongTensor),
+                                  torch.randint(0, 2, size=(10, 5, 6)).type(torch.LongTensor)))
+    assert y_pred.shape == (10, 5, 6)
+    assert y.shape == (10, 5, 6)
 
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(4))
-    y = torch.ones(4).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
+    y_pred, y = acc._check_shape((torch.randint(0, 2, size=(10, 5, 6)).type(torch.LongTensor),
+                                  torch.randint(0, 2, size=(10, 1, 5, 6)).type(torch.LongTensor)))
+    assert y_pred.shape == (10, 5, 6)
+    assert y.shape == (10, 5, 6)
 
 
-def test_compute_batch_images():
+def test_binary_wrong_inputs():
     acc = Accuracy()
-
-    y_pred = torch.sigmoid(torch.rand(1, 2, 2))
-    y = torch.ones(1, 2, 2).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 1, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_categorical_compute():
-    acc = Accuracy()
-
-    y_pred = torch.softmax(torch.rand(4, 4), dim=1)
-    y = torch.ones(4).type(torch.LongTensor)
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.softmax(torch.rand(2, 2), dim=1)
-    y = torch.ones(2).type(torch.LongTensor)
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_categorical_compute_batch_images():
-    acc = Accuracy()
-
-    y_pred = torch.softmax(torch.rand(2, 3, 2, 2), dim=1)
-    y = torch.LongTensor([[[0, 1],
-                           [0, 1]],
-                          [[0, 2],
-                           [0, 2]]])
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_ner_example():
-    acc = Accuracy()
-
-    y_pred = torch.softmax(torch.rand(2, 3, 8), dim=1)
-    y = torch.Tensor([[1, 1, 1, 1, 1, 1, 1, 1],
-                      [2, 2, 2, 2, 2, 2, 2, 2]]).type(torch.LongTensor)
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_incorrect_shape():
-    acc = Accuracy()
-
-    y_pred = torch.zeros(2, 3, 2, 2)
-    y = torch.zeros(2, 3)
 
     with pytest.raises(ValueError):
-        acc.update((y_pred, y))
+        # y has not only 0 or 1 values
+        acc.update((torch.randint(0, 2, size=(10,)).type(torch.LongTensor),
+                    torch.arange(0, 10).type(torch.LongTensor)))
 
-    y_pred = torch.zeros(2, 3, 2, 2)
-    y = torch.zeros(2, 3, 4, 4)
+    # TODO: Uncomment the following after 0.1.2 release
+    # with pytest.raises(ValueError):
+    #     # y_pred values are not thresholded to 0, 1 values
+    #     acc.update((torch.rand(10, 1),
+    #                 torch.randint(0, 2, size=(10,)).type(torch.LongTensor)))
 
     with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.randint(0, 2, size=(10,)).type(torch.LongTensor),
+                    torch.randint(0, 2, size=(10, 5)).type(torch.LongTensor)))
+
+    with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.randint(0, 2, size=(10, 5, 6)).type(torch.LongTensor),
+                    torch.randint(0, 2, size=(10,)).type(torch.LongTensor)))
+
+    with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.randint(0, 2, size=(10,)).type(torch.LongTensor),
+                    torch.randint(0, 2, size=(10, 5, 6)).type(torch.LongTensor)))
+
+
+def test_binary_input_N():
+    # Binary accuracy on input of shape (N, 1) or (N, )
+    acc = Accuracy()
+
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(10, 1)).type(torch.LongTensor)
+    y_pred = torch.rand(10, 1)
+    y = torch.randint(0, 2, size=(10,)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(10, )).type(torch.LongTensor)
+    y_pred = torch.rand(10)
+    y = torch.randint(0, 2, size=(10,)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_binary_input_NL():
+    # Binary accuracy on input of shape (N, L)
+    acc = Accuracy()
+
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(10, 5)).type(torch.LongTensor)
+    y_pred = torch.rand(10, 5)
+    y = torch.randint(0, 2, size=(10, 5)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(10, 1, 5)).type(torch.LongTensor)
+    y_pred = torch.rand(10, 1, 5)
+    y = torch.randint(0, 2, size=(10, 1, 5)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_binary_input_NHW():
+    # Binary accuracy on input of shape (N, H, W, ...)
+    acc = Accuracy()
+
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(4, 12, 10)).type(torch.LongTensor)
+    y_pred = torch.rand(4, 12, 10)
+    y = torch.randint(0, 2, size=(4, 12, 10)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(4, 1, 12, 10)).type(torch.LongTensor)
+    y_pred = torch.rand(4, 1, 12, 10)
+    y = torch.randint(0, 2, size=(4, 1, 12, 10)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    # TODO: y_pred should be binary after 0.1.2 release
+    # y_pred = torch.randint(0, 2, size=(4, 12, 10, 8)).type(torch.LongTensor)
+    y_pred = torch.rand(4, 12, 10, 8)
+    y = torch.randint(0, 2, size=(4, 12, 10, 8)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y = y.numpy().ravel()
+    # np_y_pred = y_pred.numpy().ravel()
+    np_y_pred = (y_pred.numpy().ravel() > 0.5).astype('int')
+    assert acc._type == 'binary'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_multiclass_wrong_inputs():
+    acc = Accuracy()
+
+    with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.rand(10, 5, 4),
+                    torch.randint(0, 2, size=(10,)).type(torch.LongTensor)))
+
+    with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.rand(10, 5, 6),
+                    torch.randint(0, 5, size=(10, 5)).type(torch.LongTensor)))
+
+    with pytest.raises(ValueError):
+        # incompatible shapes
+        acc.update((torch.rand(10),
+                    torch.randint(0, 5, size=(10, 5, 6)).type(torch.LongTensor)))
+
+
+def test_multiclass_input_N():
+    # Multiclass input data of shape (N, ) and (N, C)
+
+    acc = Accuracy()
+
+    y_pred = torch.rand(10, 4)
+    y = torch.randint(0, 4, size=(10,)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    y_pred = torch.rand(4, 10)
+    y = torch.randint(0, 10, size=(4, 1)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    # 2-classes
+    acc.reset()
+    y_pred = torch.rand(4, 2)
+    y = torch.randint(0, 2, size=(4, 1)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_multiclass_input_NL():
+    # Multiclass input data of shape (N, L) and (N, C, L)
+    acc = Accuracy()
+
+    y_pred = torch.rand(10, 4, 5)
+    y = torch.randint(0, 4, size=(10, 5)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    y_pred = torch.rand(4, 10, 5)
+    y = torch.randint(0, 10, size=(4, 5)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_multiclass_input_NHW():
+    # Multiclass input data of shape (N, H, W, ...) and (N, C, H, W, ...)
+    acc = Accuracy()
+
+    y_pred = torch.rand(4, 5, 12, 10)
+    y = torch.randint(0, 5, size=(4, 12, 10)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+    acc.reset()
+    y_pred = torch.rand(4, 5, 10, 12, 8)
+    y = torch.randint(0, 5, size=(4, 10, 12, 8)).type(torch.LongTensor)
+    acc.update((y_pred, y))
+    np_y_pred = y_pred.numpy().argmax(axis=1).ravel()
+    np_y = y.numpy().ravel()
+    assert acc._type == 'multiclass'
+    assert isinstance(acc.compute(), float)
+    assert accuracy_score(np_y, np_y_pred) == pytest.approx(acc.compute())
+
+
+def test_incorrect_type():
+    acc = Accuracy()
+
+    # Start as binary data
+    y_pred = torch.rand(4,)
+    y = torch.ones(4).type(torch.LongTensor)
+    acc.update((y_pred, y))
+
+    # And add a multiclass data
+    y_pred = torch.rand(4, 4)
+    y = torch.ones(4).type(torch.LongTensor)
+
+    with pytest.raises(RuntimeError):
         acc.update((y_pred, y))

tests/ignite/metrics/test_binary_accuracy.py
@@ -1,59 +1,7 @@
-from ignite.exceptions import NotComputableError
 from ignite.metrics import BinaryAccuracy
 import pytest
-import torch
-from sklearn.metrics import accuracy_score
 
 
-def test_zero_div():
-    acc = BinaryAccuracy()
-    with pytest.raises(NotComputableError):
-        acc.compute()
-
-
-def test_compute():
-    acc = BinaryAccuracy()
-
-    y_pred = torch.sigmoid(torch.rand(4, 1))
-    y = torch.ones(4).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(4))
-    y = torch.ones(4).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.data.numpy(), indices.data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_compute_batch_images():
-    acc = BinaryAccuracy()
-
-    y_pred = torch.sigmoid(torch.rand(1, 2, 2))
-    y = torch.ones(1, 2, 2).type(torch.LongTensor)
-    y_pred = y_pred.unsqueeze(1)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.sigmoid(torch.rand(2, 1, 2, 2))
-    y = torch.ones(2, 1, 2, 2).type(torch.LongTensor)
-    indices = torch.max(torch.cat([1.0 - y_pred, y_pred], dim=1), dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
+def test_warning():
+    with pytest.warns(DeprecationWarning):
+        BinaryAccuracy()

tests/ignite/metrics/test_categorical_accuracy.py
@@ -1,44 +1,7 @@
-from ignite.exceptions import NotComputableError
 from ignite.metrics import CategoricalAccuracy
 import pytest
-import torch
-from sklearn.metrics import accuracy_score
 
 
-def test_zero_div():
-    acc = CategoricalAccuracy()
-    with pytest.raises(NotComputableError):
-        acc.compute()
-
-
-def test_compute():
-    acc = CategoricalAccuracy()
-
-    y_pred = torch.softmax(torch.rand(4, 4), dim=1)
-    y = torch.ones(4).type(torch.LongTensor)
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-    acc.reset()
-    y_pred = torch.softmax(torch.rand(2, 2), dim=1)
-    y = torch.ones(2).type(torch.LongTensor)
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
-
-
-def test_compute_batch_images():
-    acc = CategoricalAccuracy()
-
-    y_pred = torch.softmax(torch.rand(2, 3, 2, 2), dim=1)
-    y = torch.LongTensor([[[0, 1],
-                           [0, 1]],
-                          [[0, 2],
-                           [0, 2]]])
-    indices = torch.max(y_pred, dim=1)[1]
-    acc.update((y_pred, y))
-    assert isinstance(acc.compute(), float)
-    assert accuracy_score(y.view(-1).data.numpy(), indices.view(-1).data.numpy()) == pytest.approx(acc.compute())
+def test_warning():
+    with pytest.warns(DeprecationWarning):
+        CategoricalAccuracy()