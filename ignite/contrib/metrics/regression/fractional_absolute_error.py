75237d03cc32a82d00207e31fe08c1aee12827fc
Anmol Joshi <anmolsjoshi@gmail.com>
2018-12-18 10:49:50 -0800

    Added D2 Regression Metrics (#334)
    * D2 Metrics without tests
    
    * Completed test_fractional_absolute_error.py, otherwise empty tests
    
    * fixed flake8 errors
    
    * Added tests for all D2 metrics, passing pytest and flake8
    
    * removed GMRAE and tests, changed GMAE calculation to remove numpy to calculate power.
    
    * Updated documentation
    
    * flake8 errors
    
    * Updated docstring.
    
    * Updated implementation of GMAE using log and exp to prevent underflow
    
    * Added common regression abs class
    
    * Added new tests of wrong input shape

--- ignite/contrib/metrics/regression/fractional_absolute_error.py
+++ ignite/contrib/metrics/regression/fractional_absolute_error.py
@@ -1,18 +1,16 @@
 from __future__ import division
-
 import torch
-
 from ignite.exceptions import NotComputableError
 from ignite.contrib.metrics.regression._base import _BaseRegression
 
 
-class FractionalBias(_BaseRegression):
+class FractionalAbsoluteError(_BaseRegression):
     r"""
-    Calculates the Fractional Bias:
+    Calculates the Fractional Absolute Error.
 
-    :math:`\text{FB} = \frac{1}{n}\sum_{j=1}^n\frac{2 * (A_j - P_j)}{A_j + P_j}`,
+    :math:`\text{FAE} = \frac{1}{n}\sum _j^n\frac{2 * |A_j - P_j|}{|A_j| + |P_j|}`
 
-    where :math:`A_j` is the ground truth and :math:`P_j` is the predicted value.
+    where, :math:`A_j` is the ground truth and :math:`P_j` is the predicted value.
 
     More details can be found in `Botchkarev 2018`__.
 
@@ -20,19 +18,19 @@ class FractionalBias(_BaseRegression):
     - `y` and `y_pred` must be of same shape `(N, )` or `(N, 1)`.
 
     __ https://arxiv.org/abs/1809.03006
-
     """
+
     def reset(self):
         self._sum_of_errors = 0.0
         self._num_examples = 0
 
     def _update(self, output):
         y_pred, y = output
-        errors = 2 * (y.view_as(y_pred) - y_pred) / (y_pred + y.view_as(y_pred))
+        errors = 2 * torch.abs(y.view_as(y_pred) - y_pred) / (torch.abs(y_pred) + torch.abs(y.view_as(y_pred)))
         self._sum_of_errors += torch.sum(errors).item()
         self._num_examples += y.shape[0]
 
     def compute(self):
         if self._num_examples == 0:
-            raise NotComputableError('FractionalBias must have at least one example before it can be computed')
+            raise NotComputableError('FractionalAbsoluteError must have at least one example before it can be computed')
         return self._sum_of_errors / self._num_examples
--- tests/ignite/contrib/metrics/regression/test_wave_hedges_distance.py
+++ tests/ignite/contrib/metrics/regression/test_wave_hedges_distance.py
@@ -2,18 +2,11 @@ import torch
 import numpy as np
 import pytest
 
-from ignite.exceptions import NotComputableError
-from ignite.contrib.metrics.regression import MeanError
-
-
-def test_zero_div():
-    m = MeanError()
-    with pytest.raises(NotComputableError):
-        m.compute()
+from ignite.contrib.metrics.regression import WaveHedgesDistance
 
 
 def test_wrong_input_shapes():
-    m = MeanError()
+    m = WaveHedgesDistance()
 
     with pytest.raises(ValueError):
         m.update((torch.rand(4, 1, 2),
@@ -32,35 +25,27 @@ def test_wrong_input_shapes():
                   torch.rand(4, 1, 2)))
 
 
-def test_mean_error():
+def test_compute():
     a = np.random.randn(4)
     b = np.random.randn(4)
     c = np.random.randn(4)
     d = np.random.randn(4)
     ground_truth = np.random.randn(4)
 
-    m = MeanError()
+    m = WaveHedgesDistance()
 
     m.update((torch.from_numpy(a), torch.from_numpy(ground_truth)))
-    np_sum = (ground_truth - a).sum()
-    np_len = len(a)
-    np_ans = np_sum / np_len
-    assert m.compute() == pytest.approx(np_ans)
+    np_sum = (np.abs(ground_truth - a) / np.maximum.reduce([a, ground_truth])).sum()
+    assert m.compute() == pytest.approx(np_sum)
 
     m.update((torch.from_numpy(b), torch.from_numpy(ground_truth)))
-    np_sum += (ground_truth - b).sum()
-    np_len += len(b)
-    np_ans = np_sum / np_len
-    assert m.compute() == pytest.approx(np_ans)
+    np_sum += (np.abs(ground_truth - b) / np.maximum.reduce([b, ground_truth])).sum()
+    assert m.compute() == pytest.approx(np_sum)
 
     m.update((torch.from_numpy(c), torch.from_numpy(ground_truth)))
-    np_sum += (ground_truth - c).sum()
-    np_len += len(c)
-    np_ans = np_sum / np_len
-    assert m.compute() == pytest.approx(np_ans)
+    np_sum += (np.abs(ground_truth - c) / np.maximum.reduce([c, ground_truth])).sum()
+    assert m.compute() == pytest.approx(np_sum)
 
     m.update((torch.from_numpy(d), torch.from_numpy(ground_truth)))
-    np_sum += (ground_truth - d).sum()
-    np_len += len(d)
-    np_ans = np_sum / np_len
-    assert m.compute() == pytest.approx(np_ans)
+    np_sum += (np.abs(ground_truth - d) / np.maximum.reduce([d, ground_truth])).sum()
+    assert m.compute() == pytest.approx(np_sum)
