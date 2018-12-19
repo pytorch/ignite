1eb5142d53821706a6f814b40f0cbe473806a514
Anmol Joshi <anmolsjoshi@gmail.com>
2018-12-19 06:19:04 -0800

    Removed super callouts in Binary and Categorical Accuracy

--- ignite/metrics/binary_accuracy.py
+++ ignite/metrics/binary_accuracy.py
@@ -9,7 +9,6 @@ class BinaryAccuracy(Accuracy):
     Note: This metric is deprecated in favor of Accuracy.
     """
     def __init__(self, *args, **kwargs):
-        super(BinaryAccuracy, self).__init__(*args, **kwargs)
         warnings.warn("The use of ignite.metrics.BinaryAccuracy is deprecated, it will be "
                       "removed in 0.2.0. Please use ignite.metrics.Accuracy instead.", DeprecationWarning)
         super(Accuracy, self).__init__(*args, **kwargs)
--- ignite/metrics/categorical_accuracy.py
+++ ignite/metrics/categorical_accuracy.py
@@ -9,7 +9,6 @@ class CategoricalAccuracy(Accuracy):
     Note: This metric is deprecated in favor of Accuracy.
     """
     def __init__(self, *args, **kwargs):
-        super(CategoricalAccuracy, self).__init__(*args, **kwargs)
         warnings.warn("The use of ignite.metrics.CategoricalAccuracy is deprecated, it will be "
                       "removed in 0.2.0. Please use ignite.metrics.Accuracy instead.", DeprecationWarning)
         super(Accuracy, self).__init__(*args, **kwargs)
