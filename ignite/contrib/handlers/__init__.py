71ffff82b4f73a13d9b4bd0f6c23651c0c5527d4
vfdev-5 <vfdev.5@gmail.com>
2019-01-06 13:06:07 +0100

    Improved docs

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -1,5 +1,5 @@
 
-from ignite.contrib.handlers.param_scheduler import ParamScheduler, CyclicalScheduler, \
-    LinearCyclicalScheduler, CosineAnnealingScheduler
+from ignite.contrib.handlers.param_scheduler import LinearCyclicalScheduler, CosineAnnealingScheduler, \
+    ConcatScheduler, LRScheduler
 
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
