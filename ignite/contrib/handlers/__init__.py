7c2199b3640ad7067bfacc6eb79ebc8952e41269
Marc Ferradou <zippeurfou@gmail.com>
2019-02-21 11:23:06 -0500

    Adding Exponential Annealing to contrib handlers

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -1,5 +1,6 @@
 
 from ignite.contrib.handlers.param_scheduler import LinearCyclicalScheduler, CosineAnnealingScheduler, \
-    ConcatScheduler, LRScheduler, create_lr_scheduler_with_warmup, PiecewiseLinear
+    ConcatScheduler, LRScheduler, create_lr_scheduler_with_warmup, PiecewiseLinear, \
+    ExponentialAnnealingScheduler
 
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
