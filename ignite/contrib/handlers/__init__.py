4dfd75ced07f4b45f0b6de6e247353aa317d7e93
vfdev <vfdev.5@gmail.com>
2019-03-03 02:28:16 +0100

    Added custom periodic events creator and handler (#351)
    * Added custom epoch length handler
    
    * Implemented and tested CustomPeriodicEvent instead of custom epoch
    
    * fix typo in the docstring

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -3,3 +3,4 @@ from ignite.contrib.handlers.param_scheduler import LinearCyclicalScheduler, Cos
     ConcatScheduler, LRScheduler, create_lr_scheduler_with_warmup, PiecewiseLinear
 
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
+from ignite.contrib.handlers.custom_events import CustomPeriodicEvent
