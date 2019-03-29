a3c6df02b257bfeb1c9411fe6135097c854ce0bd
Hicham <hicham@randrianarivo.net>
2019-03-29 16:12:21 +0100

    finish trai and eval

--- ignite/contrib/engines/__init__.py
+++ ignite/contrib/engines/__init__.py
@@ -1,2 +1,3 @@
 from ignite.contrib.engines.tbptt import create_supervised_tbptt_trainer
 from ignite.contrib.engines.tbptt import Tbptt_Events
+from ignite.contrib.engines.dali import create_supervised_dali_trainer, create_supervised_dali_evaluator
