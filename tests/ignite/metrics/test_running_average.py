45da8c9a4243597d09f9dc6186795435b8d115b1
Evpok Padding <evpok.padding@gmail.com>
2019-04-12 09:19:36 +0200

    fix lint

--- tests/ignite/metrics/test_running_average.py
+++ tests/ignite/metrics/test_running_average.py
@@ -108,15 +108,15 @@ def test_integration():
 
 
 def test_epoch_unbound():
-    
+
     n_iters = 10
     n_epochs = 3
     batch_size = 10
     n_classes = 10
     data = list(range(n_iters))
-    loss_values = iter(range(n_epochs*n_iters))
-    y_true_batch_values = iter(np.random.randint(0, n_classes, size=(n_epochs*n_iters, batch_size)))
-    y_pred_batch_values = iter(np.random.rand(n_epochs*n_iters, batch_size, n_classes))
+    loss_values = iter(range(n_epochs * n_iters))
+    y_true_batch_values = iter(np.random.randint(0, n_classes, size=(n_epochs * n_iters, batch_size)))
+    y_pred_batch_values = iter(np.random.rand(n_epochs * n_iters, batch_size, n_classes))
 
     def update_fn(engine, batch):
         loss_value = next(loss_values)
@@ -212,4 +212,3 @@ def test_multiple_attach():
 
     data = list(range(n_iters))
     trainer.run(data)
-
