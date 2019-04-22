39959710d169b5a10aa893832de6d6ddfb008a13
Anmol Joshi <anmolsjoshi@gmail.com>
2019-04-21 22:26:38 -0700

    R-Squared Metric (#496)
    * R Squared Implementation with tests
    
    * fixed flake8 error
    
    * Update docstring and rst
    
    * Fixed implemenation of R2 to not store entire history of training
    
    * Fix flake8

---
+++ ignite/contrib/metrics/regression/r2_score.py
@@ -0,0 +1,39 @@
+from __future__ import division
+
+import torch
+
+from ignite.exceptions import NotComputableError
+from ignite.contrib.metrics.regression._base import _BaseRegression
+
+
+class R2Score(_BaseRegression):
+    r"""
+        Calculates the R-Squared, the
+        `coefficient of determination <https://en.wikipedia.org/wiki/Coefficient_of_determination>`_:
+
+        :math:`R^2 = 1 - \frac{\sum_{j=1}^n(A_j - P_j)^2}{\sum_{j=1}^n(A_j - \bar{A})^2}`,
+
+        where :math:`A_j` is the ground truth, :math:`P_j` is the predicted value and
+        :math:`\bar{A}` is the mean of the ground truth.
+
+        - `update` must receive output of the form `(y_pred, y)`.
+        - `y` and `y_pred` must be of same shape `(N, )` or `(N, 1)` and of type `float32`.
+    """
+    def reset(self):
+        self._num_examples = 0
+        self._sum_of_errors = 0
+        self._y_sq_sum = 0
+        self._y_sum = 0
+
+    def _update(self, output):
+        y_pred, y = output
+        self._num_examples += y.shape[0]
+        self._sum_of_errors += torch.sum(torch.pow(y_pred - y, 2)).item()
+
+        self._y_sum += torch.sum(y).item()
+        self._y_sq_sum += torch.sum(torch.pow(y, 2)).item()
+
+    def compute(self):
+        if self._num_examples == 0:
+            raise NotComputableError('R2Score must have at least one example before it can be computed.')
+        return 1 - self._sum_of_errors / (self._y_sq_sum - (self._y_sum ** 2) / self._num_examples)
