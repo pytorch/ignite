d1d69ce4ed352484b84f0602cb0bf5208fb2b9f4
vfdev-5 <vfdev.5@gmail.com>
2019-05-13 16:52:04 +0200

    Added accumulative single variable metrics

--- ignite/metrics/__init__.py
+++ ignite/metrics/__init__.py
@@ -12,3 +12,4 @@ from ignite.metrics.top_k_categorical_accuracy import TopKCategoricalAccuracy
 from ignite.metrics.running_average import RunningAverage
 from ignite.metrics.metrics_lambda import MetricsLambda
 from ignite.metrics.confusion_matrix import ConfusionMatrix, IoU, mIoU
+from ignite.metrics.accumulation import VariableAccumulation, Average, GeometricAverage
