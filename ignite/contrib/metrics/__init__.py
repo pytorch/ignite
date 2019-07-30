4972e5e333b605b72067c27c5f210f6aef781234
vfdev-5 <vfdev.5@gmail.com>
2019-07-30 15:54:40 +0200

    Fixes #566

--- ignite/contrib/metrics/__init__.py
+++ ignite/contrib/metrics/__init__.py
@@ -1,3 +1,4 @@
 from ignite.contrib.metrics.average_precision import AveragePrecision
 from ignite.contrib.metrics.roc_auc import ROC_AUC
 import ignite.contrib.metrics.regression
+from ignite.contrib.metrics.gpu_memory import GpuMemory
---
+++ tests/ignite/contrib/metrics/test_gpu_memory.py
@@ -0,0 +1,32 @@
+import torch
+
+from ignite.contrib.metrics import GpuMemory
+
+import pytest
+
+
+@pytest.fixture
+def no_site_packages():
+    import pynvml
+    import sys
+    assert 'pynvml' in sys.modules
+    pynvml_module = sys.modules['pynvml']
+    del sys.modules['pynvml']
+    prev_path = list(sys.path)
+    sys.path = [p for p in sys.path if "site-packages" not in p]
+    yield "no_site_packages"
+    sys.path = prev_path
+    sys.modules['pynvml'] = pynvml_module
+
+
+def test_no_pynvml_package(no_site_packages):
+
+    with pytest.raises(RuntimeError, match="This contrib module requires pynvml to be installed."):
+        GpuMemory()
+
+
+@pytest.mark.skipif(torch.cuda.is_available(), reason="Skip if has GPU")
+def test_no_gpu():
+
+    with pytest.raises(RuntimeError, match="This contrib module requires available GPU"):
+        GpuMemory()
