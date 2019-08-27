6a6d721eb48fc057b4205408ff1bba06da092c48
Itamar Wilf <wilfitamar@gmail.com>
2019-08-27 15:27:17 +0300

    WIP - LRFinder handler implementation

---
+++ ignite/contrib/handlers/lr_finder.py
@@ -0,0 +1,316 @@
+# coding: utf-8
+
+from ignite.engine import Events, Engine
+from ignite.contrib.handlers import LRScheduler
+from torch.optim.lr_scheduler import _LRScheduler
+import numpy as np
+import torch
+import copy
+import os
+import logging
+import weakref
+import warnings
+
+
+class LRFinder(object):
+    """
+    Learning rate finder handler for supervised trainers.
+
+    While attached, the handler increases the learning rate in between two
+    boundaries in a linear or exponential manner. It provides valuable
+    information on how well the network can be trained over a range of learning
+    rates and what is the optimal learning rate.
+
+    Args:
+        model (`torch.nn.Module`): the model to train.
+        optimizer (`torch.optim.Optimizer`): the optimizer to use, the defined
+        optimizer learning rate is assumed to be the lower boundary of the range
+        test.
+        output_transform (callable, optional): function that transform the
+            engine's state.output after each iteration. It must return the loss
+            of that iteration
+        step_mode (str): "exp" or "linear", which way should the lr be increased
+            from optimizer's initial lr to end_lr
+        smooth_f (float): loss smoothing factor in range [0, 1)
+        diverge_th (float): Used for stopping the search when
+            `current loss > diverge_th * best_loss`
+        memory_cache (bool): if this flag is set to True, `state_dict` of model
+            and optimizer will be cached in memory. Otherwise, they will be
+            saved to files under the `cache_dir`.
+        cache_dir (str): path for storing temporary files. If no path is
+            specified, system-wide temporary directory is used. Notice that this
+             parameter will be ignored if `memory_cache` is True.
+
+    References:
+        Cyclical Learning Rates for Training Neural Networks:
+        https://arxiv.org/abs/1506.01186
+
+        fastai/lr_find: https://github.com/fastai/fastai
+    """
+
+    def __init__(self, model, optimizer, output_transform=lambda output: output, end_lr=10, step_mode="exp",
+                 smooth_f=0.05, diverge_th=5, memory_cache=True, cache_dir=None):
+
+        if smooth_f < 0 or smooth_f >= 1:
+            raise ValueError("smooth_f is outside the range [0, 1]")
+        if step_mode not in ["exp", "linear"]:
+            raise ValueError("expected one of (exp, linear), got {}".format(step_mode))
+
+        self.model = model
+        self.optimizer = optimizer
+        self.output_transform = output_transform
+        self.end_lr = end_lr
+        self.step_mode = step_mode
+        self.smooth_f = smooth_f
+        self.diverge_th = diverge_th
+        self.memory_cache = memory_cache
+        self.cache_dir = cache_dir
+
+        self._engine = None
+        self.history = None
+        self.best_loss = None
+        self._lr_schedule = None
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.addHandler(logging.NullHandler())
+
+    def _run(self, engine):
+        engine.state.state_cache = _StateCacher(self.memory_cache, cache_dir=self.cache_dir)
+        engine.state.state_cache.store("model", self.model.state_dict())
+        engine.state.state_cache.store("optimizer", self.optimizer.state_dict())
+
+        self.history = {"lr": [], "loss": []}
+        self.best_loss = None
+
+        # attach loss and lr logging
+        if not engine.has_event_handler(self._log_lr_and_loss):
+            engine.add_event_handler(Events.ITERATION_COMPLETED, self._log_lr_and_loss)
+
+        # attach LRScheduler to engine. can be done only after engine.run was called because of num_iter
+        num_iter = engine.state.max_epochs * len(engine.state.dataloader)
+        self.logger.debug("Running LR finder for {} iterations".format(num_iter))
+        # Initialize the proper learning rate policy
+        if self.step_mode.lower() == "exp":
+            self._lr_schedule = LRScheduler(ExponentialLR(self.optimizer, self.end_lr, num_iter))
+        else:
+            self._lr_schedule = LRScheduler(LinearLR(self.optimizer, self.end_lr, num_iter))
+        if not engine.has_event_handler(self._lr_schedule):
+            engine.add_event_handler(Events.ITERATION_COMPLETED, self._lr_schedule, num_iter)
+
+    # Reset model and optimizer, delete state cache and remove handlers
+    def _reset(self, engine):
+        self.model.load_state_dict(engine.state.state_cache.retrieve("model"))
+        self.optimizer.load_state_dict(engine.state.state_cache.retrieve("optimizer"))
+        del engine.state.state_cache
+        self.logger.debug("Completed LR finder run, resets model & optimizer")
+
+        # Clean up; remove event handlers added during run
+        engine.remove_event_handler(self._lr_schedule, Events.ITERATION_COMPLETED)
+        engine.remove_event_handler(self._log_lr_and_loss, Events.ITERATION_COMPLETED)
+
+    def _log_lr_and_loss(self, engine):
+        output = engine.state.output
+        loss = self.output_transform(output)
+        lr = self._lr_schedule.lr_scheduler.get_lr()[0]
+        self.history["lr"].append(lr)
+        if engine.state.iteration == 1:
+            self.best_loss = loss
+        else:
+            if self.smooth_f > 0:
+                loss = self.smooth_f * loss + (1 - self.smooth_f) * self.history["loss"][-1]
+            if loss < self.best_loss:
+                self.best_loss = loss
+        self.history["loss"].append(loss)
+
+        # Check if the loss has diverged; if it has, stop the trainer
+        if self.history["loss"][-1] > self.diverge_th * self.best_loss:
+            engine.terminate()
+            self.logger.info("Stopping early, the loss has diverged")
+
+    def _warning(self, engine):
+        if not engine.should_terminate:
+            warnings.warn("Run completed without loss diverging, increase end_lr or look at lr_finder.plot()")
+
+    def attach(self, engine: Engine):
+        if self._engine:
+            raise AlreadyAttachedError("This LRFinder is already attached. create a new one or use lr_finder.detach()")
+        self._engine = weakref.ref(engine)
+        if not engine.has_event_handler(self._run):
+            engine.add_event_handler(Events.STARTED, self._run)
+        if not engine.has_event_handler(self._warning):
+            engine.add_event_handler(Events.COMPLETED, self._warning)
+        if not engine.has_event_handler(self._reset):
+            engine.add_event_handler(Events.COMPLETED, self._reset)
+
+    def detach(self):
+        if self._engine:
+            engine = self._engine()
+            self._engine = None
+            engine.remove_event_handler(self._run, Events.STARTED)
+            engine.remove_event_handler(self._warning, Events.COMPLETED)
+            engine.remove_event_handler(self._reset, Events.COMPLETED)
+        else:
+            warnings.warn("This LRFinder isn't attached, this action has no effect")
+
+    def __del__(self):
+        self.detach()
+
+    def get_results(self):
+        """
+        Returns: dictionary with loss and lr logs fromm the previous run
+        """
+        return self.history
+
+    def plot(self, skip_start=10, skip_end=5, log_lr=True):
+        """Plots the learning rate range test.
+
+        Args:
+            skip_start (int, optional): number of batches to trim from the start.
+                Default: 10.
+            skip_end (int, optional): number of batches to trim from the start.
+                Default: 5.
+            log_lr (bool, optional): True to plot the learning rate in a logarithmic
+                scale; otherwise, plotted in a linear scale. Default: True.
+        """
+
+        if skip_start < 0:
+            raise ValueError("skip_start cannot be negative")
+        if skip_end < 0:
+            raise ValueError("skip_end cannot be negative")
+
+        # Get the data to plot from the history dictionary. Also, handle skip_end=0
+        # properly so the behaviour is the expected
+        try:
+            import matplotlib.pyplot as plt
+
+            lrs = self.history["lr"]
+            losses = self.history["loss"]
+            if skip_end == 0:
+                lrs = lrs[skip_start:]
+                losses = losses[skip_start:]
+            else:
+                lrs = lrs[skip_start:-skip_end]
+                losses = losses[skip_start:-skip_end]
+
+            # Plot loss as a function of the learning rate
+            plt.plot(lrs, losses)
+            if log_lr:
+                plt.xscale("log")
+            plt.xlabel("Learning rate")
+            plt.ylabel("Loss")
+            plt.show()
+
+        except ModuleNotFoundError:
+            self.logger.warning("matplotlib not found, can't plot result")
+
+    def lr_suggestion(self):
+        """
+        Returns: learning rate at the minimum numerical gradient
+        """
+        loss = self.history["loss"]
+        grads = [loss[i] - loss[i - 1] for i in range(1, len(loss))]
+        min_grad_idx = np.argmin(grads) + 1
+        return self.history["lr"][int(min_grad_idx)]
+
+
+class AlreadyAttachedError(Exception):
+    """
+    Exception class to raise if trying to attach an already attached LRFinder.
+    """
+
+
+class LinearLR(_LRScheduler):
+    """Linearly increases the learning rate between two boundaries over a number of
+    iterations.
+
+    Arguments:
+        optimizer (torch.optim.Optimizer): wrapped optimizer.
+        end_lr (float, optional): the initial learning rate which is the lower
+            boundary of the test. Default: 10.
+        num_iter (int, optional): the number of iterations over which the test
+            occurs. Default: 100.
+        last_epoch (int): the index of last epoch. Default: -1.
+
+    """
+
+    def __init__(self, optimizer, end_lr, num_iter, last_epoch=-1):
+        self.end_lr = end_lr
+        self.num_iter = num_iter
+        super(LinearLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        curr_iter = self.last_epoch + 1
+        r = curr_iter / self.num_iter
+        return [base_lr + r * (self.end_lr - base_lr) for base_lr in self.base_lrs]
+
+
+class ExponentialLR(_LRScheduler):
+    """Exponentially increases the learning rate between two boundaries over a number of
+    iterations.
+
+    Arguments:
+        optimizer (torch.optim.Optimizer): wrapped optimizer.
+        end_lr (float, optional): the initial learning rate which is the lower
+            boundary of the test. Default: 10.
+        num_iter (int, optional): the number of iterations over which the test
+            occurs. Default: 100.
+        last_epoch (int): the index of last epoch. Default: -1.
+
+    """
+
+    def __init__(self, optimizer, end_lr, num_iter, last_epoch=-1):
+        self.end_lr = end_lr
+        self.num_iter = num_iter
+        super(ExponentialLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        curr_iter = self.last_epoch + 1
+        r = curr_iter / self.num_iter
+        return [base_lr * (self.end_lr / base_lr) ** r for base_lr in self.base_lrs]
+
+
+class _StateCacher(object):
+
+    def __init__(self, in_memory, cache_dir=None):
+        self.in_memory = in_memory
+        self.cache_dir = cache_dir
+
+        if self.cache_dir is None:
+            import tempfile
+            self.cache_dir = tempfile.gettempdir()
+        else:
+            if not os.path.isdir(self.cache_dir):
+                raise ValueError('Given `cache_dir` is not a valid directory.')
+
+        self.cached = {}
+
+    def store(self, key, state_dict):
+        if self.in_memory:
+            self.cached.update({key: copy.deepcopy(state_dict)})
+        else:
+            fn = os.path.join(self.cache_dir, 'state_{}_{}.pt'.format(key, id(self)))
+            self.cached.update({key: fn})
+            torch.save(state_dict, fn)
+
+    def retrieve(self, key):
+        if key not in self.cached:
+            raise KeyError('Target {} was not cached.'.format(key))
+
+        if self.in_memory:
+            return self.cached.get(key)
+        else:
+            fn = self.cached.get(key)
+            if not os.path.exists(fn):
+                raise RuntimeError('Failed to load state in {}. File does not exist anymore.'.format(fn))
+            state_dict = torch.load(fn, map_location=lambda storage, location: storage)
+            return state_dict
+
+    def __del__(self):
+        """Check whether there are unused cached files existing in `cache_dir` before
+        this instance being destroyed."""
+        if self.in_memory:
+            return
+
+        for k in self.cached:
+            if os.path.exists(self.cached[k]):
+                os.remove(self.cached[k])
