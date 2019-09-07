f29a059aaec4187cad7add29f676985973b72236
Alex Lampe <19519104+Bibonaut@users.noreply.github.com>
2019-09-07 10:35:54 +0200

    update comment

--- ignite/contrib/handlers/base_logger.py
+++ ignite/contrib/handlers/base_logger.py
@@ -142,7 +142,7 @@ class BaseWeightsScalarHandler(BaseHandler):
         def _is_0D_tensor(t):
             return isinstance(t, torch.Tensor) and t.ndimension() == 0
 
-        # Test reduction function on a random tensor
+        # Test reduction function on a tensor
         o = reduction(torch.ones(4, 2))
         if not (isinstance(o, numbers.Number) or _is_0D_tensor(o)):
             raise ValueError("Output of the reduction function should be a scalar, but got {}".format(type(o)))
