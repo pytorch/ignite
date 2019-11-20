75c471154815380a224b1b04823f4d418113bbcf
vfdev-5 <vfdev.5@gmail.com>
2019-11-20 15:15:18 +0100

    [WIP] Added distrib tests for EarlyStopping

--- tests/ignite/handlers/test_early_stopping.py
+++ tests/ignite/handlers/test_early_stopping.py
@@ -1,8 +1,10 @@
-
-import pytest
+import os
+import torch
 
 from ignite.engine import Engine, Events
 from ignite.handlers import EarlyStopping
+
+import pytest
 
 
 def test_args_validation():
@@ -12,14 +14,14 @@ def test_args_validation():
 
     trainer = Engine(update_fn)
 
-    with pytest.raises(ValueError):
-        h = EarlyStopping(patience=-1, score_function=lambda engine: 0, trainer=trainer)
+    with pytest.raises(ValueError, match=r"Argument patience should be positive integer."):
+        EarlyStopping(patience=-1, score_function=lambda engine: 0, trainer=trainer)
 
-    with pytest.raises(TypeError):
-        h = EarlyStopping(patience=2, score_function=12345, trainer=trainer)
+    with pytest.raises(TypeError, match=r"Argument score_function should be a function."):
+        EarlyStopping(patience=2, score_function=12345, trainer=trainer)
 
-    with pytest.raises(TypeError):
-        h = EarlyStopping(patience=2, score_function=lambda engine: 0, trainer=None)
+    with pytest.raises(TypeError, match=r"Argument trainer should be an instance of Engine."):
+        EarlyStopping(patience=2, score_function=lambda engine: 0, trainer=None)
 
 
 def test_simple_early_stopping():
@@ -113,6 +115,7 @@ def test_with_engine_early_stopping():
     evaluator.add_event_handler(Events.COMPLETED, early_stopping)
     trainer.run([0], max_epochs=10)
     assert n_epochs_counter.count == 7
+    assert trainer.state.epoch == 7
 
 
 def test_with_engine_early_stopping_on_plateau():
@@ -141,6 +144,7 @@ def test_with_engine_early_stopping_on_plateau():
     evaluator.add_event_handler(Events.COMPLETED, early_stopping)
     trainer.run([0], max_epochs=10)
     assert n_epochs_counter.count == 5
+    assert trainer.state.epoch == 5
 
 
 def test_with_engine_no_early_stopping():
@@ -171,3 +175,71 @@ def test_with_engine_no_early_stopping():
     evaluator.add_event_handler(Events.COMPLETED, early_stopping)
     trainer.run([0], max_epochs=10)
     assert n_epochs_counter.count == 10
+    assert trainer.state.epoch == 10
+
+
+def _test_distrib_with_engine_early_stopping(device):
+
+    import torch.distributed as dist
+    from ignite.engine import Engine
+
+    torch.manual_seed(12)
+
+    class Counter(object):
+        def __init__(self, count=0):
+            self.count = count
+
+    n_epochs_counter = Counter()
+
+    scores = torch.tensor([1.0, 0.8, 1.2, 1.5, 0.9, 1.0, 0.99, 1.1, 0.9], requires_grad=False).to(device)
+
+    def score_function(engine):
+        i = trainer.state.epoch - 1
+        v = scores[i]
+        dist.all_reduce(v)
+        v /= dist.get_world_size()
+        return v.item()
+
+    def update_fn(engine, batch):
+        pass
+
+    trainer = Engine(update_fn)
+    evaluator = Engine(update_fn)
+    early_stopping = EarlyStopping(patience=3, score_function=score_function, trainer=trainer)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def evaluation(engine):
+        evaluator.run([0])
+        n_epochs_counter.count += 1
+
+    evaluator.add_event_handler(Events.COMPLETED, early_stopping)
+    trainer.run([0], max_epochs=10)
+    assert trainer.state.epoch == 7
+    assert n_epochs_counter.count == 7
+
+
+@pytest.mark.distributed
+@pytest.mark.skipif(torch.cuda.device_count() < 1, reason="Skip if no GPU")
+def test_distrib_gpu(local_rank, distributed_context_single_node_nccl):
+    device = "cuda:{}".format(local_rank)
+    _test_distrib_with_engine_early_stopping(device)
+
+
+@pytest.mark.distributed
+def test_distrib_cpu(local_rank, distributed_context_single_node_gloo):
+    device = "cpu"
+    _test_distrib_with_engine_early_stopping(device)
+
+
+@pytest.mark.multinode_distributed
+@pytest.mark.skipif('MULTINODE_DISTRIB' not in os.environ, reason="Skip if not multi-node distributed")
+def test_multinode_distrib_cpu(distributed_context_multi_node_gloo):
+    device = "cpu"
+    _test_distrib_with_engine_early_stopping(device)
+
+
+@pytest.mark.multinode_distributed
+@pytest.mark.skipif('GPU_MULTINODE_DISTRIB' not in os.environ, reason="Skip if not multi-node distributed")
+def test_multinode_distrib_gpu(distributed_context_multi_node_nccl):
+    device = "cuda:{}".format(distributed_context_multi_node_nccl['local_rank'])
+    _test_distrib_with_engine_early_stopping(device)
