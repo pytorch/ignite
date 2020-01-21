f3b67344dea8b50efa1d1a79e2e659ab21093d72
Ankit Jha <45779665+PyExtreme@users.noreply.github.com>
2020-01-22 02:44:40 +0530

    Add Dice Coefficient (#680)
    * Add Dice Coefficient
    
    * Add Dice Coefficient
    
    * add test: dice_coefficient
    
    * fix minor issues
    
    * fix minor issues
    
    * add ignore_index: dice_coefficient
    
    * fix minor issues: dice_coefficient
    
    * add  logic: ignore_index
    
    * add  logic: ignore_index
    
    * Updated DiceCoefficient code
    
    * Updated docs
    
    Co-authored-by: vfdev <vfdev.5@gmail.com>

--- ignite/metrics/__init__.py
+++ ignite/metrics/__init__.py
@@ -11,6 +11,6 @@ from ignite.metrics.root_mean_squared_error import RootMeanSquaredError
 from ignite.metrics.top_k_categorical_accuracy import TopKCategoricalAccuracy
 from ignite.metrics.running_average import RunningAverage
 from ignite.metrics.metrics_lambda import MetricsLambda
-from ignite.metrics.confusion_matrix import ConfusionMatrix, IoU, mIoU
+from ignite.metrics.confusion_matrix import ConfusionMatrix, IoU, mIoU, DiceCoefficient
 from ignite.metrics.accumulation import VariableAccumulation, Average, GeometricAverage
 from ignite.metrics.fbeta import Fbeta
