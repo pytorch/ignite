85f2f784c70dde01ce7bb9a4f50ad48fb0751041
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-02-01 16:11:22 -0500

    add usage in docstring.

--- ignite/contrib/metrics/frequency.py
+++ ignite/contrib/metrics/frequency.py
@@ -3,6 +3,22 @@ from ignite.handlers.timing import Timer
 from ignite.metrics.metric import sync_all_reduce, reinit__is_reduced
 
 class FrequencyMetric(Metric):
+    """Provides metrics for the number of examples processed per second. 
+
+    Examples:
+
+        .. code-block:: python
+
+            # Compute number of tokens processed
+            wps_metric = FrequencyMetric(output_transformer=lambda x: x['ntokens'])
+            average_wps_metric = RunningAverage(wps_metric, alpha=1.0)
+            average_wps_metric.attach(trainer, name='wps') 
+            # Logging with TQDM
+            ProgressBar(persist=True).attach(trainer, metric_names=['wps'])
+            # Progress bar will looks like
+            # Epoch [2/10]: [12/24]  50%|█████      , wps=400 [00:17<1:23]
+    """
+
     def __init__(self, output_transform=lambda x: x, device=None):
         self._timer = None
         self._n = None
