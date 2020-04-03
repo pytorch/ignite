40e2e93005837093b7ae3e31ddbd4cb69ef5ce89
Gabriele Picco <gabriele.picco@ibm.com>
2020-04-03 15:32:17 +0100

    Add Precision Recall Curve and Roc Curve to the contrib.metrics module

--- ignite/contrib/metrics/__init__.py
+++ ignite/contrib/metrics/__init__.py
@@ -1,4 +1,6 @@
 from ignite.contrib.metrics.average_precision import AveragePrecision
 from ignite.contrib.metrics.roc_auc import ROC_AUC
+from ignite.contrib.metrics.roc_curve import RocCurve
+from ignite.contrib.metrics.precision_recall_curve import PrecisionRecallCurve
 import ignite.contrib.metrics.regression
 from ignite.contrib.metrics.gpu_info import GpuInfo
