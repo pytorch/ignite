901d52b01b3f284af2bb55fa2c38b8eec5db286e
vfdev <vfdev.5@gmail.com>
2020-04-04 23:00:14 +0200

    Update mnist_save_resume_engine.py

--- examples/mnist/mnist_save_resume_engine.py
+++ examples/mnist/mnist_save_resume_engine.py
@@ -15,7 +15,14 @@ from tqdm import tqdm
 try:
     from tensorboardX import SummaryWriter
 except ImportError:
-    raise RuntimeError("No tensorboardX package is found. Please install with the command: \npip install tensorboardX")
+    try:
+        from torch.utils.tensorboard import SummaryWriter
+    except ImportError:
+        raise RuntimeError(
+            "This module requires either tensorboardX or torch >= 1.2.0. "
+            "You may install tensorboardX with command: \n pip install tensorboardX \n"
+            "or upgrade PyTorch using your package manager of choice (pip or conda)."
+        )
 
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Accuracy, Loss
