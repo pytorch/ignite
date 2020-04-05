14e255b74487941f2e5a30e387f9d7f54c7f33b8
Sylvain Desroziers <sylvain.desroziers@gmail.com>
2020-04-05 21:38:43 +0200

    add GPUs test

--- tests/ignite/handlers/test_distributed.py
+++ tests/ignite/handlers/test_distributed.py
@@ -75,3 +75,10 @@ def test_distrib_cpu(distributed_context_single_node_gloo):
 def test_multinode_distrib_cpu(distributed_context_multi_node_gloo):
     _test_distrib_one_rank_only()
     _test_distrib_one_rank_only_with_engine()
+
+
+@pytest.mark.multinode_distributed
+@pytest.mark.skipif("GPU_MULTINODE_DISTRIB" not in os.environ, reason="Skip if not multi-node distributed")
+def test_multinode_distrib_gpu(distributed_context_multi_node_nccl):
+    _test_distrib_one_rank_only()
+    _test_distrib_one_rank_only_with_engine()
