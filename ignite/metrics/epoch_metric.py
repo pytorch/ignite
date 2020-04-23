1267b485f70a77146eaf88a6d278b19498df422f
hoangmit <hoangmit@users.noreply.github.com>
2020-04-23 13:19:21 -0400

    Try to match previous dtype

--- ignite/metrics/epoch_metric.py
+++ ignite/metrics/epoch_metric.py
@@ -12,21 +12,14 @@ class EpochMetric(Metric):
     """Class for metrics that should be computed on the entire output history of a model.
     Model's output and targets are restricted to be of shape `(batch_size, n_classes)`. Output
     datatype should be `float32`. Target datatype should be `long`.
-
     .. warning::
-
         Current implementation stores all input data (output and target) in as tensors before computing a metric.
         This can potentially lead to a memory error if the input data is larger than available RAM.
-
     .. warning::
-
         Current implementation does not work with distributed computations. Results are not gather across all devices
         and computed results are valid for a single device only.
-
     - `update` must receive output of the form `(y_pred, y)` or `{'y_pred': y_pred, 'y': y}`.
-
     If target shape is `(batch_size, n_classes)` and `n_classes > 1` than it should be binary: e.g. `[[0, 1, 0, 1], ]`.
-
     Args:
         compute_fn (callable): a callable with the signature (`torch.tensor`, `torch.tensor`) takes as the input
             `predictions` and `targets` and returns a scalar.
@@ -34,7 +27,6 @@ class EpochMetric(Metric):
             :class:`~ignite.engine.Engine`'s `process_function`'s output into the
             form expected by the metric. This can be useful if, for example, you have a multi-output model and
             you want to compute the metric with respect to one of the outputs.
-
     """
 
     def __init__(self, compute_fn: Callable, output_transform: Callable = lambda x: x):
@@ -68,13 +60,13 @@ class EpochMetric(Metric):
         if y.ndimension() == 2 and y.shape[1] == 1:
             y = y.squeeze(dim=-1)
 
-        self._predictions.append(y_pred.detach())
-        self._targets.append(y.detach())
+        self._predictions.append(y_pred.detach().to(dtype=torch.float32, device="cpu"))
+        self._targets.append(y.detach().to(dtype=torch.long, device="cpu"))
 
         # Check once the signature and execution of compute_fn
         if len(self._predictions) == 1:
             try:
-                self.compute_fn(y_pred, y)
+                self.compute_fn(self._predictions[0], self._targets[0])
             except Exception as e:
                 warnings.warn("Probably, there can be a problem with `compute_fn`:\n {}.".format(e), EpochMetricWarning)
 
