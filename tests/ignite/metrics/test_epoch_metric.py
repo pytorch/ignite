1d03b1f21dbb754a68e5d84d452e8792638f0961
vfdev <vfdev.5@gmail.com>
2020-04-24 18:13:48 +0200

    Update test_epoch_metric.py

--- tests/ignite/metrics/test_epoch_metric.py
+++ tests/ignite/metrics/test_epoch_metric.py
@@ -44,7 +44,7 @@ def test_epoch_metric():
     output2 = (torch.rand(4, 3), torch.randint(0, 2, size=(4, 3), dtype=torch.long))
     em.update(output2)
 
-    assert all(t.device.type == "cpu" for t in (em._predictions + em.targets))
+    assert all([t.device.type == "cpu" for t in em._predictions + em.targets])
     assert torch.equal(em._predictions[0], output1[0])
     assert torch.equal(em._predictions[1], output2[0])
     assert torch.equal(em._targets[0], output1[1].to(torch.float32))
@@ -58,11 +58,11 @@ def test_epoch_metric():
     output2 = (torch.rand(4, 1), torch.randint(0, 2, size=(4, 1), dtype=torch.long))
     em.update(output2)
 
-    assert all(t.device.type == "cpu" for t in (em._predictions + em.targets))
+    assert all([t.device.type == "cpu" for t in em._predictions + em.targets])
     assert torch.equal(em._predictions[0], output1[0][:, 0])
     assert torch.equal(em._predictions[1], output2[0][:, 0])
-    assert torch.equal(em._targets[1], output1[1][:, 0].to(torch.float32))
-    assert torch.equal(em._targets[1], output2[1][:, 0].to(torch.float32))
+    assert torch.equal(em._targets[1], output1[1][:, 0])
+    assert torch.equal(em._targets[1], output2[1][:, 0])
     assert em.compute() == 0.0
 
 
