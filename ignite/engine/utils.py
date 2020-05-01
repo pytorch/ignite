d709f652a74037e2d8c9d2c08d92f19736181f8d
Sylvain Desroziers <sylvain.desroziers@gmail.com>
2020-05-02 01:56:22 +0200

    revert follow_wrapped

--- ignite/engine/utils.py
+++ ignite/engine/utils.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 
 def _check_signature(fn: Callable, fn_description: str, *args, **kwargs) -> None:
-    signature = inspect.signature(fn, follow_wrapped=False)
+    signature = inspect.signature(fn)
     try:  # try without engine
         signature.bind(*args, **kwargs)
     except TypeError as exc:
