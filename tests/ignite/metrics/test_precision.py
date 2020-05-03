351139f15a1acdd9c83c1925b93a6243622b5bb5
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-05-03 15:56:17 -0400

    fix XLA device retrieval issue by deferring until spawn.

--- tests/ignite/metrics/test_precision.py
+++ tests/ignite/metrics/test_precision.py
@@ -835,18 +835,28 @@ def _test_distrib_itegration_multilabel(rank, device, world_size):
     assert (pr_compute1 == pr_compute2).all()
 
 
+def _test_distrib_itegration_multiclass_xla(rank):
+    # Required because `xm.xla_device()` needs to be retrieved from a spawned context
+    import torch_xla.core.xla_model as xm
+
+    _test_distrib_itegration_multiclass(rank, xm.xla_device(), xrt_world_size)
+
+
+def _test_distrib_itegration_multilabel_xla(rank):
+    # Required because `xm.xla_device()` needs to be retrieved from a spawned context
+    import torch_xla.core.xla_model as xm
+
+    _test_distrib_itegration_multilabel(rank, xm.xla_device(), xrt_world_size)
+
+
 @pytest.mark.tpu
 @pytest.mark.distributed
 @pytest.mark.skipif(not on_xla_device or xrt_world_size < 1, reason="Skip if no TPU")
 def test_distrib_tpu(local_rank, distributed_context_single_node_xla):
-    import torch_xla.core.xla_model as xm
 
-    executor = distributed_context_single_node_xla
-    args = (xm.xla_device(), xrt_world_size)
-
-    with patch.object(sys, "exit", return_value=None):
-        executor(_test_distrib_itegration_multiclass, args)
-        executor(_test_distrib_itegration_multilabel, args)
+    args = ()
+    distributed_context_single_node_xla(_test_distrib_itegration_multiclass_xla, args)
+    distributed_context_single_node_xla(_test_distrib_itegration_multilabel_xla, args)
 
 
 @pytest.mark.distributed
