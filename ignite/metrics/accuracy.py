65bd888eaec165e49e3f032b3d6f1b34f13a250e
Oguz Serbetci <oguz.serbetci@gmail.com>
2020-05-09 00:41:27 +0200

    Ignore empty predictions in Accuracy calculation
    First of all, thanks for the package! I really enjoy using it. Here is my first contribution:
    
    I got errors coming from torch.argmax: 
    `cannot perform reduction function argmax on a tensor with no elements because the operation does not have an identity`
    
    Because I'm masking using indexing, training step sometime outputs empty predictions. I think it is more general to allow empty predictions in metric calculation.
    
    Let me know if you want any tests covering the added code path.

--- ignite/metrics/accuracy.py
+++ ignite/metrics/accuracy.py
@@ -149,7 +149,10 @@ class Accuracy(_BaseClassification):
         self._check_shape((y_pred, y))
         self._check_type((y_pred, y))
 
-        if self._type == "binary":
+        if y_pred.size(0) == 0:
+            # ignore empty predictions
+            correct = torch.tensor([])
+        elif self._type == "binary":
             correct = torch.eq(y_pred.view(-1).to(y), y.view(-1))
         elif self._type == "multiclass":
             indices = torch.argmax(y_pred, dim=1)
