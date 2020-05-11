d608434f30abac82209207530232ae3a8e86739b
AutoPEP8 <>
2020-05-11 13:59:58 +0000

    autopep8 fix

--- ignite/contrib/handlers/param_scheduler.py
+++ ignite/contrib/handlers/param_scheduler.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 from collections import OrderedDict
 from collections.abc import Mapping, Sequence
 from copy import copy
-from typing import List, Union, Optional
+from typing import List, Optional, Union
 
 import torch
 from torch.optim.lr_scheduler import _LRScheduler
