c5854a8abd80bc31dec5a581e6a461b923d18996
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-05-13 18:22:12 -0400

    use cpu tests script.

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -6,4 +6,5 @@ CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python$TRAVIS_PYTHON_VERSIO
 
 export WORLD_SIZE=2
 
-CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$TRAVIS_PYTHON_VERSION tests -m distributed -vvv
+# Rerun failing tests up to five times with a 30s delay
+CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$TRAVIS_PYTHON_VERSION tests -m distributed -vvv --reruns 5 --reruns-delay 30
