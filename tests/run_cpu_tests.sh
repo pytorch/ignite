c461242c013eb46c68ff3bc3edc9a7cf4388c0b6
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-05-13 18:27:42 -0400

    refactor cpu test script to make it more reusable.

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -2,9 +2,9 @@
 
 set -xeu
 
-CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python$TRAVIS_PYTHON_VERSION --cov ignite --cov-report term-missing -vvv tests/
+CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python$CI_PYTHON_VERSION --cov ignite --cov-report term-missing -vvv tests/
 
 export WORLD_SIZE=2
 
 # Rerun failing tests up to five times with a 30s delay
-CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$TRAVIS_PYTHON_VERSION tests -m distributed -vvv --reruns 5 --reruns-delay 30
+CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$CI_PYTHON_VERSION tests -m distributed -vvv --reruns 5 --reruns-delay 30
