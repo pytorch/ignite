421d609355860c4c2dab45eaea493f95c7c4033f
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-05-16 16:56:53 -0400

    remove args from cherry-picked branch.

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -6,5 +6,4 @@ CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python$CI_PYTHON_VERSION --
 
 export WORLD_SIZE=2
 
-# Rerun failing tests up to five times with a 30s delay
-CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$CI_PYTHON_VERSION tests -m distributed -vvv --reruns 5 --reruns-delay 30
+CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python$CI_PYTHON_VERSION tests -m distributed -vvv 
