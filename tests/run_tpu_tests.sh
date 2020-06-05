27339f451424e15558214746a5dca23c1a67f85a
vfdev <vfdev.5@gmail.com>
2020-06-05 15:22:37 +0200

    Fixes bad tpu tests conf and failing tpu tests (#1106)

--- tests/run_tpu_tests.sh
+++ tests/run_tpu_tests.sh
@@ -2,10 +2,9 @@
 
 set -xeu
 
+py.test --cov ignite --cov-report xml tests/ -vvv -m tpu
 
 if [ -z ${NUM_TPU_WORKERS+x} ]; then
     export NUM_TPU_WORKERS=1
     py.test --cov ignite --cov-report xml tests/ -vvv -m tpu
 fi
-
-py.test --cov ignite --cov-report xml tests/ -vvv -m tpu
