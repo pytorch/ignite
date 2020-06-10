ba8a6a19c47ff1bcb3318c1d1f8e5961a9489bd0
vfdev-5 <vfdev.5@gmail.com>
2020-06-10 23:02:35 +0000

    Replaced pbars by logger

--- examples/contrib/cifar10/main.py
+++ examples/contrib/cifar10/main.py
@@ -12,71 +12,12 @@ import ignite.distributed as idist
 from ignite.engine import Events, Engine, create_supervised_evaluator
 from ignite.metrics import Accuracy, Loss
 from ignite.handlers import Checkpoint
-from ignite.utils import manual_seed
+from ignite.utils import manual_seed, setup_logger
 
 from ignite.contrib.engines import common
-from ignite.contrib.handlers import ProgressBar
 from ignite.contrib.handlers import PiecewiseLinear
 
 import utils
-
-
-def get_dataflow(config):
-    # - Get train/test datasets
-    if idist.get_rank() > 0:
-        # Ensure that only rank 0 download the dataset
-        idist.barrier()
-
-    train_dataset, test_dataset = utils.get_train_test_datasets(config["data_path"])
-
-    if idist.get_rank() == 0:
-        # Ensure that only rank 0 download the dataset
-        idist.barrier()
-
-    # Setup data loader also adapted to distributed config: nccl, gloo, xla-tpu
-    train_loader = idist.auto_dataloader(
-        train_dataset,
-        batch_size=config["batch_size"],
-        num_workers=config["num_workers"],
-        shuffle=True,
-        pin_memory="cuda" in idist.device().type,
-        drop_last=True,
-    )
-
-    test_loader = idist.auto_dataloader(
-        test_dataset,
-        batch_size=2 * config["batch_size"],
-        num_workers=config["num_workers"],
-        shuffle=False,
-        pin_memory="cuda" in idist.device().type,
-    )
-    return train_loader, test_loader
-
-
-def initialize(config):
-    model = utils.get_model(config["model"])
-    # Adapt model for distributed settings if configured
-    model = idist.auto_model(model)
-
-    optimizer = optim.SGD(
-        model.parameters(),
-        lr=config["learning_rate"],
-        momentum=config["momentum"],
-        weight_decay=config["weight_decay"],
-        nesterov=True,
-    )
-    optimizer = idist.auto_optim(optimizer)
-    criterion = nn.CrossEntropyLoss().to(idist.device())
-    return model, optimizer, criterion
-
-
-def print_metrics(epoch, tag, metrics):
-    if idist.get_rank() == 0:
-        print(
-            "\nEpoch {} - {} metrics:\n {}".format(
-                epoch, tag, "\n".join(["\t{}: {}".format(k, v) for k, v in metrics.items()])
-            )
-        )
 
 
 def training(local_rank, config):
@@ -85,22 +26,9 @@ def training(local_rank, config):
     manual_seed(config["seed"] + rank)
     device = idist.device()
 
-    if rank == 0:
-        print("Train {} on CIFAR10".format(config["model"]))
-        print("- PyTorch version: {}".format(torch.__version__))
-        print("- Ignite version: {}".format(ignite.__version__))
+    logger = setup_logger(name="CIFAR10-Training", distributed_rank=local_rank)
 
-        print("\n")
-        print("Configuration:")
-        for key, value in config.items():
-            print("\t{}: {}".format(key, value))
-        print("\n")
-
-        if idist.get_world_size() > 1:
-            print("\nDistributed setting:")
-            print("\tbackend: {}".format(idist.backend()))
-            print("\tworld size: {}".format(idist.get_world_size()))
-            print("\n")
+    log_basic_info(logger, config)
 
     output_path = config["output_path"]
     if rank == 0:
@@ -114,80 +42,16 @@ def training(local_rank, config):
         if not output_path.exists():
             output_path.mkdir(parents=True)
         config["output_path"] = output_path.as_posix()
-        print("Output path: {}".format(config["output_path"]))
+        logger.info("Output path: {}".format(config["output_path"]))
 
     # Setup dataflow, model, optimizer, criterion
     train_loader, test_loader = get_dataflow(config)
 
-    # Setup model, optimizer
-    model, optimizer, criterion = initialize(config)
+    config["num_iters_per_epoch"] = len(train_loader)
+    model, optimizer, criterion, lr_scheduler = initialize(config)
 
-    le = len(train_loader)
-    milestones_values = [
-        (0, 0.0),
-        (le * config["num_warmup_epochs"], config["learning_rate"]),
-        (le * config["num_epochs"], 0.0),
-    ]
-    lr_scheduler = PiecewiseLinear(optimizer, param_name="lr", milestones_values=milestones_values)
-
-    # Setup Ignite trainer:
-    # - let's define training step
-    # - add other common handlers:
-    #    - TerminateOnNan,
-    #    - handler to setup learning rate scheduling,
-    #    - ModelCheckpoint
-    #    - RunningAverage` on `train_step` output
-    #    - Two progress bars on epochs and optionally on iterations
-
-    def train_step(engine, batch):
-
-        x, y = batch[0], batch[1]
-
-        if x.device != device:
-            x = x.to(device, non_blocking=True)
-            y = y.to(device, non_blocking=True)
-
-        model.train()
-        # Supervised part
-        y_pred = model(x)
-        loss = criterion(y_pred, y)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # This can be helpful for XLA to avoid performance slow down if fetch loss.item() every iteration
-        if config["log_every_iters"] > 0 and (engine.state.iteration - 1) % config["log_every_iters"] == 0:
-            batch_loss = loss.item()
-            engine.state.saved_batch_loss = batch_loss
-        else:
-            batch_loss = engine.state.saved_batch_loss
-
-        return {
-            "batch loss": batch_loss,
-        }
-
-    trainer = Engine(train_step)
-    trainer.state.saved_batch_loss = -1.0
-    trainer.state_dict_user_keys.append("saved_batch_loss")
-
-    to_save = {"trainer": trainer, "model": model, "optimizer": optimizer, "lr_scheduler": lr_scheduler}
-    metric_names = [
-        "batch loss",
-    ]
-    output_path = config["output_path"]
-    common.setup_common_training_handlers(
-        trainer,
-        train_sampler=train_loader.sampler,
-        to_save=to_save,
-        save_every_iters=config["checkpoint_every"],
-        output_path=output_path,
-        lr_scheduler=lr_scheduler,
-        output_names=metric_names if config["log_every_iters"] > 0 else None,
-        with_pbar_on_iters=config["log_every_iters"] > 0,
-        log_every_iters=True,
-        clear_cuda_cache=False,
-    )
+    # Create trainer for current task
+    trainer = create_trainer(model, optimizer, criterion, lr_scheduler, train_loader.sampler, config, logger)
 
     # Let's now setup evaluator engine to perform model's validation and compute metrics
     metrics = {
@@ -203,18 +67,13 @@ def training(local_rank, config):
     def run_validation(engine):
         epoch = trainer.state.epoch
         state = train_evaluator.run(train_loader)
-        print_metrics(epoch, "Train", state.metrics)
+        log_metrics(logger, epoch, state.times["COMPLETED"], "Train", state.metrics)
         state = evaluator.run(test_loader)
-        print_metrics(epoch, "Test", state.metrics)
+        log_metrics(logger, epoch, state.times["COMPLETED"], "Test", state.metrics)
 
     trainer.add_event_handler(Events.EPOCH_COMPLETED(every=config["validate_every"]) | Events.COMPLETED, run_validation)
 
     if rank == 0:
-        # Setup progress bar on evaluation engines
-        if config["log_every_iters"] > 0:
-            ProgressBar(persist=False, desc="Train evaluation").attach(train_evaluator)
-            ProgressBar(persist=False, desc="Test evaluation").attach(evaluator)
-
         # Setup TensorBoard logging on trainer and evaluators. Logged values are:
         #  - Training metrics, e.g. running average loss values
         #  - Learning rate
@@ -241,17 +100,8 @@ def training(local_rank, config):
 
         @trainer.on(Events.ITERATION_STARTED(once=config["stop_iteration"]))
         def _():
-            print("Stop training on {} iteration".format(trainer.state.iteration))
+            logger.info("Stop training on {} iteration".format(trainer.state.iteration))
             trainer.terminate()
-
-    resume_from = config["resume_from"]
-    if resume_from is not None:
-        checkpoint_fp = Path(resume_from)
-        assert checkpoint_fp.exists(), "Checkpoint '{}' is not found".format(checkpoint_fp.as_posix())
-        if rank == 0:
-            print("Resume from a checkpoint: {}".format(checkpoint_fp.as_posix()))
-        checkpoint = torch.load(checkpoint_fp.as_posix(), map_location="cpu")
-        Checkpoint.load_objects(to_load=to_save, checkpoint=checkpoint)
 
     try:
         trainer.run(train_loader, max_epochs=config["num_epochs"])
@@ -324,7 +174,166 @@ def run(
     spawn_kwargs["num_procs_per_node"] = num_procs_per_node
 
     with idist.Parallel(backend=backend, **spawn_kwargs) as parallel:
+
         parallel.run(training, config)
+
+
+def get_dataflow(config):
+    # - Get train/test datasets
+    if idist.get_rank() > 0:
+        # Ensure that only rank 0 download the dataset
+        idist.barrier()
+
+    train_dataset, test_dataset = utils.get_train_test_datasets(config["data_path"])
+
+    if idist.get_rank() == 0:
+        # Ensure that only rank 0 download the dataset
+        idist.barrier()
+
+    # Setup data loader also adapted to distributed config: nccl, gloo, xla-tpu
+    train_loader = idist.auto_dataloader(
+        train_dataset,
+        batch_size=config["batch_size"],
+        num_workers=config["num_workers"],
+        shuffle=True,
+        pin_memory="cuda" in idist.device().type,
+        drop_last=True,
+    )
+
+    test_loader = idist.auto_dataloader(
+        test_dataset,
+        batch_size=2 * config["batch_size"],
+        num_workers=config["num_workers"],
+        shuffle=False,
+        pin_memory="cuda" in idist.device().type,
+    )
+    return train_loader, test_loader
+
+
+def initialize(config):
+    model = utils.get_model(config["model"])
+    # Adapt model for distributed settings if configured
+    model = idist.auto_model(model)
+
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=config["learning_rate"],
+        momentum=config["momentum"],
+        weight_decay=config["weight_decay"],
+        nesterov=True,
+    )
+    optimizer = idist.auto_optim(optimizer)
+    criterion = nn.CrossEntropyLoss().to(idist.device())
+
+    le = config["num_iters_per_epoch"]
+    milestones_values = [
+        (0, 0.0),
+        (le * config["num_warmup_epochs"], config["learning_rate"]),
+        (le * config["num_epochs"], 0.0),
+    ]
+    lr_scheduler = PiecewiseLinear(optimizer, param_name="lr", milestones_values=milestones_values)
+
+    return model, optimizer, criterion, lr_scheduler
+
+
+def log_metrics(logger, epoch, elapsed, tag, metrics):
+    logger.info(
+        "\nEpoch {} - elapsed: {} - {} metrics:\n {}".format(
+            epoch, elapsed, tag, "\n".join(["\t{}: {}".format(k, v) for k, v in metrics.items()])
+        )
+    )
+
+
+def log_basic_info(logger, config):
+    logger.info("Train {} on CIFAR10".format(config["model"]))
+    logger.info("- PyTorch version: {}".format(torch.__version__))
+    logger.info("- Ignite version: {}".format(ignite.__version__))
+
+    logger.info("\n")
+    logger.info("Configuration:")
+    for key, value in config.items():
+        logger.info("\t{}: {}".format(key, value))
+    logger.info("\n")
+
+    if idist.get_world_size() > 1:
+        logger.info("\nDistributed setting:")
+        logger.info("\tbackend: {}".format(idist.backend()))
+        logger.info("\tworld size: {}".format(idist.get_world_size()))
+        logger.info("\n")
+
+
+def create_trainer(model, optimizer, criterion, lr_scheduler, train_sampler, config, logger):
+
+    device = idist.device()
+
+    # Setup Ignite trainer:
+    # - let's define training step
+    # - add other common handlers:
+    #    - TerminateOnNan,
+    #    - handler to setup learning rate scheduling,
+    #    - ModelCheckpoint
+    #    - RunningAverage` on `train_step` output
+    #    - Two progress bars on epochs and optionally on iterations
+
+    def train_step(engine, batch):
+
+        x, y = batch[0], batch[1]
+
+        if x.device != device:
+            x = x.to(device, non_blocking=True)
+            y = y.to(device, non_blocking=True)
+
+        model.train()
+        # Supervised part
+        y_pred = model(x)
+        loss = criterion(y_pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # This can be helpful for XLA to avoid performance slow down if fetch loss.item() every iteration
+        if config["log_every_iters"] > 0 and (engine.state.iteration - 1) % config["log_every_iters"] == 0:
+            batch_loss = loss.item()
+            engine.state.saved_batch_loss = batch_loss
+        else:
+            batch_loss = engine.state.saved_batch_loss
+
+        return {
+            "batch loss": batch_loss,
+        }
+
+    trainer = Engine(train_step)
+    trainer.state.saved_batch_loss = -1.0
+    trainer.state_dict_user_keys.append("saved_batch_loss")
+    trainer.logger = logger
+
+    to_save = {"trainer": trainer, "model": model, "optimizer": optimizer, "lr_scheduler": lr_scheduler}
+    metric_names = [
+        "batch loss",
+    ]
+    output_path = config["output_path"]
+    common.setup_common_training_handlers(
+        trainer,
+        train_sampler=train_sampler,
+        to_save=to_save,
+        save_every_iters=config["checkpoint_every"],
+        output_path=output_path,
+        lr_scheduler=lr_scheduler,
+        output_names=metric_names if config["log_every_iters"] > 0 else None,
+        with_pbars=False,
+        clear_cuda_cache=False,
+    )
+
+    resume_from = config["resume_from"]
+    if resume_from is not None:
+        checkpoint_fp = Path(resume_from)
+        assert checkpoint_fp.exists(), "Checkpoint '{}' is not found".format(checkpoint_fp.as_posix())
+        logger.info("Resume from a checkpoint: {}".format(checkpoint_fp.as_posix()))
+        checkpoint = torch.load(checkpoint_fp.as_posix(), map_location="cpu")
+        Checkpoint.load_objects(to_load=to_save, checkpoint=checkpoint)
+
+    return trainer
 
 
 if __name__ == "__main__":
