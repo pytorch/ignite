b9f63474f3190d5ebe857514b4076c094676fa08
vfdev-5 <vfdev.5@gmail.com>
2020-06-11 13:22:35 +0000

    Updated coverage report type for gpu/tpu

--- tests/run_gpu_tests.sh
+++ tests/run_gpu_tests.sh
@@ -8,17 +8,17 @@ fi
 
 set -xeu
 
-py.test --cov ignite --cov-report term-missing -vvv tests/ -k 'on_cuda'
+py.test --cov ignite --cov-report term-missing --cov-report xml -vvv tests/ -k 'on_cuda'
 
 if [ "${ngpus}" -eq "1" ]; then
 
-    py.test --cov ignite --cov-append --cov-report term-missing -vvv tests/ -m distributed
+    py.test --cov ignite --cov-append --cov-report term-missing --cov-report xml -vvv tests/ -m distributed
 
 else
 
-    py.test --cov ignite --cov-append --cov-report term-missing -vvv tests/ -m distributed
+    py.test --cov ignite --cov-append --cov-report term-missing --cov-report xml -vvv tests/ -m distributed
 
     export WORLD_SIZE=${ngpus}
-    py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx ${WORLD_SIZE}*popen//python=python3.7 tests -m distributed -vvv
+    py.test --cov ignite --cov-append --cov-report term-missing --cov-report xml --dist=each --tx ${WORLD_SIZE}*popen//python=python3.7 tests -m distributed -vvv
 
 fi
