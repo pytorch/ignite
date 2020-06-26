9a7be7bfd27dea84a88a510f1729ef27ab5bfba4
vfdev <vfdev.5@gmail.com>
2020-06-26 02:05:26 +0200

    Fixes #1162 (#1163)
    * Fixes #1162
    - relaxed check of optimizer type
    
    * Updated docs

--- ignite/contrib/handlers/visdom_logger.py
+++ ignite/contrib/handlers/visdom_logger.py
@@ -218,7 +218,8 @@ class OptimizerParamsHandler(BaseOptimizerParamsHandler, _BaseVisDrawer):
             )
 
     Args:
-        optimizer (torch.optim.Optimizer): torch optimizer which parameters to log
+        optimizer (torch.optim.Optimizer or object): torch optimizer or any object with attribute ``param_groups``
+            as a sequence.
         param_name (str): parameter name
         tag (str, optional): common title for all produced plots. For example, "generator"
         show_legend (bool, optional): flag to show legend in the window
