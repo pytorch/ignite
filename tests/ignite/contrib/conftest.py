585236f8abf5d6f8d85abf4335eeac018cd0a7e0
Sylvain Desroziers <sylvain.desroziers@gmail.com>
2020-07-22 19:56:40 +0200

    Issue 1133 - Fixes flaky Visdom tests (#1149)
    * [WIP] inspect bug
    
    * Attempt to fix flaky Visdom tests
    
    * autopep8 fix
    
    Co-authored-by: vfdev-5 <vfdev.5@gmail.com>
    Co-authored-by: AutoPEP8 <>

--- tests/ignite/contrib/conftest.py
+++ tests/ignite/contrib/conftest.py
@@ -1,21 +1,63 @@
+import random
+
 import pytest
 
 
 @pytest.fixture()
-def visdom_server():
+def visdom_offline_logfile(dirname):
 
     import os
-    import signal
-    import subprocess
+
+    log_file = os.path.join(dirname, "logs.visdom")
+    yield log_file
+
+
+vd_hostname = None
+vd_port = None
+vd_server_process = None
+
+
+@pytest.fixture()
+def visdom_server():
+    # Start Visdom server once and stop it with visdom_server_stop
+    global vd_hostname, vd_port, vd_server_process
+
+    if vd_server_process is None:
+
+        import subprocess
+        import time
+
+        from visdom import Visdom
+        from visdom.server import download_scripts
+
+        download_scripts()
+
+        vd_hostname = "localhost"
+        vd_port = random.randint(8089, 8887)
+
+        try:
+            vis = Visdom(server=vd_hostname, port=vd_port, raise_exceptions=True)
+        except ConnectionError:
+            pass
+
+        vd_server_process = subprocess.Popen(
+            ["python", "-m", "visdom.server", "--hostname", vd_hostname, "-port", str(vd_port)]
+        )
+        time.sleep(5)
+
+        vis = Visdom(server=vd_hostname, port=vd_port)
+        assert vis.check_connection()
+        vis.close()
+
+    yield (vd_hostname, vd_port)
+
+
+@pytest.fixture()
+def visdom_server_stop():
+
+    yield None
+
     import time
 
-    from visdom.server import download_scripts
-
-    download_scripts()
-
-    hostname = "localhost"
-    port = 8098
-    p = subprocess.Popen("visdom --hostname {} -port {}".format(hostname, port), shell=True, preexec_fn=os.setsid)
-    time.sleep(5)
-    yield (hostname, port)
-    os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    vd_server_process.kill()
+    time.sleep(2)
