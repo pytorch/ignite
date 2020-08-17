eca0bc37685d32603730563ad76719e0c39b511c
Nicholas Vadivelu <nicholas.vadivelu@gmail.com>
2020-08-17 14:59:31 -0400

    Update ignite/metrics/recall.py
    Co-authored-by: vfdev <vfdev.5@gmail.com>

--- ignite/metrics/recall.py
+++ ignite/metrics/recall.py
@@ -61,7 +61,7 @@ class Recall(_BasePrecisionRecall):
         is_multilabel (bool, optional) flag to use in multilabel case. By default, value is False. If True, average
             parameter should be True and the average is computed across samples, instead of classes.
         device (str or torch.device): specifies which device updates are accumulated on. Setting the metric's
-            device to be the same as your `update` arguments ensures the `update` method is non-blocking. By
+            device to be the same as your ``update`` arguments ensures the ``update`` method is non-blocking. By
             default, CPU.
 
     """
