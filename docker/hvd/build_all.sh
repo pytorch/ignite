d040efa757e973e4e7e8efd9004918407ad11fde
vfdev-5 <vfdev.5@gmail.com>
2020-09-21 10:03:31 +0000

    Updated hvd images - added scripts to auto build and push images

---
+++ docker/hvd/build_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Build all Horovod flavoured PyTorch-Ignite images"
+
+# Start script from ignite docker folder
+if [ ! -d hvd ]; then
+    echo "Can not find 'hvd' folder"
+    echo "Usage: sh hvd/build_all.sh"
+    exit 1
+fi
+
+curr_dir=$PWD
+cd $curr_dir/hvd
+
+set -xeu
+
+image_name="hvd-base"
+
+docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+image_tag=`docker run --rm -it pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
+docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+for image_name in "hvd-vision" "hvd-nlp" "hvd-apex" "hvd-apex-vision" "hvd-apex-nlp"
+do
+
+    docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+    docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+done
+
+cd $curr_dir
---
+++ docker/msdp/build_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Build all MS DeepSpeed flavoured PyTorch-Ignite images"
+
+# Start script from ignite docker folder
+if [ ! -d msdp ]; then
+    echo "Can not find 'msdp' folder"
+    echo "Usage: sh msdp/build_all.sh"
+    exit 1
+fi
+
+curr_dir=$PWD
+cd $curr_dir/msdp
+
+set -xeu
+
+image_name="msdp-apex-base"
+
+docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+image_tag=`docker run --rm -it pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
+docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+image_name="msdp-apex-vision"
+docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+image_name="msdp-apex-nlp"
+docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+cd $curr_dir
