936ef5333681b62c7b4154e35a1224209c042488
vfdev <vfdev.5@gmail.com>
2020-09-22 18:00:49 +0200

    Updated hvd images + scripts (#1306)
    * Updated hvd images
    - added scripts to auto build and push images
    
    * Updated scripts according to the review

---
+++ docker/main/build_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Build all PyTorch-Ignite images"
+
+# Start script from ignite docker folder
+if [ ! -d main ]; then
+    echo "Can not find 'main' folder"
+    echo "Usage: sh main/build_all.sh"
+    exit 1
+fi
+
+curr_dir=$PWD
+cd $curr_dir/main
+
+set -xeu
+
+image_name="base"
+
+docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+image_tag=`docker run --rm -it pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
+docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+for image_name in "vision" "nlp" "apex" "apex-vision" "apex-nlp"
+do
+
+    docker build -t pytorchignite/${image_name}:latest -f Dockerfile.${image_name} .
+    docker tag pytorchignite/${image_name}:latest pytorchignite/${image_name}:${image_tag}
+
+done
+
+cd $curr_dir
