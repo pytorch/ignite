Fix FID computation

ignite/contrib/metrics/fid.py
@@ -29,7 +29,7 @@ class FID(Metric):
             try:
                 from torchvision import models
 
-                fid_model = models.inception_v3(pretrained=True)
+                fid_model = models.inception_v3(pretrained=True, transform_input=True)
             except ImportError:
                 raise ValueError("Argument fid_model should be set")
 
@@ -51,11 +51,8 @@ class FID(Metric):
         y_pred, y = output[0].detach(), output[1].detach()
 
         with torch.no_grad():
-            batch_features_real = self._fid_model(y)
-            batch_features_fake = self._fid_model(y_pred)
-
-        batch_features_real = batch_features_real.view(*batch_features_real.size()[:2], -1).sum(-1)
-        batch_features_fake = batch_features_fake.view(*batch_features_fake.size()[:2], -1).sum(-1)
+            batch_features_real = self._fid_model(y).sum(axis=0)
+            batch_features_fake = self._fid_model(y_pred).sum(axis=0)
 
         self._num_of_data += 1
 
@@ -63,8 +60,8 @@ class FID(Metric):
         self._features_sum_fake += batch_features_fake
 
         if self._num_of_data > 1:
-            X_real = y - (self._features_sum_real / self._num_of_data)
-            X_fake = y_pred - (self._features_sum_fake / self._num_of_data)
+            X_real = batch_features_real - (self._features_sum_real / self._num_of_data)
+            X_fake = batch_features_fake - (self._features_sum_fake / self._num_of_data)
 
             self._cov_real = torch.ger(X_real, X_real) * (
                 self._num_of_data / (self._num_of_data + 1) ** 2

tests/ignite/contrib/metrics/test_fid.py
@@ -4,6 +4,7 @@ import numpy as np
 import PIL
 import pytest
 import torch
+from torchvision import models
 from torchvision.transforms.functional import to_tensor
 
 from ignite.contrib.metrics import FID
@@ -26,19 +27,22 @@ def test_fid(img_filepath):
     size = 10
 
     pil_img = PIL.Image.open(img_filepath)
-    tensor_img = to_tensor(pil_img)
+    tensor_img = to_tensor(pil_img).view(1, 3, 400, 300)
 
-    y = [tensor_img for _ in range(size)]
-    np_y = [img.cpu().numpy() for img in y]
+    y = torch.cat([tensor_img for _ in range(size)])
+    y_pred = torch.cat([(tensor_img + torch.rand_like(tensor_img) * 0.01) for _ in range(size)])
 
-    y_pred = [(tensor_img + torch.rand_like(tensor_img) * 0.01) for _ in range(size)]
-    np_y_pred = [img.cpu().numpy() for img in y_pred]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    inception_model = models.inception_v3(pretrained=True, transform_input=True).eval().to(devide)
 
-    mu_fake = np.mean(np_y_pred, axis=0)
-    mu_real = np.mean(np_y, axis=0)
+    np_batch_fake = inception_model(y_pred).detach().cpu().numpy()
+    np_batch_real = inception_model(y).detach().cpu().numpy()
 
-    cov_fake = np.cov(np_y_pred, rowvar=False)
-    cov_real = np.cov(np_y, rowvar=False)
+    mu_fake = np.mean(np_batch_fake, axis=0)
+    mu_real = np.mean(np_batch_real, axis=0)
+
+    cov_fake = np.cov(np_batch_fake, rowvar=False)
+    cov_real = np.cov(np_batch_real, rowvar=False)
 
     cc, _ = linalg.sqrtm(np.dot(cov_fake, cov_real), disp=False)
     dist = np.sum((mu_fake - mu_real) ** 2) + np.trace(cov_fake + cov_real - 2 * cc)