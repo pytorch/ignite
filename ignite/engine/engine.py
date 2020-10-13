415279676ab0b1d5923b507213069768b2ecb288
Benjamin Kinga <somethingscripted@gmail.com>
2020-10-12 19:17:15 -0500

    typo fix

--- ignite/engine/engine.py
+++ ignite/engine/engine.py
@@ -751,6 +751,7 @@ class Engine(Serializable):
             while self.state.epoch < self.state.max_epochs and not self.should_terminate:  # type: ignore[operator]
                 self.state.epoch += 1
                 self._fire_event(Events.EPOCH_STARTED)
+
                 if self._dataloader_iter is None:
                     self._setup_engine()
 
