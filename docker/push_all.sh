4ee7b5eeaefb993d03f93507ed354b7f817ac2d3
Taras Savchyn <trsvchn@gmail.com>
2020-10-19 19:05:45 +0300

    Add token envs for GH docker action, fix push all tag_image

--- docker/push_all.sh
+++ docker/push_all.sh
@@ -33,7 +33,7 @@ done
 
 
 image_name="hvd-base"
-image_tag=`docker run --rm -it pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
+image_tag=`docker run --rm -i pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
 
 for image_name in "hvd-base" "hvd-vision" "hvd-nlp" "hvd-apex" "hvd-apex-vision" "hvd-apex-nlp"
 do
@@ -44,7 +44,7 @@ do
 done
 
 image_name="msdp-apex-base"
-image_tag=`docker run --rm -it pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
+image_tag=`docker run --rm -i pytorchignite/${image_name}:latest -c 'python -c "import torch; import ignite; print(torch.__version__ + \"-\" + ignite.__version__, end=\"\")"'`
 
 for image_name in "msdp-apex-base" "msdp-apex-vision" "msdp-apex-nlp"
 do
