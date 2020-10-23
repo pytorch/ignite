acd2982dfc6ecbea347d1ff88d893f8acdd87a56
Taras Savchyn <30748114+trsvchn@users.noreply.github.com>
2020-10-23 11:20:22 +0300

    Toctrees for classes and methods using sphinx autosummary (#1393)
    * Implement autosummary patch for autolisting
    
    * Fix css for autogenerated tables via autosummary
    
    * Improve autolist feature
    
    * Add toctrees for methods and classes for ignite
    
    * Better import for autolist
    
    * Add toctrees for methods and classes for contrib package
    
    * Fix CSS for autosummary table row height
    
    * Fix warnings raised by toctree

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -205,3 +205,98 @@ from sphinx import addnodes
 
 # replaces pending_xref node with desc_type for type annotations
 sphinx.domains.python.type_to_xref = lambda t, e=None: addnodes.desc_type("", nodes.Text(t))
+
+# -- Autosummary patch to get list of a classes, funcs automatically ----------
+
+from importlib import import_module
+from inspect import getmembers, isclass, isfunction
+import sphinx.ext.autosummary
+from sphinx.ext.autosummary import Autosummary
+from docutils.parsers.rst import directives
+from docutils.statemachine import StringList
+
+
+class BetterAutosummary(Autosummary):
+    """Autosummary with autolisting for modules.
+
+    By default it tries to import all public names (__all__),
+    otherwise import all classes and/or functions in a module.
+
+    Options:
+    - :autolist: option to get list of classes and functions from currentmodule.
+    - :autolist-classes: option to get list of classes from currentmodule.
+    - :autolist-functions: option to get list of functions from currentmodule.
+
+    Example Usage:
+
+    .. currentmodule:: ignite.metrics
+
+    .. autosummary::
+        :nosignatures:
+        :autolist:
+    """
+
+    # Add new option
+    _option_spec = Autosummary.option_spec.copy()
+    _option_spec.update(
+        {
+            "autolist": directives.unchanged,
+            "autolist-classes": directives.unchanged,
+            "autolist-functions": directives.unchanged,
+        }
+    )
+    option_spec = _option_spec
+
+    def run(self):
+        for auto in ("autolist", "autolist-classes", "autolist-functions"):
+            if auto in self.options:
+                # Get current module name
+                module_name = self.env.ref_context.get("py:module")
+                # Import module
+                module = import_module(module_name)
+
+                # Get public names (if possible)
+                try:
+                    names = getattr(module, "__all__")
+                except AttributeError:
+                    # Get classes defined in the module
+                    cls_names = [
+                        name[0]
+                        for name in getmembers(module, isclass)
+                        if name[-1].__module__ == module_name and not (name[0].startswith("_"))
+                    ]
+                    # Get functions defined in the module
+                    fn_names = [
+                        name[0]
+                        for name in getmembers(module, isfunction)
+                        if (name[-1].__module__ == module_name) and not (name[0].startswith("_"))
+                    ]
+                    names = cls_names + fn_names
+                    # It may happen that module doesn't have any defined class or func
+                    if not names:
+                        names = [name[0] for name in getmembers(module)]
+
+                if auto == "autolist":
+                    # Get list of all classes and functions inside module
+                    names = [
+                        name for name in names if (isclass(getattr(module, name)) or isfunction(getattr(module, name)))
+                    ]
+                else:
+                    if auto == "autolist-classes":
+                        # Get only classes
+                        check = isclass
+                    elif auto == "autolist-functions":
+                        # Get only functions
+                        check = isfunction
+                    else:
+                        raise NotImplementedError
+
+                    names = [name for name in names if check(getattr(module, name))]
+
+                # Update content
+                self.content = StringList(names)
+        return super().run()
+
+
+# Patch original Autosummary
+sphinx.ext.autosummary.Autosummary = BetterAutosummary
