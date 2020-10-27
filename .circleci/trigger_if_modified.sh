75bd6f7bafcc82b492c577562c4c8390d2f2c54c
vfdev-5 <vfdev.5@gmail.com>
2020-10-27 02:23:44 +0100

    [skip ci] Updated trigger_if_modified.sh script - excluded docs from github actions - added 1.6.0 to pytorch-version-tests

--- .circleci/trigger_if_modified.sh
+++ .circleci/trigger_if_modified.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Script is taken from https://circleci.com/developer/orbs/orb/roopakv/swissknife#commands-run_if_modified
-# Usage: sh trigger_if_modified.sh <pattern>
+# Usage: sh trigger_if_modified.sh <pattern> [base-branch]
 # - for example: sh trigger_if_modified.sh "^(ignite|tests|examples|\.circleci).*"
 
 if [ -z "$1" ]; then
-    echo "Pattern should be provided. Usage: sh trigger_if_modified.sh <pattern>"
-    exit 1
+  echo "Pattern should be provided. Usage: sh trigger_if_modified.sh <pattern>"
+  exit 1
 fi
 
-base_branch=master
 pattern=$1
+
+if [ -z "$2" ]; then
+  base_branch=master
+else
+  base_branch=$2
+fi
+
+echo "- Pattern: ${pattern}"
+echo "- Base branch: ${base_branch}"
 
 if [ -z "$BASH" ]; then
   echo Bash not installed.
@@ -33,5 +41,5 @@ if [ -z "$FILES_MODIFIED" ]; then
   echo "Files not modified. Halting job"
   circleci-agent step halt
 else
-  echo "Files modified, continuing steps"
+  echo "Files modified: ${FILES_MODIFIED}, continuing steps"
 fi
