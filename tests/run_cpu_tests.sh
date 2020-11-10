7b47af1bc3bc5e1bf12f0e201c695a7ce71f8006
vfdev <vfdev.5@gmail.com>
2020-11-10 13:40:51 +0100

    Fixed broken coverage (#1451)
    * Fixed broken coverage
    
    * Updated hvd-tests.yml

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -8,7 +8,7 @@ else
     skip_distrib_opt=(-m "")
 fi
 
-CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python${CI_PYTHON_VERSION:-3.7} --cov ignite --cov-report term-missing -vvv tests "${skip_distrib_opt[@]}"
+CUDA_VISIBLE_DEVICES="" py.test --tx 4*popen//python=python${CI_PYTHON_VERSION:-3.7} --cov ignite --cov-report term-missing --cov-report xml -vvv tests "${skip_distrib_opt[@]}"
 
 # https://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 if [ "${SKIP_DISTRIB_TESTS:-0}" -eq "1" ]; then
@@ -18,4 +18,4 @@ fi
 export WORLD_SIZE=2
 
 
-CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --dist=each --tx $WORLD_SIZE*popen//python=python${CI_PYTHON_VERSION:-3.7} tests -m distributed -vvv
+CUDA_VISIBLE_DEVICES="" py.test --cov ignite --cov-append --cov-report term-missing --cov-report xml --dist=each --tx $WORLD_SIZE*popen//python=python${CI_PYTHON_VERSION:-3.7} tests -m distributed -vvv
