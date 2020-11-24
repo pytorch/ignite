b88213b837cae693401350e11f93d1631eef0060
ydcjeff <ydcjeff@outlook.com>
2020-11-24 21:18:33 +0630

    fix path

--- .github/workflows/install_docs_deps.sh
+++ .github/workflows/install_docs_deps.sh
@@ -1,5 +1,5 @@
 pip install -U pip wheel
 pip install torch torchvision -f https://download.pytorch.org/whl/cpu/torch_stable.html -U
-pip install -r ~/requirements-dev.txt
-pip install -r ~/docs/requirements.txt
+pip install -r ../../requirements-dev.txt
+pip install -r ../../docs/requirements.txt
 pip install git+https://github.com/vfdev-5/sphinxcontrib-versioning.git
