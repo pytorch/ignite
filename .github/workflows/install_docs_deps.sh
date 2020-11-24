928c1613dc0373a6f2ef5b6aaae19c756e280882
vfdev <vfdev.5@gmail.com>
2020-11-25 00:17:38 +0100

    Update install_docs_deps.sh

--- .github/workflows/install_docs_deps.sh
+++ .github/workflows/install_docs_deps.sh
@@ -1,4 +1,5 @@
-pip install -U pip wheel
+# remove pkg-resources as it causes failure when installing https://github.com/vfdev-5/sphinxcontrib-versioning
+pip uninstall -y pkg-resources setuptools && pip install --upgrade setuptools
 pip install torch torchvision -f https://download.pytorch.org/whl/cpu/torch_stable.html -U
 pip install -r requirements-dev.txt
 pip install -r docs/requirements.txt
