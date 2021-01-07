3c8b781b7bd0848cacf3e0e214b3e3a0b8d408db
vfdev <vfdev.5@gmail.com>
2021-01-07 22:25:23 +0100

    Refactored assert allowed events (#1549)
    - moved _utils._to_hours_mins_secs -> engine.utils._to_hours_mins_secs

--- ignite/engine/utils.py
+++ ignite/engine/utils.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, Callable
+from typing import Any, Callable, Tuple, Union
 
 
 def _check_signature(fn: Callable, fn_description: str, *args: Any, **kwargs: Any) -> None:
@@ -19,3 +19,10 @@ def _check_signature(fn: Callable, fn_description: str, *args: Any, **kwargs: An
             f"takes parameters {fn_params} but will be called with {passed_params}"
             f"({exception_msg})."
         )
+
+
+def _to_hours_mins_secs(time_taken: Union[float, int]) -> Tuple[int, int, int]:
+    """Convert seconds to hours, mins, and seconds."""
+    mins, secs = divmod(time_taken, 60)
+    hours, mins = divmod(mins, 60)
+    return round(hours), round(mins), round(secs)
