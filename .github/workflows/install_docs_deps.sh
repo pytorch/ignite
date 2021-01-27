26ace81a02e470302955819165b90dbbcc6a0b56
ydcjeff <ydcjeff@outlook.com>
2021-01-27 14:59:45 +0630

    install katex globally

--- .github/workflows/install_docs_deps.sh
+++ .github/workflows/install_docs_deps.sh
@@ -1,8 +1,5 @@
-npm install katex
-PATH="${PATH}:$(pwd)/node_modules/.bin"
-
 # remove pkg-resources as it causes failure when installing https://github.com/vfdev-5/sphinxcontrib-versioning
-pip uninstall -y pkg-resources setuptools && pip install --upgrade setuptools
+pip uninstall -y pkg-resources setuptools && pip install --upgrade setuptools pip wheel
 pip install torch torchvision -f https://download.pytorch.org/whl/cpu/torch_stable.html -U
 pip install -r requirements-dev.txt
 pip install -r docs/requirements.txt
