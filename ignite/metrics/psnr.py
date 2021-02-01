02030d5b614311e167c4ca3ff38d6fc1f09d9ffd
Jeff Yang <ydcjeff@outlook.com>
2021-02-01 16:16:34 +0630

    [metrics] improve PSNR (#1595)
    * [metrics] data_range became required arg, add tests for Y channel
    
    * Apply suggestions from code review

--- ignite/metrics/psnr.py
+++ ignite/metrics/psnr.py
@@ -12,21 +12,16 @@ class PSNR(Metric):
     r"""Computes average `Peak signal-to-noise ratio (PSNR) <https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio>`_.
 
     .. math::
-        \text{PSNR}(i, j) = 10 * \log_{10}\left(\frac{ MAX_{i}^2 }{ \text{ MSE } }\right)
+        \text{PSNR}(I, J) = 10 * \log_{10}\left(\frac{ MAX_{I}^2 }{ \text{ MSE } }\right)
 
     where :math:`\text{MSE}` is `mean squared error <https://en.wikipedia.org/wiki/Mean_squared_error>`_.
 
-    Note:
-        A few things to note:
-
-        - `y_pred` and `y` **must** have (batch_size, ...) shape.
-        - `y_pred` and `y` **must** have same dtype and same shape.
-        - If `y_pred` and `y` have almost identical values, the result will be infinity.
+    - `y_pred` and `y` **must** have (batch_size, ...) shape.
+    - `y_pred` and `y` **must** have same dtype and same shape.
 
     Args:
-        data_range (int or float, optional): The data range of the target image (distance between minimum
-            and maximum possible values). If not provided, it will be estimated from the input
-            data type: ``1.0`` for float/double tensor or ``255`` for unsigned 8-bit tensor.
+        data_range (int or float): The data range of the target image (distance between minimum
+            and maximum possible values).
             For other data types, please set the data range, otherwise an exception will be raised.
         output_transform (callable, optional): A callable that is used to transform the Engine’s
             process_function’s output into the form expected by the metric.
@@ -46,27 +41,42 @@ class PSNR(Metric):
             # ...
             return y_pred, y
         engine = Engine(process_function)
-        psnr = PSNR()
+        psnr = PSNR(data_range=1.0)
         psnr.attach(engine, "psnr")
         # ...
         state = engine.run(data)
         print(f"PSNR: {state.metrics['psnr']}")
+
+    This metric by default accepts Grayscale or RGB images. But if you have YCbCr or YUV images, only
+    Y channel is needed for computing PSNR. And, this can be done with ``output_transform``. For instance,
+
+    .. code-block:: python
+
+        def get_y_channel(output):
+            y_pred, y = output
+            # y_pred and y are (B, 3, H, W) and YCbCr or YUV images
+            # let's select y channel
+            return y_pred[:, 0, ...], y[:, 0, ...]
+
+        psnr = PSNR(data_range=219, output_transform=get_y_channel)
+        psnr.attach(engine, "psnr")
+        # ...
+        state = engine.run(data)
+        print(f"PSNR: {state.metrics['psrn']}")
 
     .. versionadded:: 0.5.0
     """
 
     def __init__(
         self,
-        data_range: Optional[Union[int, float]] = None,
+        data_range: Union[int, float],
         output_transform: Callable = lambda x: x,
         device: Union[str, torch.device] = torch.device("cpu"),
     ):
         super().__init__(output_transform=output_transform, device=device)
         self.data_range = data_range
 
-    def _check_shape_dtype_drange(
-        self, output: Sequence[torch.Tensor], data_range: Union[int, float, None]
-    ) -> Union[int, float]:
+    def _check_shape_dtype(self, output: Sequence[torch.Tensor]) -> None:
         y_pred, y = output
         if y_pred.dtype != y.dtype:
             raise TypeError(
@@ -78,26 +88,6 @@ class PSNR(Metric):
                 f"Expected y_pred and y to have the same shape. Got y_pred: {y_pred.shape} and y: {y.shape}."
             )
 
-        if data_range is None:
-            try:
-                dmin, dmax = _dtype_range[y.dtype]
-            except KeyError:
-                raise ValueError(
-                    "Range for this dtype cannot be automatically estimated. Please manually specify the data_range."
-                )
-            true_min, true_max = y.min(), y.max()
-            if true_max > dmax or true_min < dmin:
-                raise ValueError(
-                    "y has intensity values outside the range expected "
-                    "for its data type. Please manually specify the data_range."
-                )
-            if true_min >= 0:
-                # most common case (255 for uint8, 1 for float)
-                data_range = dmax
-            else:
-                data_range = dmax - dmin
-        return data_range
-
     @reinit__is_reduced
     def reset(self) -> None:
         self._sum_of_batchwise_psnr = torch.tensor(0.0, dtype=torch.float64, device=self._device)
@@ -105,13 +95,12 @@ class PSNR(Metric):
 
     @reinit__is_reduced
     def update(self, output: Sequence[torch.Tensor]) -> None:
-        data_range = self.data_range
-        data_range = self._check_shape_dtype_drange(output, data_range)
+        self._check_shape_dtype(output)
         y_pred, y = output[0].detach(), output[1].detach()
 
         dim = tuple(range(1, y.ndim))
         mse_error = torch.pow(y_pred.double() - y.view_as(y_pred).double(), 2).mean(dim=dim)
-        self._sum_of_batchwise_psnr += torch.sum(10.0 * torch.log10(data_range ** 2 / mse_error)).to(
+        self._sum_of_batchwise_psnr += torch.sum(10.0 * torch.log10(self.data_range ** 2 / (mse_error + 1e-10))).to(
             device=self._device
         )
         self._num_examples += y.shape[0]
@@ -121,11 +110,3 @@ class PSNR(Metric):
         if self._num_examples == 0:
             raise NotComputableError("PSNR must have at least one example before it can be computed.")
         return self._sum_of_batchwise_psnr / self._num_examples
-
-
-_dtype_range = {
-    torch.uint8: (0, 255),
-    torch.float16: (-1.0, 1.0),
-    torch.float32: (-1.0, 1.0),
-    torch.float64: (-1.0, 1.0),
-}
