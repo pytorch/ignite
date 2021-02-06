add50c729b698b27701e5a6a30e1286d752883cb
Ahmed Omar <ahmed42795@gmail.com>
2021-02-06 02:32:39 +0200

    added test for engine termination

--- tests/ignite/handlers/test_time_limit.py
+++ tests/ignite/handlers/test_time_limit.py
@@ -22,10 +22,18 @@ def test_terminate_on_time_limit():
     def _test(n_iters, limit):
         started = time.time()
         trainer = Engine(_train_func)
+
+        @trainer.on(Events.TERMINATE)
+        def _():
+            trainer.state.is_terminated = True
+
         trainer.add_event_handler(Events.ITERATION_COMPLETED, TimeLimit(limit))
+        trainer.state.is_terminated = False
+
         trainer.run(range(n_iters))
         elapsed = round(time.time() - started)
         assert elapsed <= limit + 1
+        assert trainer.state.is_terminated == (n_iters > limit)
 
     _test(20, 10)
     _test(5, 10)
