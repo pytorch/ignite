9ef05ceb58b08cf8a1540a3b47c081f34a65a40a
Ahmed Omar <ahmed42795@gmail.com>
2021-02-20 00:33:48 +0200

    transformers example

---
+++ examples/contrib/transformers/dataset.py
@@ -0,0 +1,35 @@
+import torch
+from torch._C import dtype
+
+
+class TransformerDataset(torch.utils.data.Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __getitem__(self, idx):
+        text = str(self.texts[idx])
+        text = " ".join(text.split())
+        inputs = self.tokenizer.encode_plus(
+            text, None, add_special_tokens=True, max_length=self.max_length, truncation=True
+        )
+
+        ids = inputs["input_ids"]
+        token_type_ids = inputs["token_type_ids"]
+        mask = inputs["attention_mask"]
+        padding_length = self.max_length - len(ids)
+
+        ids = ids + ([0] * padding_length)
+        mask = mask + ([0] * padding_length)
+        token_type_ids = token_type_ids + ([0] * padding_length)
+        return {
+            "input_ids": torch.tensor(ids, dtype=torch.long),
+            "attention_mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "label": torch.tensor(self.labels[idx], dtype=torch.float),
+        }
+
+    def __len__(self):
+        return len(self.labels)
