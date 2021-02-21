418d26bec08c4280541360c0c693d1919b3d3c5a
trsvchn <trsvchn@users.noreply.github.com>
2021-02-21 13:33:07 +0000

    autopep8 fix

--- ignite/fixme.py
+++ ignite/fixme.py
@@ -1,7 +1,11 @@
 # I am just a demo file that doesn't like PEP rules
 import time
-def fn(): print("Hello, World!")
+
+
+def fn():
+    print("Hello, World!")
+
 
 class FooBar:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
