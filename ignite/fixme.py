0ea10f4596860d6673a019edaf5d48eb30ec4a57
Taras Savchyn <trsvchn@gmail.com>
2021-02-21 15:51:16 +0200

    Add some changes

--- ignite/fixme.py
+++ ignite/fixme.py
@@ -1,11 +1,12 @@
 # I am just a demo file that doesn't like PEP rules
 import time
-
-
 def fn():
     print("Hello, World!")
-
-
 class FooBar:
     def __init__(self):
         pass
+    def a(self):
+        pass
+    def b(self):
+        pass
+
