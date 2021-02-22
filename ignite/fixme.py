7e253840b76542499dd285582afd0a89df4328e6
Taras Savchyn <trsvchn@gmail.com>
2021-02-22 12:44:12 +0200

    Add broken file

---
+++ ignite/fixme.py
@@ -0,0 +1,10 @@
+"""I don't like PEP rules.
+"""
+def fn(): print("Hello, World!")
+class Class:
+    def __init__(self):
+        pass
+    def a(self):
+        pass
+    def b(self):
+        pass
