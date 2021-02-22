df57f15ab81549035894000587d3eacc8a0f3b5d
trsvchn <trsvchn@users.noreply.github.com>
2021-02-22 10:46:19 +0000

    autopep8 fix

--- ignite/fixme.py
+++ ignite/fixme.py
@@ -1,10 +1,17 @@
 """I don't like PEP rules.
 """
-def fn(): print("Hello, World!")
+
+
+def fn():
+    print("Hello, World!")
+
+
 class Class:
     def __init__(self):
         pass
+
     def a(self):
         pass
+
     def b(self):
         pass
