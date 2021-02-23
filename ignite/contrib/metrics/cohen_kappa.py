3037b3f509006ebab4476632b576c849b993cf49
KickItLikeShika <ahmedkhaled19701@gmail.com>
2021-02-24 00:52:42 +0200

    add cohen kappa and all tests

--- ignite/contrib/metrics/cohen_kappa.py
+++ ignite/contrib/metrics/cohen_kappa.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 
 import torch
 
@@ -16,12 +16,13 @@ class CohenKappa(EpochMetric):
             :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
             form expected by the metric. This can be useful if, for example, you have a multi-output model and
             you want to compute the metric with respect to one of the outputs.
-        weights (str): a string is used to define the type of Cohen's Kappa whether Non-Weighted or Linear or Quadratic
+        weights: a string is used to define the type of Cohen's Kappa whether Non-Weighted or Linear or Quadratic
             (default: "")
-        check_compute_fn (bool): Default False. If True, `cohen_kappa_score
+        check_compute_fn: Default False. If True, `cohen_kappa_score
             <https://scikit-learn.org/stable/modules/generated/sklearn.metrics.cohen_kappa_score.html>`_
             is run on the first batch of data to ensure there are
             no issues. User will be warned in case there are any issues computing the function.
+        device (str or torch.device, optional): optional device specification for internal storage.
 
     .. code-block:: python
 
@@ -36,7 +37,11 @@ class CohenKappa(EpochMetric):
     """
 
     def __init__(
-        self, output_transform: Callable = lambda x: x, weights: Optional[str] = None, check_compute_fn: bool = False
+        self,
+        output_transform: Callable = lambda x: x,
+        weights: Optional[str] = None,
+        check_compute_fn: bool = False,
+        device: Union[str, torch.device] = torch.device("cpu"),
     ):
 
         try:
@@ -44,21 +49,24 @@ class CohenKappa(EpochMetric):
         except ImportError:
             raise RuntimeError("This contrib module requires sklearn to be installed.")
 
-        self.cohen_kappa_sk = cohen_kappa_score
-
         # initalize weights
         self.weights = weights
 
         self.cohen_kappa_compute = self.get_cohen_kappa_fn()
 
         super(CohenKappa, self).__init__(
-            self.cohen_kappa_compute, output_transform=output_transform, check_compute_fn=check_compute_fn
+            self.cohen_kappa_compute,
+            output_transform=output_transform,
+            check_compute_fn=check_compute_fn,
+            device=device,
         )
 
     def get_cohen_kappa_fn(self) -> Callable[[torch.Tensor, torch.Tensor], float]:
+        from sklearn.metrics import cohen_kappa_score
+
         def wrapper(y_targets: torch.Tensor, y_preds: torch.Tensor) -> float:
-            y_true = y_targets.numpy()
-            y_pred = y_preds.numpy()
-            return self.cohen_kappa_sk(y_true, y_pred, weights=self.weights)
+            y_true = y_targets.cpu().numpy()
+            y_pred = y_preds.cpu().numpy()
+            return cohen_kappa_score(y_true, y_pred, weights=self.weights)
 
         return wrapper
--- tests/ignite/contrib/metrics/test_cohen_kappa.py
+++ tests/ignite/contrib/metrics/test_cohen_kappa.py
@@ -1,8 +1,11 @@
+import os
+
 import numpy as np
 import pytest
 import torch
 from sklearn.metrics import cohen_kappa_score
 
+import ignite.distributed as idist
 from ignite.contrib.metrics import CohenKappa
 from ignite.engine import Engine
 
@@ -146,3 +149,99 @@ def test_integration_cohen_kappa_quadratic_weighted_with_output_transform():
     ck_value = engine.run(data, max_epochs=1).metrics["cohen_kappa"]
 
     assert np.array_equal(ck_value, ck_value_sk)
+
+
+def _test_distrib_compute(device):
+    rank = idist.get_rank()
+
+    def _test(metric_device):
+        metric_device = torch.device(metric_device)
+        ck_metric = CohenKappa(device=metric_device)
+
+        torch.manual_seed(10 + rank)
+
+        y_pred = torch.randint(0, 2, size=(100, 1), device=device)
+        y = torch.randint(0, 2, size=(100, 1), device=device)
+
+        ck_metric.update((y_pred, y))
+
+        # gather y_pred, y
+        y_pred = idist.all_gather(y_pred)
+        y = idist.all_gather(y)
+
+        np_y_pred = y_pred.cpu().numpy()
+        np_y = y.cpu().numpy()
+
+        np_ck = cohen_kappa_score(np_y, np_y_pred)
+
+        res = ck_metric.compute()
+        assert res == np_ck
+
+    for _ in range(3):
+        _test("cpu")
+        if device.type != "xla":
+            _test(idist.device())
+
+
+@pytest.mark.distributed
+@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
+@pytest.mark.skipif(torch.cuda.device_count() < 1, reason="Skip if no GPU")
+def test_distrib_gpu(distributed_context_single_node_nccl):
+    device = torch.device(f"cuda:{distributed_context_single_node_nccl['local_rank']}")
+    _test_distrib_compute(device)
+
+
+@pytest.mark.distributed
+@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
+def test_distrib_cpu(distributed_context_single_node_gloo):
+
+    device = torch.device("cpu")
+    _test_distrib_compute(device)
+
+
+@pytest.mark.distributed
+@pytest.mark.skipif(not idist.has_hvd_support, reason="Skip if no Horovod dist support")
+@pytest.mark.skipif("WORLD_SIZE" in os.environ, reason="Skip if launched as multiproc")
+def test_distrib_hvd(gloo_hvd_executor):
+
+    device = torch.device("cpu" if not torch.cuda.is_available() else "cuda")
+    nproc = 4 if not torch.cuda.is_available() else torch.cuda.device_count()
+
+    gloo_hvd_executor(_test_distrib_compute, (device,), np=nproc, do_init=True)
+
+
+@pytest.mark.multinode_distributed
+@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
+@pytest.mark.skipif("MULTINODE_DISTRIB" not in os.environ, reason="Skip if not multi-node distributed")
+def test_multinode_distrib_cpu(distributed_context_multi_node_gloo):
+    device = torch.device("cpu")
+    _test_distrib_compute(device)
+
+
+@pytest.mark.multinode_distributed
+@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
+@pytest.mark.skipif("GPU_MULTINODE_DISTRIB" not in os.environ, reason="Skip if not multi-node distributed")
+def test_multinode_distrib_gpu(distributed_context_multi_node_nccl):
+    device = torch.device(f"cuda:{distributed_context_multi_node_nccl['local_rank']}")
+    _test_distrib_compute(device)
+
+
+@pytest.mark.tpu
+@pytest.mark.skipif("NUM_TPU_WORKERS" in os.environ, reason="Skip if NUM_TPU_WORKERS is in env vars")
+@pytest.mark.skipif(not idist.has_xla_support, reason="Skip if no PyTorch XLA package")
+def test_distrib_single_device_xla():
+    device = idist.device()
+    _test_distrib_compute(device)
+
+
+def _test_distrib_xla_nprocs(index):
+    device = idist.device()
+    _test_distrib_compute(device)
+
+
+@pytest.mark.tpu
+@pytest.mark.skipif("NUM_TPU_WORKERS" not in os.environ, reason="Skip if no NUM_TPU_WORKERS in env vars")
+@pytest.mark.skipif(not idist.has_xla_support, reason="Skip if no PyTorch XLA package")
+def test_distrib_xla_nprocs(xmp_executor):
+    n = int(os.environ["NUM_TPU_WORKERS"])
+    xmp_executor(_test_distrib_xla_nprocs, args=(), nprocs=n)
