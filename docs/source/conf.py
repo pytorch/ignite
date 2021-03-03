72de0d7116a695a39d4e0fab0512422e65edb462
ydcjeff <ydcjeff@outlook.com>
2021-03-03 13:31:23 +0630

    feat(searchbox): visible UI, respect to version while searching

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,8 @@ try:
     version = os.environ["code_version"]
     if "master" in version:
         version = "master (" + ignite.__version__ + ")"
-    else:
-        version = version.replace("v", "")
+    # else:
+    #     version = version.replace("v", "")
 except KeyError:
     version = ignite.__version__
 
