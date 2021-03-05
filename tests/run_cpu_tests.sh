07dfe09d13821e641c8973aa3cb080f60cc881d9
ydcjeff <ydcjeff@outlook.com>
2021-03-05 21:11:13 +0630

    test test-results artifacts

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -8,7 +8,7 @@ else
     skip_distrib_opt=(-m "")
 fi
 
-CUDA_VISIBLE_DEVICES="" pytest --tx 4*popen//python=python --cov ignite --cov-report term-missing --cov-report xml -vvv tests "${skip_distrib_opt[@]}"
+CUDA_VISIBLE_DEVICES="" pytest --tx 4*popen//python=python --cov ignite --cov-report term-missing --cov-report xml -vvv tests/ignite/engine "${skip_distrib_opt[@]}" --junit-xml test-results/coverage.xml
 
 # https://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 if [ "${SKIP_DISTRIB_TESTS:-0}" -eq "1" ]; then
@@ -18,4 +18,4 @@ fi
 export WORLD_SIZE=2
 
 
-CUDA_VISIBLE_DEVICES="" pytest --cov ignite --cov-append --cov-report term-missing --cov-report xml --dist=each --tx $WORLD_SIZE*popen//python=python tests -m distributed -vvv
+CUDA_VISIBLE_DEVICES="" pytest --cov ignite --cov-append --cov-report term-missing --cov-report xml --dist=each --tx $WORLD_SIZE*popen//python=python tests/ignite/engine -m distributed -vvv --junit-xml test-results/coverage.xml
