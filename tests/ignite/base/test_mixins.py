c31a73636b7fd0863784f9479d80a38fcf5333e4
ydcjeff <ydcjeff@outlook.com>
2021-03-17 17:22:44 +0630

    cov: add test for ignite.base

--- tests/ignite/base/test_mixins.py
+++ tests/ignite/base/test_mixins.py
@@ -1,7 +1,8 @@
 from ignite.base import Serializable
 
 
-def test_load_state_dict():
+def test_state_dict_and_load_state_dict():
 
     s = Serializable()
+    s.state_dict()
     s.load_state_dict({})
