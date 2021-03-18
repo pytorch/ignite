236173359ec6e78a26e2443643c03cb8af855757
Jeff Yang <ydcjeff@outlook.com>
2021-03-18 16:18:33 +0630

    cov: add test for ignite.base (#1807)
    * cov: add test for ignite.base
    
    * raise NotImplementedError
    
    Co-authored-by: Sylvain Desroziers <sylvain.desroziers@gmail.com>

--- tests/ignite/base/test_mixins.py
+++ tests/ignite/base/test_mixins.py
@@ -1,4 +1,12 @@
+import pytest
+
 from ignite.base import Serializable
+
+
+def test_state_dict():
+    s = Serializable()
+    with pytest.raises(NotImplementedError):
+        s.state_dict()
 
 
 def test_load_state_dict():
