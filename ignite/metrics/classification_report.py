3de6b67d0fd39127b6f9e2b9af70164b95671f22
Moh-Yakoub <momo.ah.yakoub@gmail.com>
2021-04-10 14:49:53 +0100

    replace the metric with a function instead of class

--- ignite/metrics/classification_report.py
+++ ignite/metrics/classification_report.py
@@ -4,137 +4,105 @@ from typing import Callable, Collection, Dict, List, Optional, Sequence, Union
 import torch
 
 from ignite.metrics.fbeta import Fbeta
-from ignite.metrics.metric import Metric, reinit__is_reduced
+from ignite.metrics.metric import Metric
 from ignite.metrics.metrics_lambda import MetricsLambda
 from ignite.metrics.precision import Precision
 from ignite.metrics.recall import Recall
 
 __all__ = ["ClassificationReport"]
 
+"""Build a text report showing the main classification metrics. The report resembles in functionality
+    'https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html#sklearn.metrics.classification_report'
+    The underlying implementation doesn't use the sklearn function.
 
-class ClassificationReport(MetricsLambda):
-    """Build a text report showing the main classification metrics. The report resembles in functionality
-        'https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html#sklearn.metrics.classification_report'
-        The underlying implementation doesn't use the sklearn function.
+    Args:
+        beta: weight of precision in harmonic mean
+        output_dict: If True, return output as dict, otherwise return a str
+        output_transform: a callable that is used to transform the
+            :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
+            form expected by the metric. This can be useful if, for example, you have a multi-output model and
+            you want to compute the metric with respect to one of the outputs.
+        device: optional device specification for internal storage.
+        labels: Optional list of label indices to include in the report
+        digits: Number of digits for formatting output floating point values, by default it is 5
 
-        Args:
-            beta: weight of precision in harmonic mean
-            output_dict: If True, return output as dict, otherwise return a str
-            output_transform: a callable that is used to transform the
-                :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
-                form expected by the metric. This can be useful if, for example, you have a multi-output model and
-                you want to compute the metric with respect to one of the outputs.
-            device: optional device specification for internal storage.
-            labels: Optional list of label indices to include in the report
-            digits: Number of digits for formatting output floating point values, by default it is 5
-
-        .. code-block:: python
+    .. code-block:: python
 
 
-            y_true = torch.tensor([[1, 0],
-            [0, 1], [1, 0], [1, 1], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]])
-            y_pred = torch.randint(0, 2, size=(10,))
+        y_true = torch.tensor([[1, 0],
+        [0, 1], [1, 0], [1, 1], [1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0]])
+        y_pred = torch.randint(0, 2, size=(10,))
 
-            classification_report = ClassificationReport(output_dict=True, digits=2)
-            classification_report.update((y_true, y_pred))
-            res = classification_report.compute()
+        classification_report = ClassificationReport(output_dict=True, digits=2)
+        classification_report.update((y_true, y_pred))
+        res = classification_report.compute()
 
-            # result should look like this:
-            {
-                "0": {
-                    "precision": 0.33,
-                    "recall": 0.5,
-                    "f1-score": 0.4
-                },
-                "1": {
-                    "precision": 0.5,
-                    "recall": 0.33,
-                    "f1-score": 0.4
-                },
-                "macro avg": {
-                    "precision": 0.42,
-                    "recall": 0.42,
-                    "f1-score": 0.4
-                }
+        # result should look like this:
+        {
+            "0": {
+                "precision": 0.33,
+                "recall": 0.5,
+                "f1-score": 0.4
+            },
+            "1": {
+                "precision": 0.5,
+                "recall": 0.33,
+                "f1-score": 0.4
+            },
+            "macro avg": {
+                "precision": 0.42,
+                "recall": 0.42,
+                "f1-score": 0.4
             }
-    """
+        }
+"""
 
-    def __init__(
-        self,
-        beta: int = 1,
-        output_dict: bool = False,
-        output_transform: Optional[Callable] = None,
-        device: Union[str, torch.device] = torch.device("cpu"),
-        labels: Optional[List[str]] = None,
-    ):
-        self.beta = beta
-        # setup all the underlying metrics
-        self.precision = Precision(
-            average=False,
-            output_transform=(lambda x: x) if output_transform is None else output_transform,  # type: ignore[arg-type]
-            device=device,
-        )
-        self.recall = Recall(
-            average=False,
-            output_transform=(lambda x: x) if output_transform is None else output_transform,  # type: ignore[arg-type]
-            device=device,
-        )
-        self.fbeta = Fbeta(beta, average=False, precision=self.precision, recall=self.recall)
-        self.averaged_precision = self.precision.mean()
-        self.averaged_recall = self.recall.mean()
-        self.averaged_fbeta = self.fbeta.mean()
 
-        # setup all output functions
-        self.output_dict = output_dict
-        self.output_transform = output_transform
-        self.labels = labels
-        self.combined_metric = MetricsLambda(
-            self._wrapper,
-            self.recall,
-            self.precision,
-            self.fbeta,
-            self.averaged_recall,
-            self.averaged_precision,
-            self.averaged_fbeta,
-        )
-        super(ClassificationReport, self).__init__(
-            f=self._wrapper, device=device, precision=self.precision, recall=self.recall
-        )
+def ClassificationReport(
+    beta: int = 1,
+    output_dict: bool = False,
+    output_transform: Optional[Callable] = None,
+    device: Union[str, torch.device] = torch.device("cpu"),
+    labels: Optional[List[str]] = None,
+) -> MetricsLambda:
+
+    # setup all the underlying metrics
+    precision = Precision(
+        average=False,
+        output_transform=(lambda x: x) if output_transform is None else output_transform,  # type: ignore[arg-type]
+        device=device,
+    )
+    recall = Recall(
+        average=False,
+        output_transform=(lambda x: x) if output_transform is None else output_transform,  # type: ignore[arg-type]
+        device=device,
+    )
+    fbeta = Fbeta(beta, average=False, precision=precision, recall=recall)
+    averaged_precision = precision.mean()
+    averaged_recall = recall.mean()
+    averaged_fbeta = fbeta.mean()
 
     def _wrapper(
-        self, recall: Metric, precision: Metric, f: Metric, a_recall: Metric, a_precision: Metric, a_f: Metric,
+        recall_metric: Metric, precision_metric: Metric, f: Metric, a_recall: Metric, a_precision: Metric, a_f: Metric,
     ) -> Union[Collection[str], Dict]:
-        p_tensor, r_tensor, f_tensor = precision, recall, f
+        p_tensor, r_tensor, f_tensor = precision_metric, recall_metric, f
         assert p_tensor.shape == r_tensor.shape
         dict_obj = {}
         for idx, p_label in enumerate(p_tensor):
-            dict_obj[self._get_label_for_class(idx)] = {
+            dict_obj[_get_label_for_class(idx)] = {
                 "precision": p_label.item(),
                 "recall": r_tensor[idx].item(),
-                "f{0}-score".format(self.beta): f_tensor[idx].item(),
+                "f{0}-score".format(beta): f_tensor[idx].item(),
             }
         dict_obj["macro avg"] = {
             "precision": a_precision.item(),
             "recall": a_recall.item(),
-            "f{0}-score".format(self.beta): a_f.item(),
+            "f{0}-score".format(beta): a_f.item(),
         }
-        return dict_obj if self.output_dict else json.dumps(dict_obj)
+        return dict_obj if output_dict else json.dumps(dict_obj)
 
     # helper method to get a label for a given class
-    def _get_label_for_class(self, idx: int) -> str:
-        return self.labels[idx] if self.labels else str(idx)
+    def _get_label_for_class(idx: int) -> str:
+        return labels[idx] if labels else str(idx)
 
-    @reinit__is_reduced
-    def reset(self) -> None:
-        self.combined_metric.reset()
-
-    @reinit__is_reduced
-    def update(self, output: Sequence[Union[torch.Tensor, Dict]]) -> None:
-        # MetricsLambda doesn't update underlying metrics automatically
-        # so we have to do this for every underlying metric
-        self.precision.update(output)
-        self.recall.update(output)
-        # self.fbeta.update(output)
-
-    def compute(self) -> Union[Dict, str]:
-        return self.combined_metric.compute()
+    return MetricsLambda(_wrapper, recall, precision, fbeta, averaged_recall, averaged_precision, averaged_fbeta,)
--- tests/ignite/metrics/test_classification_report.py
+++ tests/ignite/metrics/test_classification_report.py
@@ -3,49 +3,16 @@ import os
 
 import pytest
 import torch
+from ignite.engine import Engine
 
 import ignite.distributed as idist
 from ignite.metrics.classification_report import ClassificationReport
 
+
 torch.manual_seed(12)
 
 
-def test_input_types():
-    classification_report = ClassificationReport()
-    classification_report.reset()
-
-    y_true, y_preds = torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long()
-    classification_report.update((_unflatten_binary(y_true), y_preds))
-
-    with pytest.raises(RuntimeError, match=r"Input data type has changed from multiclass to binary"):
-        classification_report.update((y_true, y_preds))
-
-    with pytest.raises(ValueError, match=r"y and y_pred must have compatible shapes."):
-        y_true = torch.randint(0, 2, size=(100,)).long()
-        classification_report.update((_unflatten_binary(y_true), y_preds))
-
-
-def test_non_binary():
-    classification_report = ClassificationReport()
-    classification_report.reset()
-
-    y_true, y_preds = torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long()
-    classification_report.update((_unflatten_binary(y_true), y_preds))
-
-    with pytest.raises(ValueError, match=r"For binary cases, y_pred must be comprised of 0's and 1's."):
-        y_true = torch.tensor([1, 2, 3, 4, 0.5, 6, 7, 8, 9, 10])
-        classification_report.update((y_true, y_preds))
-
-    classification_report.reset()
-
-    with pytest.raises(ValueError, match=r"For binary cases, y must be comprised of 0's and 1's."):
-        y_true = torch.tensor([1, 2, 3, 4, 0.5, 6, 7, 8, 9, 10])
-        classification_report.update((y_true, y_true))
-
-
 def _test_integration_binary(device):
-
-    from ignite.engine import Engine
 
     rank = idist.get_rank()
     torch.manual_seed(12)
@@ -88,7 +55,59 @@ def _test_integration_binary(device):
         assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
         assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
 
-    for _ in range(10):
+    for _ in range(5):
+        # check multiple random inputs as random exact occurencies are rare
+        metric_devices = ["cpu"]
+        if device.type != "xla":
+            metric_devices.append(idist.device())
+        for metric_device in metric_devices:
+            _test(metric_device)
+
+
+def _test_integration_binary_labels(device, labels):
+
+    rank = idist.get_rank()
+    torch.manual_seed(12)
+
+    def _test(metric_device):
+
+        n_iters = 80
+        s = 16
+        offset = n_iters * s
+        y_true = torch.randint(0, 2, size=(offset * idist.get_world_size(),)).to(device)
+        y_preds = torch.randint(0, 2, size=(offset * idist.get_world_size(),)).to(device)
+        y_true_unflat = _unflatten_binary(y_true)
+        classification_report = ClassificationReport(device=metric_device, output_dict="False", labels=labels)
+
+        def update(engine, i):
+            return (
+                y_true_unflat[i * s + rank * offset : (i + 1) * s + rank * offset],
+                y_preds[i * s + rank * offset : (i + 1) * s + rank * offset],
+            )
+
+        engine = Engine(update)
+
+        classification_report.attach(engine, "cr")
+
+        data = list(range(n_iters))
+        engine.run(data=data)
+
+        assert "cr" in engine.state.metrics
+        res = engine.state.metrics["cr"]
+        res2 = classification_report.compute()
+        assert res == res2
+
+        from sklearn.metrics import classification_report as sklearn_classification_report
+
+        sklearn_result = sklearn_classification_report(y_preds.cpu().numpy(), y_true.cpu().numpy(), output_dict=True)
+
+        assert pytest.approx(res[labels[0]] == sklearn_result["0"])
+        assert pytest.approx(res[labels[1]] == sklearn_result["1"])
+        assert pytest.approx(res["macro avg"]["precision"] == sklearn_result["macro avg"]["precision"])
+        assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
+        assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
+
+    for _ in range(5):
         # check multiple random inputs as random exact occurencies are rare
         metric_devices = ["cpu"]
         if device.type != "xla":
@@ -98,8 +117,6 @@ def _test_integration_binary(device):
 
 
 def _test_integration_multilabel(device, output_dict):
-
-    from ignite.engine import Engine
 
     rank = idist.get_rank()
     torch.manual_seed(12)
@@ -140,13 +157,15 @@ def _test_integration_multilabel(device, output_dict):
 
         sklearn_result = sklearn_classification_report(y_preds.cpu().numpy(), y_true.cpu().numpy(), output_dict=True)
 
-        assert pytest.approx(res["0"] == sklearn_result["0"])
-        assert pytest.approx(res["1"] == sklearn_result["1"])
+        for i in range(n_classes):
+            assert pytest.approx(res[str(i)]["precision"] == sklearn_result[str(i)]["precision"])
+            assert pytest.approx(res[str(i)]["f1-score"] == sklearn_result[str(i)]["f1-score"])
+            assert pytest.approx(res[str(i)]["recall"] == sklearn_result[str(i)]["recall"])
         assert pytest.approx(res["macro avg"]["precision"] == sklearn_result["macro avg"]["precision"])
         assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
         assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
 
-    for _ in range(10):
+    for _ in range(5):
         # check multiple random inputs as random exact occurencies are rare
         metric_devices = ["cpu"]
         if device.type != "xla":
@@ -155,148 +174,62 @@ def _test_integration_multilabel(device, output_dict):
             _test(metric_device, 3)
 
 
-@pytest.mark.parametrize("output_dict", [True, False])
-def test_binary_input_N(output_dict):
+def _test_integration_multilabel_with_labels(device, output_dict):
 
-    classification_report = ClassificationReport(output_dict=output_dict)
+    rank = idist.get_rank()
+    torch.manual_seed(12)
 
-    def _test(y_true, y_pred, batch_size):
-        y_true_unflat = _unflatten_binary(y_true)
-        classification_report.reset()
+    def _test(metric_device, n_classes, labels):
 
-        if batch_size > 1:
-            n_iters = y_true_unflat.shape[0] // batch_size + 1
-            for i in range(n_iters):
-                idx = i * batch_size
-                classification_report.update((y_true_unflat[idx : idx + batch_size], y_pred[idx : idx + batch_size]))
-        else:
-            classification_report.update((y_true_unflat, y_pred))
-
-        from sklearn.metrics import classification_report as sklearn_classification_report
-
-        res = classification_report.compute()
-        assert isinstance(res, dict if output_dict else str)
-
-        sklearn_result = sklearn_classification_report(y_pred, y_true, output_dict=True)
-        if not output_dict:
-            res = ast.literal_eval(res)
-
-        assert pytest.approx(res["0"] == sklearn_result["0"])
-        assert pytest.approx(res["1"] == sklearn_result["1"])
-        assert pytest.approx(res["macro avg"]["precision"] == sklearn_result["macro avg"]["precision"])
-        assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
-        assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
-
-    def get_test_cases():
-        test_cases = [
-            (torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long(), 1),
-            (torch.randint(0, 2, size=(100,)).long(), torch.randint(0, 2, size=(100,)).long(), 1),
-            # updated batches
-            (torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long(), 16),
-            (torch.randint(0, 2, size=(100,)).long(), torch.randint(0, 2, size=(100,)).long(), 16),
-        ]
-        return test_cases
-
-    for _ in range(10):
-        # check multiple random inputs as random exact occurencies are rare
-        test_cases = get_test_cases()
-        for y_true, y_pred, batch_size in test_cases:
-            _test(y_true, y_pred, batch_size)
-
-
-@pytest.mark.parametrize("output_dict", [True, False])
-def test_binary_input_N_with_labels(output_dict):
-    def _test(y_true, y_pred, batch_size, labels):
-        classification_report = ClassificationReport(output_dict=output_dict, labels=labels)
-
-        y_true_unflat = _unflatten_binary(y_true)
-
-        if batch_size > 1:
-            n_iters = y_true_unflat.shape[0] // batch_size + 1
-            for i in range(n_iters):
-                idx = i * batch_size
-                classification_report.update((y_true_unflat[idx : idx + batch_size], y_pred[idx : idx + batch_size]))
-        else:
-            classification_report.update((y_true_unflat, y_pred))
-
-        from sklearn.metrics import classification_report as sklearn_classification_report
-
-        res = classification_report.compute()
-        assert isinstance(res, dict if output_dict else str)
-
-        sklearn_result = sklearn_classification_report(y_pred, y_true, output_dict=True)
-        if not output_dict:
-            res = ast.literal_eval(res)
-
-        assert pytest.approx(res[labels[0]] == sklearn_result["0"])
-        assert pytest.approx(res[labels[1]] == sklearn_result["1"])
-        assert pytest.approx(res["macro avg"]["precision"] == sklearn_result["macro avg"]["precision"])
-        assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
-        assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
-
-    def get_test_cases():
-        test_cases = [
-            (torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long(), 1),
-            (torch.randint(0, 2, size=(100,)).long(), torch.randint(0, 2, size=(100,)).long(), 1),
-            # updated batches
-            (torch.randint(0, 2, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long(), 16),
-            (torch.randint(0, 2, size=(100,)).long(), torch.randint(0, 2, size=(100,)).long(), 16),
-        ]
-        return test_cases
-
-    test_cases = get_test_cases()
-    for y_true, y_pred, batch_size in test_cases:
-        _test(y_true, y_pred, batch_size, ["label0", "label1"])
-
-
-@pytest.mark.parametrize("output_dict", [True, False])
-def test_multilabel_input_N(output_dict):
-
-    classification_report = ClassificationReport(output_dict=output_dict)
-
-    def _test(y_true, y_pred, batch_size, n_classes):
+        n_iters = 80
+        s = 16
+        offset = n_iters * s
+        y_true = torch.randint(0, n_classes, size=(offset * idist.get_world_size(),)).to(device)
+        y_preds = torch.randint(0, n_classes, size=(offset * idist.get_world_size(),)).to(device)
         y_true_unflat = _unflatten_multilabel(y_true, n_classes)
-        classification_report.reset()
+        classification_report = ClassificationReport(device=metric_device, output_dict=output_dict, labels=labels)
 
-        if batch_size > 1:
-            n_iters = y_true_unflat.shape[0] // batch_size + 1
-            for i in range(n_iters):
-                idx = i * batch_size
-                classification_report.update((y_true_unflat[idx : idx + batch_size], y_pred[idx : idx + batch_size]))
-        else:
-            classification_report.update((y_true_unflat, y_pred))
+        def update(engine, i):
+            return (
+                y_true_unflat[i * s + rank * offset : (i + 1) * s + rank * offset],
+                y_preds[i * s + rank * offset : (i + 1) * s + rank * offset],
+            )
+
+        engine = Engine(update)
+
+        classification_report.attach(engine, "cr")
+
+        data = list(range(n_iters))
+        engine.run(data=data)
+
+        assert "cr" in engine.state.metrics
+        res = engine.state.metrics["cr"]
+        res2 = classification_report.compute()
+        assert res == res2
+
+        assert isinstance(res, dict if output_dict else str)
+        if not output_dict:
+            res = ast.literal_eval(res)
 
         from sklearn.metrics import classification_report as sklearn_classification_report
 
-        res = classification_report.compute()
-        assert isinstance(res, dict if output_dict else str)
-
-        sklearn_result = sklearn_classification_report(y_pred, y_true, output_dict=True)
-
-        if not output_dict:
-            res = ast.literal_eval(res)
+        sklearn_result = sklearn_classification_report(y_preds.cpu().numpy(), y_true.cpu().numpy(), output_dict=True)
 
         for i in range(n_classes):
-            assert pytest.approx(res[str(i)] == sklearn_result[str(i)])
+            assert pytest.approx(res[labels[i]]["precision"] == sklearn_result[str(i)]["precision"])
+            assert pytest.approx(res[labels[i]]["f1-score"] == sklearn_result[str(i)]["f1-score"])
+            assert pytest.approx(res[labels[i]]["recall"] == sklearn_result[str(i)]["recall"])
         assert pytest.approx(res["macro avg"]["precision"] == sklearn_result["macro avg"]["precision"])
         assert pytest.approx(res["macro avg"]["recall"] == sklearn_result["macro avg"]["recall"])
         assert pytest.approx(res["macro avg"]["f1-score"] == sklearn_result["macro avg"]["f1-score"])
 
-    def get_test_cases():
-        test_cases = [
-            (torch.randint(0, 3, size=(10,)).long(), torch.randint(0, 3, size=(10,)).long(), 1, 3),
-            (torch.randint(0, 3, size=(100,)).long(), torch.randint(0, 3, size=(100,)).long(), 1, 3),
-            # updated batches
-            (torch.randint(0, 3, size=(10,)).long(), torch.randint(0, 2, size=(10,)).long(), 16, 3),
-            (torch.randint(0, 3, size=(100,)).long(), torch.randint(0, 2, size=(100,)).long(), 16, 3),
-        ]
-        return test_cases
-
-    for _ in range(10):
+    for _ in range(5):
         # check multiple random inputs as random exact occurencies are rare
-        test_cases = get_test_cases()
-        for y_true, y_pred, batch_size, n_classes in test_cases:
-            _test(y_true, y_pred, batch_size, n_classes)
+        metric_devices = ["cpu"]
+        if device.type != "xla":
+            metric_devices.append(idist.device())
+        for metric_device in metric_devices:
+            _test(metric_device, 3, ["label0", "label1", "label2"])
 
 
 @pytest.mark.multinode_distributed
@@ -306,8 +239,11 @@ def test_multinode_distrib_gpu(distributed_context_multi_node_nccl):
 
     device = torch.device(f"cuda:{distributed_context_multi_node_nccl['local_rank']}")
     _test_integration_binary(device)
+    _test_integration_binary_labels(device, labels=["label0", "label1"])
     _test_integration_multilabel(device, True)
     _test_integration_multilabel(device, False)
+    _test_integration_multilabel_with_labels(device, True)
+    _test_integration_multilabel_with_labels(device, False)
 
 
 @pytest.mark.multinode_distributed
@@ -316,8 +252,11 @@ def test_multinode_distrib_gpu(distributed_context_multi_node_nccl):
 def test_multinode_distrib_cpu(distributed_context_multi_node_gloo):
     device = torch.device("cpu")
     _test_integration_binary(device)
+    _test_integration_binary_labels(device, labels=["label0", "label1"])
     _test_integration_multilabel(device, True)
     _test_integration_multilabel(device, False)
+    _test_integration_multilabel_with_labels(device, True)
+    _test_integration_multilabel_with_labels(device, False)
 
 
 @pytest.mark.distributed
@@ -325,14 +264,18 @@ def test_multinode_distrib_cpu(distributed_context_multi_node_gloo):
 def test_distrib_cpu(local_rank, distributed_context_single_node_gloo):
     device = torch.device("cpu")
     _test_integration_binary(device)
+    _test_integration_binary_labels(device, labels=["label0", "label1"])
     _test_integration_multilabel(device, True)
     _test_integration_multilabel(device, False)
+    _test_integration_multilabel_with_labels(device, True)
+    _test_integration_multilabel_with_labels(device, False)
 
 
 def _test_distrib_xla_nprocs(index):
 
     device = idist.device()
     _test_integration_binary(device)
+    _test_integration_binary_labels(device, labels=["label0", "label1"])
     _test_integration_multilabel(device, True)
     _test_integration_multilabel(device, False)
 
