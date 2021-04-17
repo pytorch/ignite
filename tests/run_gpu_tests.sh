e2b8dc9764790f7fc53a793dd557eefb4e1ee0c7
vfdev <vfdev.5@gmail.com>
2021-04-17 23:18:19 +0200

    Update run_gpu_tests.sh
    Remove `-k on_cuda` to avoid situations when test is written to be executed on gpu but does not contain "on_cuda"

--- tests/run_gpu_tests.sh
+++ tests/run_gpu_tests.sh
@@ -8,7 +8,7 @@ fi
 
 set -xeu
 
-pytest --cov ignite --cov-report term-missing --cov-report xml -vvv tests/ -k 'on_cuda'
+pytest --cov ignite --cov-report term-missing --cov-report xml -vvv tests/
 
 # https://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 if [ "${SKIP_DISTRIB_TESTS:-0}" -eq "1" ]; then
