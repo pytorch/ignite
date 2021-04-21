8b6ecc07436ec5c935129d68be426ccaeebc66ee
KickItLikeShika <ahmedkhaled19701@gmail.com>
2021-04-21 14:20:31 +0200

    edit __init__

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -1,6 +1,15 @@
 from ignite.contrib.handlers.clearml_logger import ClearMLLogger
 from ignite.contrib.handlers.mlflow_logger import MLflowLogger
 from ignite.contrib.handlers.neptune_logger import NeptuneLogger
+from ignite.contrib.handlers.param_scheduler import (
+    ConcatScheduler,
+    CosineAnnealingScheduler,
+    LinearCyclicalScheduler,
+    LRScheduler,
+    ParamGroupScheduler,
+    PiecewiseLinear,
+    create_lr_scheduler_with_warmup,
+)
 from ignite.contrib.handlers.polyaxon_logger import PolyaxonLogger
 from ignite.contrib.handlers.stores import EpochOutputStore
 from ignite.contrib.handlers.tensorboard_logger import TensorboardLogger
@@ -10,12 +19,3 @@ from ignite.contrib.handlers.trains_logger import TrainsLogger
 from ignite.contrib.handlers.visdom_logger import VisdomLogger
 from ignite.contrib.handlers.wandb_logger import WandBLogger
 from ignite.handlers import global_step_from_engine  # ref
-from ignite.handlers.param_scheduler import (
-    ConcatScheduler,
-    CosineAnnealingScheduler,
-    LinearCyclicalScheduler,
-    LRScheduler,
-    ParamGroupScheduler,
-    PiecewiseLinear,
-    create_lr_scheduler_with_warmup,
-)
