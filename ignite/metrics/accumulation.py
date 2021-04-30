5f5f4531faac742ff5bbe76129f080e165023a59
vfdev <vfdev.5@gmail.com>
2021-04-30 14:02:23 +0200

    Accumulation with apex (#1977) (#1978)
    * Accumulation with apex (#1977)
    
    * add a test for ccumulation metric  with Apex
    
    * autopep8 fix
    
    * cast accumulation output to accumulator dtype / update apex test
    
    * autopep8 fix
    
    * formatting code
    
    * one step conversion ( device and dtype )
    
    Co-authored-by: fco-dv <fco-dv@users.noreply.github.com>
    
    * Update test_accumulation.py
    
    Co-authored-by: François COKELAER <francois.cokelaer@gmail.com>
    Co-authored-by: fco-dv <fco-dv@users.noreply.github.com>

--- ignite/metrics/accumulation.py
+++ ignite/metrics/accumulation.py
@@ -67,8 +67,8 @@ class VariableAccumulation(Metric):
 
         if isinstance(output, torch.Tensor):
             output = output.detach()
-            if output.device != self._device:
-                output = output.to(self._device)
+            if not (output.device == self._device and output.dtype == self.accumulator.dtype):
+                output = output.to(self.accumulator)
 
         self.accumulator = self._op(self.accumulator, output)
 
