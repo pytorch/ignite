e963779e4f3768c8e067af5731159f777e27ad07
vfdev-5 <vfdev.5@gmail.com>
2021-05-31 11:04:01 +0000

    Revert "Issue #1247 (#1252)"
    This reverts commit b8294730d6bd9348436a54197d4cd521c60fda93.

--- tests/ignite/contrib/handlers/test_base_logger.py
+++ tests/ignite/contrib/handlers/test_base_logger.py
@@ -1,8 +1,10 @@
 from unittest.mock import MagicMock, call
 
+import math
 import pytest
 import torch
 
+from ignite.contrib.handlers import CustomPeriodicEvent
 from ignite.contrib.handlers.base_logger import BaseLogger, BaseOptimizerParamsHandler, BaseOutputHandler
 from ignite.engine import Engine, Events, EventsList, State
 from tests.ignite.contrib.handlers import MockFP16DeepSpeedZeroOptimizer
@@ -187,6 +189,33 @@ def test_attach_on_custom_event():
 
         mock_log_handler.assert_called_with(trainer, logger, event)
         assert mock_log_handler.call_count == n_calls
+
+    with pytest.warns(DeprecationWarning, match="CustomPeriodicEvent is deprecated"):
+        n_iterations = 10
+        cpe1 = CustomPeriodicEvent(n_iterations=n_iterations)
+        n = len(data) * n_epochs / n_iterations
+        nf = math.floor(n)
+        ns = nf + 1 if nf < n else nf
+        _test(cpe1.Events.ITERATIONS_10_STARTED, ns, cpe1)
+        _test(cpe1.Events.ITERATIONS_10_COMPLETED, nf, cpe1)
+
+    with pytest.warns(DeprecationWarning, match="CustomPeriodicEvent is deprecated"):
+        n_iterations = 15
+        cpe2 = CustomPeriodicEvent(n_iterations=n_iterations)
+        n = len(data) * n_epochs / n_iterations
+        nf = math.floor(n)
+        ns = nf + 1 if nf < n else nf
+        _test(cpe2.Events.ITERATIONS_15_STARTED, ns, cpe2)
+        _test(cpe2.Events.ITERATIONS_15_COMPLETED, nf, cpe2)
+
+    with pytest.warns(DeprecationWarning, match="CustomPeriodicEvent is deprecated"):
+        n_custom_epochs = 2
+        cpe3 = CustomPeriodicEvent(n_epochs=n_custom_epochs)
+        n = n_epochs / n_custom_epochs
+        nf = math.floor(n)
+        ns = nf + 1 if nf < n else nf
+        _test(cpe3.Events.EPOCHS_2_STARTED, ns, cpe3)
+        _test(cpe3.Events.EPOCHS_2_COMPLETED, nf, cpe3)
 
 
 def test_as_context_manager():
