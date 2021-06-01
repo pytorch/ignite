40f7af2fe54f66b7a53940725f7cb723b06cd60e
OBITORASU <OBITORASU@users.noreply.github.com>
2021-06-01 08:21:58 +0000

    autopep8 fix

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -1,16 +1,6 @@
 from ignite.contrib.handlers.clearml_logger import ClearMLLogger
-from ignite.handlers.lr_finder import FastaiLRFinder
 from ignite.contrib.handlers.mlflow_logger import MLflowLogger
 from ignite.contrib.handlers.neptune_logger import NeptuneLogger
-from ignite.handlers.param_scheduler import (
-    ConcatScheduler,
-    CosineAnnealingScheduler,
-    LinearCyclicalScheduler,
-    LRScheduler,
-    ParamGroupScheduler,
-    PiecewiseLinear,
-    create_lr_scheduler_with_warmup,
-)
 from ignite.contrib.handlers.polyaxon_logger import PolyaxonLogger
 from ignite.contrib.handlers.tensorboard_logger import TensorboardLogger
 from ignite.contrib.handlers.time_profilers import BasicTimeProfiler, HandlersTimeProfiler
@@ -20,3 +10,13 @@ from ignite.contrib.handlers.visdom_logger import VisdomLogger
 from ignite.contrib.handlers.wandb_logger import WandBLogger
 from ignite.handlers import EpochOutputStore  # ref
 from ignite.handlers import global_step_from_engine  # ref
+from ignite.handlers.lr_finder import FastaiLRFinder
+from ignite.handlers.param_scheduler import (
+    ConcatScheduler,
+    CosineAnnealingScheduler,
+    LinearCyclicalScheduler,
+    LRScheduler,
+    ParamGroupScheduler,
+    PiecewiseLinear,
+    create_lr_scheduler_with_warmup,
+)
