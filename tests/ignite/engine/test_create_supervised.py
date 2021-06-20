64144b0f97a88c1702dcee54d021573d18250a19
afzal442 <afzal442@gmail.com>
2021-06-20 20:23:52 +0530

    Updated test fo CI

--- tests/ignite/engine/test_create_supervised.py
+++ tests/ignite/engine/test_create_supervised.py
@@ -81,8 +81,8 @@ def _test_create_supervised_trainer(
             else:
                 assert not hasattr(state, "scaler")
     else:
-        if LooseVersion(torch.__version__) >= LooseVersion("1.7.0"):
-            # This is broken in 1.6.0 but will be probably fixed with 1.7.0
+        if LooseVersion(torch.__version__) >= LooseVersion("1.9.0"):
+            # This is broken in 1.8.1 but will be probably fixed with 1.9.0
             with pytest.raises(RuntimeError, match=r"is on CPU, but expected them to be on GPU"):
                 trainer.run(data)
 
@@ -210,8 +210,8 @@ def _test_create_supervised_evaluator(
         assert model.bias.item() == approx(0.0)
 
     else:
-        if LooseVersion(torch.__version__) >= LooseVersion("1.7.0"):
-            # This is broken in 1.6.0 but will be probably fixed with 1.7.0
+        if LooseVersion(torch.__version__) >= LooseVersion("1.9.0"):
+            # This is broken in 1.8.1 but will be probably fixed with 1.9.0
             with pytest.raises(RuntimeError, match=r"is on CPU, but expected them to be on GPU"):
                 evaluator.run(data)
 
