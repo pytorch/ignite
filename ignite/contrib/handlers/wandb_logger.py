866df130d4776022d65c3eb70802ecd739e5ab84
Ishan Kumar <ishankumar216@gmail.com>
2021-08-11 01:05:45 +0530

    added state attributes for WandB Logger

--- ignite/contrib/handlers/wandb_logger.py
+++ ignite/contrib/handlers/wandb_logger.py
@@ -226,6 +226,22 @@ class OutputHandler(BaseOutputHandler):
                 global_step_transform=global_step_transform
             )
 
+        Another example where the State Attributes ``trainer.state.alpha`` and ``trainer.state.beta``
+        are also logged along with the NLL and Accuracy after each iteration:
+
+        .. code-block:: python
+
+            clearml_logger.attach(
+                trainer,
+                log_handler=OutputHandler(
+                    tag="training",
+                    metric_names=["nll", "accuracy"],
+                    state_attributes=["alpha", "beta"],
+                ),
+                event_name=Events.ITERATION_COMPLETED
+            )
+
+
     Args:
         tag: common title for all produced plots. For example, "training"
         metric_names: list of metric names to plot or a string "all" to plot all available
@@ -251,6 +267,8 @@ class OutputHandler(BaseOutputHandler):
             def global_step_transform(engine, event_name):
                 return engine.state.get_event_attrib_value(event_name)
 
+    ..  versionchanged:: 0.5.0
+        accepts an optional list of `state_attributes`
     """
 
     def __init__(
@@ -260,8 +278,11 @@ class OutputHandler(BaseOutputHandler):
         output_transform: Optional[Callable] = None,
         global_step_transform: Optional[Callable] = None,
         sync: Optional[bool] = None,
+        state_attributes: Optional[List[str]] = None,
     ):
-        super().__init__(tag, metric_names, output_transform, global_step_transform)
+        super().__init__(
+            tag, metric_names, output_transform, global_step_transform, state_attributes
+        )
         self.sync = sync
 
     def __call__(self, engine: Engine, logger: WandBLogger, event_name: Union[str, Events]) -> None:
