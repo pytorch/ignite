b0a847ad90afbd0eb087c8046b7330abd29a69f0
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-09-22 11:33:30 +0630

    fix: test torch 1.9.1

--- tests/ignite/distributed/test_auto.py
+++ tests/ignite/distributed/test_auto.py
@@ -204,7 +204,7 @@ def test_auto_methods_gloo(distributed_context_single_node_gloo):
     _test_auto_model_optimizer(ws, device)
 
     if ws > 1 and device.type == "cpu":
-        error_type = AssertionError if LooseVersion(torch.__version__) <= LooseVersion("1.9.0") else ValueError
+        error_type = AssertionError if LooseVersion(torch.__version__) <= LooseVersion("1.9.1") else ValueError
         with pytest.raises(error_type, match=r"SyncBatchNorm layers only work with GPU modules"):
             model = nn.Sequential(nn.Linear(20, 100), nn.BatchNorm1d(100))
             auto_model(model, sync_bn=True)
