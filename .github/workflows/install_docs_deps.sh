55f417b1930cd631712386ad30662e97f98e8767
Taras Savchyn <trsvchn@gmail.com>
2021-10-12 20:19:04 +0300

    Revert "Refactor sphinx autosummary patching"
    This reverts commit 02f04553d81e2189322237f9038b81e4f6ddad8a.

--- .github/workflows/install_docs_deps.sh
+++ .github/workflows/install_docs_deps.sh
@@ -4,15 +4,3 @@ pip install torch torchvision -f https://download.pytorch.org/whl/cpu/torch_stab
 pip install -r requirements-dev.txt
 pip install -r docs/requirements.txt
 pip install git+https://github.com/trsvchn/sphinxcontrib-versioning.git@update-sphinx
-# patch autosummary
-echo "Patching autosummary..."
-sphinx_location=$pythonLocation/lib/python`python -c "import sys; print(sys.version_info.major, sys.version_info.minor, sep='.')"`/site-packages
-cp docs/autosummary.patch $sphinx_location
-cd $sphinx_location
-echo "Applying patch..."
-# Some checks
-git apply --check autosummary.patch
-git apply --summary autosummary.patch
-# Now apply
-git apply autosummary.patch
-echo "Done!"
