0459492864efc984a8a6b4f57c029fc71341d824
Desroziers <sylvain.desroziers@michelin.com>
2021-11-13 11:06:27 +0100

    add doctest for regression metrics

--- ignite/contrib/metrics/regression/r2_score.py
+++ ignite/contrib/metrics/regression/r2_score.py
@@ -32,6 +32,24 @@ class R2Score(_BaseRegression):
             metric's device to be the same as your ``update`` arguments ensures the ``update`` method is
             non-blocking. By default, CPU.
 
+    Examples:
+        To use with ``Engine`` and ``process_function``, simply attach the metric instance to the engine.
+        The output of the engine's ``process_function`` needs to be in format of
+        ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y, ...}``.
+
+        .. testcode::
+
+            metric = R2Score()
+            metric.attach(default_evaluator, 'r2')
+            y_true = torch.Tensor([0, 1, 2, 3, 4, 5])
+            y_pred = y_true * 0.75
+            state = default_evaluator.run([[y_pred, y_true]])
+            print(state.metrics['r2'])
+
+        .. testoutput::
+
+            0.8035...
+
     .. versionchanged:: 0.4.3
         Works with DDP.
     """
