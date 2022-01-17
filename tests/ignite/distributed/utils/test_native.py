c0ae8fddce329df6b7a7807ad472ea375399f1c7
FrAnCOisCokELaER <francois.cokelaer@gmail.com>
2022-01-17 16:51:19 +0100

    rm test params

--- tests/ignite/distributed/utils/test_native.py
+++ tests/ignite/distributed/utils/test_native.py
@@ -285,8 +285,6 @@ def test_idist_barrier_nccl(distributed_context_single_node_nccl, args, kwargs):
         ([], {}),
         ([GroupMember.WORLD, False], {}),
         ([GroupMember.WORLD, True], {}),
-        ([GroupMember.NON_GROUP_MEMBER, True], {}),
-        ([GroupMember.NON_GROUP_MEMBER, False], {}),
     ],
 )
 def test_idist_barrier_gloo(distributed_context_single_node_gloo, args, kwargs):
