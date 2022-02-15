b3eafb2aaf7129d2d07bf3450b996dc5441a998e
yuta0821 <yuta0821baske@outlook.jp>
2022-02-15 09:08:45 +0900

    revert to the original code using the os

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import io
 import os
 import re
-from pathlib import Path
 
 from setuptools import find_packages, setup
 
 
 def read(*names, **kwargs):
-    with io.open(Path(os.path.dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fp:
+    with io.open(os.path.join(os.path.dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fp:
         return fp.read()
 
 
