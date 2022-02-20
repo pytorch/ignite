aba9b256fc781fc93dc9fdd49afdd22bc1d55605
yuta0821 <yuta0821baske@outlook.jp>
2022-02-20 13:31:13 +0900

    fix mkdir in neural_style.py

--- examples/fast_neural_style/neural_style.py
+++ examples/fast_neural_style/neural_style.py
@@ -21,7 +21,7 @@ from ignite.handlers import ModelCheckpoint
 def check_paths(args):
     try:
         if args.checkpoint_model_dir is not None and not (Path(args.checkpoint_model_dir).exists()):
-            Path.mkdir(args.checkpoint_model_dir, parents=True)
+            Path(args.checkpoint_model_dir).mkdir(parents=True)
     except OSError as e:
         raise OSError(e)
 
