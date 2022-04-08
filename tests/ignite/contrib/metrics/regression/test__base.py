27b7ee44be341989ee75608a7502d80e3d7349d5
nmcguire101 <nmcguire101@gmail.com>
2022-04-08 15:14:21 -0400

    Added: Improved test tests\ignite\contrib\metrics\regression\test__base.py using pytest parametrize

--- tests/ignite/contrib/metrics/regression/test__base.py
+++ tests/ignite/contrib/metrics/regression/test__base.py
@@ -4,7 +4,8 @@ import torch
 from ignite.contrib.metrics.regression._base import _BaseRegression
 
 
-def test_base_regression_shapes():
+@pytest.mark.parametrize("OutputShapes", [("NDimensionShape", [1, 1, 1]), ("NDimensionFloat", [1, 1])])
+def test_base_regression_shapes(OutputShapes):
     class L1(_BaseRegression):
         def reset(self):
             self._sum_of_errors = 0.0
@@ -19,21 +20,25 @@ def test_base_regression_shapes():
 
     m = L1()
 
-    with pytest.raises(ValueError, match=r"Input y_pred should have shape \(N,\) or \(N, 1\)"):
-        y = torch.rand([1, 1, 1])
-        m.update((y, y))
-
-    with pytest.raises(ValueError, match=r"Input y should have shape \(N,\) or \(N, 1\)"):
-        y = torch.rand([1, 1, 1])
-        m.update((torch.rand(1, 1), y))
+    (input_type, input_values) = OutputShapes
 
     with pytest.raises(ValueError, match=r"Input data shapes should be the same, but given"):
         m.update((torch.rand(2), torch.rand(2, 1)))
 
-    with pytest.raises(TypeError, match=r"Input y_pred dtype should be float"):
-        y = torch.tensor([1, 1])
-        m.update((y, y))
+    if input_type == "NDimensionShape":
+        with pytest.raises(ValueError, match=r"Input y_pred should have shape \(N,\) or \(N, 1\)"):
+            y = torch.rand(input_values)
+            m.update((y, y))
 
-    with pytest.raises(TypeError, match=r"Input y dtype should be float"):
-        y = torch.tensor([1, 1])
-        m.update((y.float(), y))
+        with pytest.raises(ValueError, match=r"Input y should have shape \(N,\) or \(N, 1\)"):
+            y = torch.rand(input_values)
+            m.update((torch.rand(1, 1), y))
+
+    elif input_type == "NDimensionFloat":
+        with pytest.raises(TypeError, match=r"Input y_pred dtype should be float"):
+            y = torch.tensor(input_values)
+            m.update((y, y))
+
+        with pytest.raises(TypeError, match=r"Input y dtype should be float"):
+            y = torch.tensor(input_values)
+            m.update((y.float(), y))
