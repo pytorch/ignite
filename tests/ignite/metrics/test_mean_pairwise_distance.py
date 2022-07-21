81705c42e811f62a54b78bb6d180ba7d155f8f8c
OJASV Kamal <43926001+kamalojasv181@users.noreply.github.com>
2022-07-21 13:32:17 +0530

    Parametrized tests for test_mean_pairwise_distance.py (#2628)

--- tests/ignite/metrics/test_mean_pairwise_distance.py
+++ tests/ignite/metrics/test_mean_pairwise_distance.py
@@ -17,42 +17,38 @@ def test_zero_sample():
         mpd.compute()
 
 
-def test_compute():
+@pytest.fixture(params=[item for item in range(4)])
+def test_case(request):
+
+    return [
+        (torch.randint(0, 10, size=(100, 1)), torch.randint(0, 10, size=(100, 1)), 1),
+        (torch.randint(-20, 20, size=(100, 5)), torch.randint(-20, 20, size=(100, 5)), 1),
+        # updated batches
+        (torch.randint(0, 10, size=(100, 1)), torch.randint(0, 10, size=(100, 1)), 16),
+        (torch.randint(-20, 20, size=(100, 5)), torch.randint(-20, 20, size=(100, 5)), 16),
+    ][request.param]
+
+
+@pytest.mark.parametrize("n_times", range(5))
+def test_compute(n_times, test_case):
 
     mpd = MeanPairwiseDistance()
 
-    def _test(y_pred, y, batch_size):
-        mpd.reset()
-        if batch_size > 1:
-            n_iters = y.shape[0] // batch_size + 1
-            for i in range(n_iters):
-                idx = i * batch_size
-                mpd.update((y_pred[idx : idx + batch_size], y[idx : idx + batch_size]))
-        else:
-            mpd.update((y_pred, y))
+    y_pred, y, batch_size = test_case
 
-        np_res = np.mean(torch.pairwise_distance(y_pred, y, p=mpd._p, eps=mpd._eps).numpy())
+    mpd.reset()
+    if batch_size > 1:
+        n_iters = y.shape[0] // batch_size + 1
+        for i in range(n_iters):
+            idx = i * batch_size
+            mpd.update((y_pred[idx : idx + batch_size], y[idx : idx + batch_size]))
+    else:
+        mpd.update((y_pred, y))
 
-        assert isinstance(mpd.compute(), float)
-        assert pytest.approx(mpd.compute()) == np_res
+    np_res = np.mean(torch.pairwise_distance(y_pred, y, p=mpd._p, eps=mpd._eps).numpy())
 
-    def get_test_cases():
-
-        test_cases = [
-            (torch.randint(0, 10, size=(100, 1)), torch.randint(0, 10, size=(100, 1)), 1),
-            (torch.randint(-20, 20, size=(100, 5)), torch.randint(-20, 20, size=(100, 5)), 1),
-            # updated batches
-            (torch.randint(0, 10, size=(100, 1)), torch.randint(0, 10, size=(100, 1)), 16),
-            (torch.randint(-20, 20, size=(100, 5)), torch.randint(-20, 20, size=(100, 5)), 16),
-        ]
-
-        return test_cases
-
-    for _ in range(5):
-        # check multiple random inputs as random exact occurencies are rare
-        test_cases = get_test_cases()
-        for y_pred, y, batch_size in test_cases:
-            _test(y_pred, y, batch_size)
+    assert isinstance(mpd.compute(), float)
+    assert pytest.approx(mpd.compute()) == np_res
 
 
 def _test_distrib_integration(device):
