4535af15069352dce7cc290d60b1f0da2ead48bf
Ojasv Kamal <kamalojasv2000@gmail.com>
2022-07-23 22:46:57 +0530

    added GPU support, corrected mypy errors, and minor fixes

--- ignite/metrics/ndcg.py
+++ ignite/metrics/ndcg.py
@@ -5,41 +5,49 @@ import torch
 from ignite.exceptions import NotComputableError
 from ignite.metrics.metric import Metric
 
-__all__ = ["NGCD"]
+__all__ = ["NDCG"]
 
 
-class NGCD(Metric):
+class NDCG(Metric):
     def __init__(
         self,
         output_transform: Callable = lambda x: x,
         device: Union[str, torch.device] = torch.device("cpu"),
-        k=None,
-        log_base=2,
+        k: Union[int, None] = None,
+        log_base: Union[int, float] = 2,
     ):
-        super(NGCD, self).__init__(output_transform=output_transform, device=device)
+        super(NDCG, self).__init__(output_transform=output_transform, device=device)
 
         self.log_base = log_base
         self.k = k
         self.num_examples = 0
-        self.ngcd = torch.tensor(0, device=self._device, dtype=torch.float32)
+        self.ngcd = torch.tensor(0, dtype=torch.float32, device=device)
 
-    def _dcg_sample_scores(self, y_true, y_score, k=None, log_base=2) -> torch.Tensor:
+    def _dcg_sample_scores(
+        self, y_true: torch.Tensor, y_score: torch.Tensor, k: Union[int, None] = None, log_base: Union[int, float] = 2
+    ) -> torch.Tensor:
 
-        discount = torch.div(torch.log(torch.arange(y_true.shape[1]) + 2), torch.log(torch.tensor(log_base))).pow(-1)
+        discount = (
+            torch.div(torch.log(torch.arange(y_true.shape[1]) + 2), torch.log(torch.tensor(log_base)))
+            .pow(-1)
+            .to(self._device)
+        )
 
         if k is not None:
             discount[k:] = 0
 
         ranking = torch.argsort(y_score, descending=True)
 
-        ranked = torch.zeros(y_score.shape, dtype=y_score.dtype)
+        ranked = torch.zeros(y_score.shape, dtype=y_score.dtype, device=self._device)
         ranked = ranked.scatter_(1, ranking, y_true)
 
         discounted_gains = torch.mm(ranked, discount.reshape(-1, 1))
 
         return discounted_gains
 
-    def _ndcg_sample_scores(self, y_true, y_score, k=None, log_base=2) -> torch.Tensor:
+    def _ndcg_sample_scores(
+        self, y_true: torch.Tensor, y_score: torch.Tensor, k: Union[int, None] = None, log_base: Union[int, float] = 2
+    ) -> torch.Tensor:
 
         gain = self._dcg_sample_scores(y_true, y_score, k, log_base=log_base)
 
@@ -60,13 +68,13 @@ class NGCD(Metric):
 
         y_pred, y = output[0], output[1]
 
-        gain = self._ndcg_sample_scores(y, y_pred, k=self.k)
+        gain = self._ndcg_sample_scores(y, y_pred, k=self.k, log_base=self.log_base)
 
         self.ngcd += torch.sum(gain)
 
         self.num_examples += y_pred.shape[0]
 
-    def compute(self) -> float:
+    def compute(self) -> torch.float32:  # type: ignore
         if self.num_examples == 0:
             raise NotComputableError("NGCD must have at least one example before it can be computed.")
 
