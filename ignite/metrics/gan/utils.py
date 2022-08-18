47a4fcd41c9b7c3663f3b00c63bbe5baaa6f122b
puhuk <wetr235@gmail.com>
2022-08-18 18:30:53 +0900

    Update to simplify version check

--- ignite/metrics/gan/utils.py
+++ ignite/metrics/gan/utils.py
@@ -25,9 +25,10 @@ class InceptionModel(torch.nn.Module):
         super(InceptionModel, self).__init__()
         self._device = device
         if Version(torch.__version__) <= Version("1.7.0"):
-            self.model = models.inception_v3(pretrained=True).to(self._device)
+            model_kwargs = {"pretrained": True}
         else:
-            self.model = models.inception_v3(weights=models.Inception_V3_Weights.DEFAULT).to(self._device)
+            model_kwargs = {"weights": models.Inception_V3_Weights.DEFAULT}
+        self.model = models.inception_v3(model_kwargs).to(self._device)
         if return_features:
             self.model.fc = torch.nn.Identity()
         else:
