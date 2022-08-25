2d6d32641181bf162721301b68abaaf546ce0166
puhuk <wetr235@gmail.com>
2022-08-25 20:48:57 +0900

    Update test_classification_report.py

--- tests/ignite/metrics/test_classification_report.py
+++ tests/ignite/metrics/test_classification_report.py
@@ -24,7 +24,7 @@ def _test_integration_multiclass(device, output_dict):
 
         def update(engine, i):
             return (
-                y_preds[i * batch_size : (i + 1) * batch_size],
+                y_preds[i * batch_size : (i + 1) * batch_size, :],
                 y_true[i * batch_size : (i + 1) * batch_size],
             )
 
