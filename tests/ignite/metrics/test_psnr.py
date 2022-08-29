b1b48b9ed2b24ad2d71d832ccadb3a4affed8970
puhuk <wetr235@gmail.com>
2022-08-29 23:55:59 +0900

    Reconstruct `test_psnr.py`
    Reconstruct `test_psnr.py`

--- tests/ignite/metrics/test_psnr.py
+++ tests/ignite/metrics/test_psnr.py
@@ -79,144 +79,344 @@ def test_psnr():
     _test_psnr(y_pred, y, data_range, device)
 
 
-def _test(
-    y_pred,
-    y,
-    data_range,
-    metric_device,
-    n_iters,
-    s,
-    offset,
-    rank,
-    atol,
-    output_transform=lambda x: x,
-    compute_y_channel=False,
-):
-    from ignite.engine import Engine
+def _test_distrib_input_float(device, atol=1e-8):
+    def _test(
+        y_pred,
+        y,
+        data_range,
+        metric_device,
+        n_iters,
+        batch_size,
+        atol,
+        output_transform=lambda x: x,
+        compute_y_channel=False,
+    ):
 
-    def update(engine, i):
-        return (
-            y_pred[i * s + offset * rank : (i + 1) * s + offset * rank],
-            y[i * s + offset * rank : (i + 1) * s + offset * rank],
-        )
+        from ignite.engine import Engine
 
-    engine = Engine(update)
-    PSNR(data_range=data_range, output_transform=output_transform, device=metric_device).attach(engine, "psnr")
+        def update(engine, i):
+            return (
+                y_pred[i * batch_size : (i + 1) * batch_size],
+                y[i * batch_size : (i + 1) * batch_size],
+            )
 
-    data = list(range(n_iters))
-    engine.run(data=data, max_epochs=1)
-    result = engine.state.metrics["psnr"]
-    assert result > 0.0
-    assert "psnr" in engine.state.metrics
+        engine = Engine(update)
+        psnr = PSNR(data_range=data_range, output_transform=output_transform, device=metric_device)
+        psnr.attach(engine, "psnr")
+        data = list(range(n_iters))
 
-    if compute_y_channel:
-        np_y_pred = y_pred[:, 0, ...].cpu().numpy()
-        np_y = y[:, 0, ...].cpu().numpy()
-    else:
-        np_y_pred = y_pred.cpu().numpy()
-        np_y = y.cpu().numpy()
+        engine.run(data=data, max_epochs=1)
 
-    np_psnr = 0
-    for np_y_pred_, np_y_ in zip(np_y_pred, np_y):
-        np_psnr += ski_psnr(np_y_, np_y_pred_, data_range=data_range)
+        y = idist.all_gather(y)
+        y_pred = idist.all_gather(y_pred)
 
-    assert np.allclose(result, np_psnr / np_y.shape[0], atol=atol)
+        result = engine.state.metrics["psnr"]
+        assert result > 0.0
+        assert "psnr" in engine.state.metrics
 
+        if compute_y_channel:
+            np_y_pred = y_pred[:, 0, ...].cpu().numpy()
+            np_y = y[:, 0, ...].cpu().numpy()
+        else:
+            np_y_pred = y_pred.cpu().numpy()
+            np_y = y.cpu().numpy()
 
-def _test_distrib_integration(device, atol=1e-8):
+        np_psnr = 0
+        for np_y_pred_, np_y_ in zip(np_y_pred, np_y):
+            np_psnr += ski_psnr(np_y_, np_y_pred_, data_range=1)
 
-    rank = idist.get_rank()
+        assert np.allclose(result, np_psnr / np_y.shape[0], atol=atol)
+
+    def get_test_cases():
+
+        y_pred = torch.rand(n_iters * batch_size, 2, 2, device=device)
+        y = y_pred * 0.65
+
+        return y_pred, y
+
     n_iters = 100
-    s = 10
-    offset = n_iters * s
+    batch_size = 10
 
-    # test for float
-    manual_seed(42)
-    y_pred = torch.rand(offset * idist.get_world_size(), 3, 28, 28, device=device)
-    y = y_pred * 0.65
-    data_range = (y.max() - y.min()).cpu().item()
-    _test(y_pred, y, data_range, "cpu", n_iters, s, offset, rank, atol=atol)
+    for i in range(3):
+        # check multiple random inputs as random exact occurencies are rare
+        rank = idist.get_rank()
+        torch.manual_seed(42 + rank + i)
+        y_pred, y = get_test_cases()
+        _test(
+            y_pred,
+            y,
+            1,
+            "cpu",
+            n_iters,
+            batch_size,
+            atol=atol,
+        )
+        if device.type != "xla":
+            _test(
+                y_pred,
+                y,
+                1,
+                idist.device(),
+                n_iters,
+                batch_size,
+                atol=atol,
+            )
 
-    # test for YCbCr
-    manual_seed(42)
-    y_pred = torch.randint(16, 236, (offset * idist.get_world_size(), 1, 12, 12), dtype=torch.uint8, device=device)
-    cbcr_pred = torch.randint(16, 241, (offset * idist.get_world_size(), 2, 12, 12), dtype=torch.uint8, device=device)
-    y = torch.randint(16, 236, (offset * idist.get_world_size(), 1, 12, 12), dtype=torch.uint8, device=device)
-    cbcr = torch.randint(16, 241, (offset * idist.get_world_size(), 2, 12, 12), dtype=torch.uint8, device=device)
 
-    y_pred, y = torch.cat((y_pred, cbcr_pred), dim=1), torch.cat((y, cbcr), dim=1)
-    data_range = (y[:, 0, ...].max() - y[:, 0, ...].min()).cpu().item()
-    _test(
-        y_pred=y_pred,
-        y=y,
-        data_range=data_range,
-        metric_device="cpu",
-        n_iters=n_iters,
-        s=s,
-        offset=offset,
-        rank=rank,
-        atol=atol,
+def _test_distrib_multilabel_input_YCbCr(device, atol=1e-8):
+    def _test(
+        y_pred,
+        y,
+        data_range,
+        metric_device,
+        n_iters,
+        batch_size,
+        atol,
         output_transform=lambda x: (x[0][:, 0, ...], x[1][:, 0, ...]),
         compute_y_channel=True,
-    )
+    ):
 
-    # test for uint8
-    manual_seed(42)
-    y_pred = torch.randint(0, 256, (offset * idist.get_world_size(), 3, 16, 16), device=device, dtype=torch.uint8)
-    y = (y_pred * 0.65).to(torch.uint8)
-    data_range = (y.max() - y.min()).cpu().item()
-    _test(y_pred, y, data_range, "cpu", n_iters, s, offset, rank, atol=atol)
+        from ignite.engine import Engine
 
-    # test with NHW shape
-    manual_seed(42)
-    y_pred = torch.rand(offset * idist.get_world_size(), 28, 28, device=device)
-    y = y_pred * 0.8
-    data_range = (y.max() - y.min()).cpu().item()
-    _test(y_pred, y, data_range, "cpu", n_iters, s, offset, rank, atol=atol)
+        def update(engine, i):
+            return (
+                y_pred[i * batch_size : (i + 1) * batch_size],
+                y[i * batch_size : (i + 1) * batch_size],
+            )
 
-    if torch.device(device).type != "xla":
-        manual_seed(42)
-        y_pred = torch.rand(offset * idist.get_world_size(), 3, 28, 28, device=device)
-        y = y_pred * 0.65
-        data_range = (y.max() - y.min()).cpu().item()
-        _test(y_pred, y, data_range, idist.device(), n_iters, s, offset, rank, atol=atol)
+        engine = Engine(update)
+        psnr = PSNR(data_range=data_range, output_transform=output_transform, device=metric_device)
+        psnr.attach(engine, "psnr")
+        data = list(range(n_iters))
 
-        # test for YCbCr
-        manual_seed(42)
-        y_pred = torch.randint(16, 236, (offset * idist.get_world_size(), 1, 12, 12), dtype=torch.uint8, device=device)
-        cbcr_pred = torch.randint(
-            16, 241, (offset * idist.get_world_size(), 2, 12, 12), dtype=torch.uint8, device=device
-        )
-        y = torch.randint(16, 236, (offset * idist.get_world_size(), 1, 12, 12), dtype=torch.uint8, device=device)
-        cbcr = torch.randint(16, 241, (offset * idist.get_world_size(), 2, 12, 12), dtype=torch.uint8, device=device)
+        engine.run(data=data, max_epochs=1)
+
+        y = idist.all_gather(y)
+        y_pred = idist.all_gather(y_pred)
+
+        result = engine.state.metrics["psnr"]
+        assert result > 0.0
+        assert "psnr" in engine.state.metrics
+
+        if compute_y_channel:
+            np_y_pred = y_pred[:, 0, ...].cpu().numpy()
+            np_y = y[:, 0, ...].cpu().numpy()
+        else:
+            np_y_pred = y_pred.cpu().numpy()
+            np_y = y.cpu().numpy()
+
+        np_psnr = 0
+        for np_y_pred_, np_y_ in zip(np_y_pred, np_y):
+            np_psnr += ski_psnr(np_y_, np_y_pred_, data_range=220)
+
+        assert np.allclose(result, np_psnr / np_y.shape[0], atol=atol)
+
+    def get_test_cases():
+
+        y_pred = torch.randint(16, 236, (n_iters * batch_size, 1, 12, 12), dtype=torch.uint8, device=device)
+        cbcr_pred = torch.randint(16, 241, (n_iters * batch_size, 2, 12, 12), dtype=torch.uint8, device=device)
+        y = torch.randint(16, 236, (n_iters * batch_size, 1, 12, 12), dtype=torch.uint8, device=device)
+        cbcr = torch.randint(16, 241, (n_iters * batch_size, 2, 12, 12), dtype=torch.uint8, device=device)
+
         y_pred, y = torch.cat((y_pred, cbcr_pred), dim=1), torch.cat((y, cbcr), dim=1)
-        data_range = (y[:, 0, ...].max() - y[:, 0, ...].min()).cpu().item()
+
+        return y_pred, y
+
+    n_iters = 100
+    batch_size = 10
+
+    for i in range(3):
+        # check multiple random inputs as random exact occurencies are rare
+        rank = idist.get_rank()
+        torch.manual_seed(42 + rank + i)
+        y_pred, y = get_test_cases()
         _test(
-            y_pred=y_pred,
-            y=y,
-            data_range=data_range,
-            metric_device=idist.device(),
-            n_iters=n_iters,
-            s=s,
-            offset=offset,
-            rank=rank,
-            atol=atol,
-            output_transform=lambda x: (x[0][:, 0, ...], x[1][:, 0, ...]),
-            compute_y_channel=True,
+            y_pred,
+            y,
+            220,
+            "cpu",
+            n_iters,
+            batch_size,
+            atol,
         )
+        if device.type != "xla":
+            _test(
+                y_pred,
+                y,
+                220,
+                idist.device(),
+                n_iters,
+                batch_size,
+                atol,
+            )
 
-        manual_seed(42)
-        y_pred = torch.randint(0, 256, (offset * idist.get_world_size(), 3, 16, 16), device=device, dtype=torch.uint8)
+
+def _test_distrib_multilabel_input_uint8(device, atol=1e-8):
+    def _test(
+        y_pred,
+        y,
+        data_range,
+        metric_device,
+        n_iters,
+        batch_size,
+        atol,
+        output_transform=lambda x: x,
+        compute_y_channel=False,
+    ):
+
+        from ignite.engine import Engine
+
+        def update(engine, i):
+            return (
+                y_pred[i * batch_size : (i + 1) * batch_size],
+                y[i * batch_size : (i + 1) * batch_size],
+            )
+
+        engine = Engine(update)
+        psnr = PSNR(data_range=data_range, output_transform=output_transform, device=metric_device)
+        psnr.attach(engine, "psnr")
+        data = list(range(n_iters))
+
+        engine.run(data=data, max_epochs=1)
+
+        y = idist.all_gather(y)
+        y_pred = idist.all_gather(y_pred)
+
+        result = engine.state.metrics["psnr"]
+        assert result > 0.0
+        assert "psnr" in engine.state.metrics
+
+        if compute_y_channel:
+            np_y_pred = y_pred[:, 0, ...].cpu().numpy()
+            np_y = y[:, 0, ...].cpu().numpy()
+        else:
+            np_y_pred = y_pred.cpu().numpy()
+            np_y = y.cpu().numpy()
+
+        np_psnr = 0
+        for np_y_pred_, np_y_ in zip(np_y_pred, np_y):
+            np_psnr += ski_psnr(np_y_, np_y_pred_, data_range=100)
+
+        assert np.allclose(result, np_psnr / np_y.shape[0], atol=atol)
+
+    def get_test_cases():
+
+        y_pred = torch.randint(0, 256, (n_iters * batch_size, 3, 16, 16), device=device, dtype=torch.uint8)
         y = (y_pred * 0.65).to(torch.uint8)
-        data_range = (y.max() - y.min()).cpu().item()
-        _test(y_pred, y, data_range, idist.device(), n_iters, s, offset, rank, atol=atol)
 
-        # test with NHW shape
-        manual_seed(42)
-        y_pred = torch.rand(offset * idist.get_world_size(), 28, 28, device=device)
+        return y_pred, y
+
+    n_iters = 100
+    batch_size = 10
+
+    for i in range(3):
+        # check multiple random inputs as random exact occurencies are rare
+        rank = idist.get_rank()
+        torch.manual_seed(42 + rank + i)
+        y_pred, y = get_test_cases()
+        _test(
+            y_pred,
+            y,
+            100,
+            "cpu",
+            n_iters,
+            batch_size,
+            atol,
+        )
+        if device.type != "xla":
+            _test(
+                y_pred,
+                y,
+                100,
+                idist.device(),
+                n_iters,
+                batch_size,
+                atol,
+            )
+
+
+def _test_distrib_multilabel_input_NHW(device, atol=1e-8):
+    def _test(
+        y_pred,
+        y,
+        data_range,
+        metric_device,
+        n_iters,
+        batch_size,
+        atol,
+        output_transform=lambda x: x,
+        compute_y_channel=False,
+    ):
+
+        from ignite.engine import Engine
+
+        def update(engine, i):
+            return (
+                y_pred[i * batch_size : (i + 1) * batch_size],
+                y[i * batch_size : (i + 1) * batch_size],
+            )
+
+        engine = Engine(update)
+        psnr = PSNR(data_range=data_range, output_transform=output_transform, device=metric_device)
+        psnr.attach(engine, "psnr")
+        data = list(range(n_iters))
+
+        engine.run(data=data, max_epochs=1)
+
+        y = idist.all_gather(y)
+        y_pred = idist.all_gather(y_pred)
+
+        result = engine.state.metrics["psnr"]
+        assert result > 0.0
+        assert "psnr" in engine.state.metrics
+
+        if compute_y_channel:
+            np_y_pred = y_pred[:, 0, ...].cpu().numpy()
+            np_y = y[:, 0, ...].cpu().numpy()
+        else:
+            np_y_pred = y_pred.cpu().numpy()
+            np_y = y.cpu().numpy()
+
+        np_psnr = 0
+        for np_y_pred_, np_y_ in zip(np_y_pred, np_y):
+            np_psnr += ski_psnr(np_y_, np_y_pred_, data_range=10)
+
+        assert np.allclose(result, np_psnr / np_y.shape[0], atol=atol)
+
+    def get_test_cases():
+
+        y_pred = torch.rand(n_iters * batch_size, 28, 28, device=device)
         y = y_pred * 0.8
-        data_range = (y.max() - y.min()).cpu().item()
-        _test(y_pred, y, data_range, idist.device(), n_iters, s, offset, rank, atol=atol)
+
+        return y_pred, y
+
+    n_iters = 100
+    batch_size = 10
+
+    for i in range(3):
+        # check multiple random inputs as random exact occurencies are rare
+        rank = idist.get_rank()
+        torch.manual_seed(42 + rank + i)
+        y_pred, y = get_test_cases()
+        _test(
+            y_pred,
+            y,
+            10,
+            "cpu",
+            n_iters,
+            batch_size,
+            atol,
+        )
+        if device.type != "xla":
+            _test(
+                y_pred,
+                y,
+                10,
+                idist.device(),
+                n_iters,
+                batch_size,
+                atol,
+            )
 
 
 def _test_distrib_accumulator_device(device):
@@ -242,7 +442,10 @@ def _test_distrib_accumulator_device(device):
 def test_distrib_gloo_cpu_or_gpu(distributed_context_single_node_gloo):
 
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
@@ -252,7 +455,10 @@ def test_distrib_gloo_cpu_or_gpu(distributed_context_single_node_gloo):
 def test_distrib_nccl_gpu(distributed_context_single_node_nccl):
 
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
@@ -262,7 +468,10 @@ def test_distrib_nccl_gpu(distributed_context_single_node_nccl):
 def test_multinode_distrib_gloo_cpu_or_gpu(distributed_context_multi_node_gloo):
 
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
@@ -272,7 +481,10 @@ def test_multinode_distrib_gloo_cpu_or_gpu(distributed_context_multi_node_gloo):
 def test_multinode_distrib_nccl_gpu(distributed_context_multi_node_nccl):
 
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
@@ -282,13 +494,19 @@ def test_multinode_distrib_nccl_gpu(distributed_context_multi_node_nccl):
 def test_distrib_single_device_xla():
 
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
 def _test_distrib_xla_nprocs(index):
     device = idist.device()
-    _test_distrib_integration(device)
+    _test_distrib_input_float(device)
+    _test_distrib_multilabel_input_YCbCr(device)
+    _test_distrib_multilabel_input_uint8(device)
+    _test_distrib_multilabel_input_NHW(device)
     _test_distrib_accumulator_device(device)
 
 
@@ -307,5 +525,8 @@ def test_distrib_hvd(gloo_hvd_executor):
     device = "cpu" if not torch.cuda.is_available() else "cuda"
     nproc = 4 if not torch.cuda.is_available() else torch.cuda.device_count()
 
-    gloo_hvd_executor(_test_distrib_integration, (device,), np=nproc, do_init=True)
+    gloo_hvd_executor(_test_distrib_input_float, (device,), np=nproc, do_init=True)
+    gloo_hvd_executor(_test_distrib_multilabel_input_YCbCr, (device,), np=nproc, do_init=True)
+    gloo_hvd_executor(_test_distrib_multilabel_input_uint8, (device,), np=nproc, do_init=True)
+    gloo_hvd_executor(_test_distrib_multilabel_input_NHW, (device,), np=nproc, do_init=True)
     gloo_hvd_executor(_test_distrib_accumulator_device, (device,), np=nproc, do_init=True)
