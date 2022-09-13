2a19a772adba674c420efec18a8540e347ac4ed9
puhuk <wetr235@gmail.com>
2022-09-13 13:32:13 +0900

    Update base.py

--- ignite/distributed/comp_models/base.py
+++ ignite/distributed/comp_models/base.py
@@ -279,9 +279,7 @@ class ComputationModel(metaclass=ABCMeta):
         return tensor
 
     @abstractmethod
-    def _do_all_reduce(
-        self, tensor: torch.Tensor, op: str = "SUM", group: Optional[Any] = None
-    ) -> torch.Tensor:
+    def _do_all_reduce(self, tensor: torch.Tensor, op: str = "SUM", group: Optional[Any] = None) -> torch.Tensor:
         pass
 
     @abstractmethod
