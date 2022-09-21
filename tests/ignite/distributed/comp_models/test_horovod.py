7653af903813dce1b256ada30320ad8a6e3f138b
puhuk <puhuk@users.noreply.github.com>
2022-09-21 15:30:44 +0000

    autopep8 fix

--- tests/ignite/distributed/comp_models/test_horovod.py
+++ tests/ignite/distributed/comp_models/test_horovod.py
@@ -159,13 +159,17 @@ def test__hvd_dist_model_create_dist_2(gloo_hvd_executor):
 @pytest.mark.distributed
 @pytest.mark.skipif(torch.cuda.device_count() < 1, reason="Skip if no GPU")
 def test__hvd_dist_model_create_dist_cuda_1(gloo_hvd_executor):
-    gloo_hvd_executor(_test__hvd_dist_model_create_from_backend_dist, ("horovod", "cuda"), num_proc=torch.cuda.device_count())
+    gloo_hvd_executor(
+        _test__hvd_dist_model_create_from_backend_dist, ("horovod", "cuda"), num_proc=torch.cuda.device_count()
+    )
 
 
 @pytest.mark.distributed
 @pytest.mark.skipif(torch.cuda.device_count() < 1, reason="Skip if no GPU")
 def test__hvd_dist_model_create_dist_cuda_2(gloo_hvd_executor):
-    gloo_hvd_executor(_test__hvd_dist_model_create_from_context_dist, ("horovod", "cuda"), num_proc=torch.cuda.device_count())
+    gloo_hvd_executor(
+        _test__hvd_dist_model_create_from_context_dist, ("horovod", "cuda"), num_proc=torch.cuda.device_count()
+    )
 
 
 def _test__hvd_dist_model_warning_index_less_localrank():
