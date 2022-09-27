40309cb106f997efb69e7835bf3aa69285c398d6
puhuk <wetr235@gmail.com>
2022-09-28 06:06:14 +0900

    Update horovod.py

--- ignite/distributed/comp_models/horovod.py
+++ ignite/distributed/comp_models/horovod.py
@@ -175,7 +175,6 @@ if has_hvd_support:
             if group is not None:
                 if not isinstance(group, hvd.ProcessSet):
                     raise ValueError("Argument group should be list of int or ProcessSet")
-                hvd.init(process_sets=[group])
                 return hvd.allreduce(tensor, op=op, process_set=group)
             return hvd.allreduce(tensor, op=op)
 
