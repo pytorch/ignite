a580a9c188202e19d2dcdf9b9abdc5d9650090a7
Somasree <somasree@pop-os.localdomain>
2022-10-17 17:52:34 +0530

    Dropper TrainsLoger and TrainsSaver also removed the backward compatibility

--- ignite/contrib/handlers/__init__.py
+++ ignite/contrib/handlers/__init__.py
@@ -4,7 +4,7 @@ from ignite.contrib.handlers.neptune_logger import NeptuneLogger
 from ignite.contrib.handlers.polyaxon_logger import PolyaxonLogger
 from ignite.contrib.handlers.tensorboard_logger import TensorboardLogger
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
-from ignite.contrib.handlers.trains_logger import TrainsLogger
+
 from ignite.contrib.handlers.visdom_logger import VisdomLogger
 from ignite.contrib.handlers.wandb_logger import WandBLogger
 from ignite.handlers import EpochOutputStore, global_step_from_engine  # ref  # ref
