7aeecb9a91f925ec9a050f972b3e0f8f0574ccdd
sallycaoyu <sallycaoyu@gmail.com>
2023-02-05 22:07:46 -0500

    fixed the default dict format

--- ignite/metrics/mean_pairwise_distance.py
+++ ignite/metrics/mean_pairwise_distance.py
@@ -22,7 +22,7 @@ class MeanPairwiseDistance(Metric):
             :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
             form expected by the metric. This can be useful if, for example, you have a multi-output model and
             you want to compute the metric with respect to one of the outputs.
-            By default, metrics require the output as ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y, ...}``.
+            By default, metrics require the output as ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y}``.
         device: specifies which device updates are accumulated on. Setting the
             metric's device to be the same as your ``update`` arguments ensures the ``update`` method is
             non-blocking. By default, CPU.
