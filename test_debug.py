7cea85959e4f67cb550fbbed37ebb5d12326dbdd
puhuk <puhuk@users.noreply.github.com>
2023-02-07 07:20:50 +0000

    autopep8 fix

--- test_debug.py
+++ test_debug.py
@@ -5,25 +5,24 @@ from torchvision.datasets import MNIST
 from torchvision.models import resnet18
 from torchvision.transforms import Compose, Normalize, ToTensor
 
-from ignite.engine import Engine, Events, create_supervised_trainer, create_supervised_evaluator
-from ignite.metrics import Accuracy, Loss
+from ignite.contrib.handlers import global_step_from_engine, TensorboardLogger
+
+from ignite.engine import create_supervised_evaluator, create_supervised_trainer, Engine, Events
 from ignite.handlers import ModelCheckpoint
-from ignite.contrib.handlers import TensorboardLogger, global_step_from_engine
+from ignite.metrics import Accuracy, Loss
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class Net(nn.Module):
 
+class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        
+
         # Changed the output layer to output 10 classes instead of 1000 classes
         self.model = resnet18(num_classes=10)
 
         # Changed the input layer to take grayscale images for MNIST instaed of RGB images
-        self.model.conv1 = nn.Conv2d(
-            1, 64, kernel_size=3, padding=1, bias=False
-        )
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x):
         return self.model(x)
@@ -46,23 +45,18 @@ criterion = nn.CrossEntropyLoss()
 
 trainer = create_supervised_trainer(model, optimizer, criterion, device)
 
-val_metrics = {
-    "accuracy": Accuracy(),
-    "loss": Loss(criterion)
-}
+val_metrics = {"accuracy": Accuracy(), "loss": Loss(criterion)}
 
 train_evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=device)
 val_evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=device)
 
 trainer = create_supervised_trainer(model, optimizer, criterion, device)
 
-val_metrics = {
-    "accuracy": Accuracy(),
-    "loss": Loss(criterion)
-}
+val_metrics = {"accuracy": Accuracy(), "loss": Loss(criterion)}
 
 train_evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=device)
 val_evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=device)
+
 
 def train_step(engine, batch):
     model.train()
@@ -74,7 +68,9 @@ def train_step(engine, batch):
     optimizer.step()
     return loss.item()
 
+
 trainer = Engine(train_step)
+
 
 def validation_step(engine, batch):
     model.eval()
@@ -82,6 +78,7 @@ def validation_step(engine, batch):
         x, y = batch[0].to(device), batch[1].to(device)
         y_pred = model(x)
         return y_pred, y
+
 
 train_evaluator = Engine(validation_step)
 val_evaluator = Engine(validation_step)
@@ -93,6 +90,7 @@ for name, metric in val_metrics.items():
 for name, metric in val_metrics.items():
     metric.attach(val_evaluator, name)
 
+
 def train_step(engine, batch):
     model.train()
     optimizer.zero_grad()
@@ -103,7 +101,9 @@ def train_step(engine, batch):
     optimizer.step()
     return loss.item()
 
+
 trainer = Engine(train_step)
+
 
 def validation_step(engine, batch):
     model.eval()
@@ -111,6 +111,7 @@ def validation_step(engine, batch):
         x, y = batch[0].to(device), batch[1].to(device)
         y_pred = model(x)
         return y_pred, y
+
 
 train_evaluator = Engine(validation_step)
 val_evaluator = Engine(validation_step)
@@ -125,13 +126,16 @@ for name, metric in val_metrics.items():
 # How many batches to wait before logging training status
 log_interval = 100
 
+
 @trainer.on(Events.ITERATION_COMPLETED(every=log_interval))
 def trigger_custom_event():
     trainer.debug(level=Engine.DEBUG_OUTPUT)
 
+
 # Score function to return current value of any metric we defined above in val_metrics
 def score_function(engine):
     return engine.state.metrics["accuracy"]
+
 
 # Checkpoint to store n_saved best models wrt score function
 model_checkpoint = ModelCheckpoint(
@@ -140,10 +144,10 @@ model_checkpoint = ModelCheckpoint(
     filename_prefix="best",
     score_function=score_function,
     score_name="accuracy",
-    global_step_transform=global_step_from_engine(trainer), # helps fetch the trainer's state,
-    require_empty=False
+    global_step_transform=global_step_from_engine(trainer),  # helps fetch the trainer's state,
+    require_empty=False,
 )
-  
+
 # Save the model after every epoch of val_evaluator is completed
 val_evaluator.add_event_handler(Events.COMPLETED, model_checkpoint, {"model": model})
 
@@ -169,34 +173,3 @@ for tag, evaluator in [("training", train_evaluator), ("validation", val_evaluat
     )
 
 trainer.run(train_loader, max_epochs=2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
