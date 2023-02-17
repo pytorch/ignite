af380f598824ee5ffd6bd4f6fd276ca4d0b70d6f
Sadra Barikbin <sadraqazvin1@yahoo.com>
2023-02-18 00:38:39 +0330

    Revert a change in test_accuracy exactly

--- tests/ignite/metrics/test_accuracy.py
+++ tests/ignite/metrics/test_accuracy.py
@@ -317,9 +317,9 @@ def _test_distrib_multilabel_input_NHW(device):
         assert accuracy_score(np_y, np_y_pred) == pytest.approx(res)
         # check that result is not changed
         res = acc.compute()
-        assert accuracy_score(np_y, np_y_pred) == pytest.approx(res)
         assert n == acc._num_examples
         assert isinstance(res, float)
+        assert accuracy_score(np_y, np_y_pred) == pytest.approx(res)
 
         # Batched Updates
         acc.reset()
