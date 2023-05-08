45456b576c294983810d833e129538f84e06c956
AlexanderChaptykov <123diz321@gmail.com>
2023-05-08 18:05:50 +0300

    add step

--- ignite/handlers/param_scheduler.py
+++ ignite/handlers/param_scheduler.py
@@ -875,6 +875,7 @@ class LRScheduler(ParamScheduler):
     def __call__(self, engine: Optional[Engine], name: Optional[str] = None) -> None:
         super(LRScheduler, self).__call__(engine, name)
         self.lr_scheduler.last_epoch += 1
+        self.lr_scheduler.step()
 
     def get_param(self) -> Union[float, List[float]]:
         """Method to get current optimizer's parameter value"""
