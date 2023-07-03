40b5a8b96ef30cd8b4fac82eba582dc858df07f4
Sadra Barikbin <sadraqazvin1@yahoo.com>
2023-07-03 15:09:22 +0330

    Update Pillow usages

--- examples/fast_neural_style/utils.py
+++ examples/fast_neural_style/utils.py
@@ -4,9 +4,9 @@ from PIL import Image
 def load_image(filename, size=None, scale=None):
     img = Image.open(filename)
     if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
+        img = img.resize((size, size), Image.LANCZOS)
     elif scale is not None:
-        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.LANCZOS)
     return img
 
 
