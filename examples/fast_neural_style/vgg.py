fb49febd8a1d7de862a315d0c8872cf21104566b
vfdev <vfdev.5@gmail.com>
2023-07-07 11:20:49 +0200

    Update vgg.py

--- examples/fast_neural_style/vgg.py
+++ examples/fast_neural_style/vgg.py
@@ -37,4 +37,3 @@ class Vgg16(torch.nn.Module):
         vgg_outputs = namedtuple("VggOutputs", ["relu1_2", "relu2_2", "relu3_3", "relu4_3"])
         out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
         return out
-
