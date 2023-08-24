85eebd572a2e1e44c489befa50ee647ef4efa333
Marc Bresson <marco.bresson@gmail.com>
2023-08-24 11:43:30 +0200

    fix: use a temp var that will be moved with y_pred
    The comparison with self._device was not possible because it
    can be created with `torch.device("cuda")` which is not equal
    to `torch.device("cuda:0")` which is the device of a tensor
    created with `torch.device("cuda")`. This change will have
    a bigger performance hit when self._kernel is not on the same
    device as y_pred as it will need to be moved onto y_pred's
    device every time update() is called.

--- ignite/metrics/ssim.py
+++ ignite/metrics/ssim.py
@@ -161,28 +161,29 @@ class SSIM(Metric):
         channel = y_pred.size(1)
 
         self._kernel = self._kernel.expand(channel, 1, -1, -1)
+        kernel = self._kernel
 
-        if y_pred.device != self._device:
+        if y_pred.device != self._kernel.device:
             warnings.warn(
                 "The metric's device should be the same than your update tensors. See SSIM() device argument.",
                 RuntimeWarning,
             )
 
-            if self._device == torch.device("cpu"):
-                self._kernel = self._kernel.to(device=y_pred.device)
+            if self._kernel.device == torch.device("cpu"):
+                kernel = kernel.to(device=y_pred.device)
 
-            if y_pred == torch.device("cpu"):
-                y_pred = y_pred.to(device=self._device)
-                y = y.to(device=self._device)
+            if y_pred.device == torch.device("cpu"):
+                y_pred = y_pred.to(device=self._kernel.device)
+                y = y.to(device=self._kernel.device)
 
         y_pred = F.pad(y_pred, [self.pad_w, self.pad_w, self.pad_h, self.pad_h], mode="reflect")
         y = F.pad(y, [self.pad_w, self.pad_w, self.pad_h, self.pad_h], mode="reflect")
 
-        if y_pred.dtype != self._kernel.dtype:
-            self._kernel = self._kernel.to(dtype=y_pred.dtype)
+        if y_pred.dtype != kernel.dtype:
+            kernel = kernel.to(dtype=y_pred.dtype)
 
         input_list = [y_pred, y, y_pred * y_pred, y * y, y_pred * y]
-        outputs = F.conv2d(torch.cat(input_list), self._kernel, groups=channel)
+        outputs = F.conv2d(torch.cat(input_list), kernel, groups=channel)
         batch_size = y_pred.size(0)
         output_list = [outputs[x * batch_size : (x + 1) * batch_size] for x in range(len(input_list))]
 
