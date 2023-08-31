d67303167f16de3a5c218f2cfb340b75f6b3a4b0
HoJinLee <2hjc12@gmail.com>
2023-08-31 23:49:40 +0900

    small changes in distributed tests

--- tests/ignite/metrics/test_ndcg.py
+++ tests/ignite/metrics/test_ndcg.py
@@ -31,18 +31,18 @@ def test_case(request):
 def test_output_cpu(test_case, k, exponential, ignore_ties, replacement):
 
     device = "cpu"
-    y_pred_distribution, y_true = test_case
+    y_pred_distribution, y = test_case
 
     y_pred = torch.multinomial(y_pred_distribution, 5, replacement=replacement)
 
     ndcg = NDCG(k=k, device=device, exponential=exponential, ignore_ties=ignore_ties)
-    ndcg.update([y_pred, y_true])
+    ndcg.update([y_pred, y])
     result_ignite = ndcg.compute()
 
     if exponential:
-        y_true = 2 ** y_true - 1
+        y = 2 ** y - 1
 
-    result_sklearn = ndcg_score(y_true.numpy(), y_pred.numpy(), k=k, ignore_ties=ignore_ties)
+    result_sklearn = ndcg_score(y.numpy(), y_pred.numpy(), k=k, ignore_ties=ignore_ties)
 
     np.testing.assert_allclose(np.array(result_ignite), result_sklearn, rtol=2e-6)
 
@@ -54,41 +54,41 @@ def test_output_cpu(test_case, k, exponential, ignore_ties, replacement):
 def test_output_cuda(test_case, k, exponential, ignore_ties, replacement):
 
     device = "cuda"
-    y_pred_distribution, y_true = test_case
+    y_pred_distribution, y = test_case
 
     y_pred = torch.multinomial(y_pred_distribution, 5, replacement=replacement)
 
     y_pred = y_pred.to(device)
-    y_true = y_true.to(device)
+    y = y.to(device)
 
     ndcg = NDCG(k=k, device=device, exponential=exponential, ignore_ties=ignore_ties)
-    ndcg.update([y_pred, y_true])
+    ndcg.update([y_pred, y])
     result_ignite = ndcg.compute()
 
     if exponential:
-        y_true = 2 ** y_true - 1
+        y = 2 ** y - 1
 
-    result_sklearn = ndcg_score(y_true.cpu().numpy(), y_pred.cpu().numpy(), k=k, ignore_ties=ignore_ties)
+    result_sklearn = ndcg_score(y.cpu().numpy(), y_pred.cpu().numpy(), k=k, ignore_ties=ignore_ties)
 
     np.testing.assert_allclose(np.array(result_ignite), result_sklearn, rtol=2e-6)
 
 
 def test_reset():
 
-    y_true = torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0]])
+    y = torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0]])
     y_pred = torch.tensor([[0.1, 0.2, 0.3, 0.4, 0.5]])
     ndcg = NDCG()
-    ndcg.update([y_pred, y_true])
+    ndcg.update([y_pred, y])
     ndcg.reset()
 
     with pytest.raises(NotComputableError, match=r"NGCD must have at least one example before it can be computed."):
         ndcg.compute()
 
 
-def _ndcg_sample_scores(y_true, y_score, k=None, ignore_ties=False):
+def _ndcg_sample_scores(y, y_score, k=None, ignore_ties=False):
 
-    gain = _dcg_sample_scores(y_true, y_score, k, ignore_ties=ignore_ties)
-    normalizing_gain = _dcg_sample_scores(y_true, y_true, k, ignore_ties=True)
+    gain = _dcg_sample_scores(y, y_score, k, ignore_ties=ignore_ties)
+    normalizing_gain = _dcg_sample_scores(y, y, k, ignore_ties=True)
     all_irrelevant = normalizing_gain == 0
     gain[all_irrelevant] = 0
     gain[~all_irrelevant] /= normalizing_gain[~all_irrelevant]
@@ -97,38 +97,38 @@ def _ndcg_sample_scores(y_true, y_score, k=None, ignore_ties=False):
 
 @pytest.mark.parametrize("log_base", [2, 3, 10])
 def test_log_base(log_base):
-    def ndcg_score_with_log_base(y_true, y_score, *, k=None, sample_weight=None, ignore_ties=False, log_base=2):
+    def ndcg_score_with_log_base(y, y_score, *, k=None, sample_weight=None, ignore_ties=False, log_base=2):
 
-        gain = _ndcg_sample_scores(y_true, y_score, k=k, ignore_ties=ignore_ties)
+        gain = _ndcg_sample_scores(y, y_score, k=k, ignore_ties=ignore_ties)
         return np.average(gain, weights=sample_weight)
 
-    y_true = torch.tensor([[3.7, 4.8, 3.9, 4.3, 4.9]])
+    y = torch.tensor([[3.7, 4.8, 3.9, 4.3, 4.9]])
     y_pred = torch.tensor([[2.9, 5.6, 3.8, 7.9, 6.2]])
 
     ndcg = NDCG(log_base=log_base)
-    ndcg.update([y_pred, y_true])
+    ndcg.update([y_pred, y])
 
     result_ignite = ndcg.compute()
-    result_sklearn = ndcg_score_with_log_base(y_true.numpy(), y_pred.numpy(), log_base=log_base)
+    result_sklearn = ndcg_score_with_log_base(y.numpy(), y_pred.numpy(), log_base=log_base)
 
     np.testing.assert_allclose(np.array(result_ignite), result_sklearn, rtol=2e-6)
 
 
 def test_update(test_case):
 
-    y_pred, y_true = test_case
+    y_pred, y = test_case
 
     y_pred = y_pred
-    y_true = y_true
+    y = y
 
     y1_pred = torch.multinomial(y_pred, 5, replacement=True)
-    y1_true = torch.multinomial(y_true, 5, replacement=True)
+    y1_true = torch.multinomial(y, 5, replacement=True)
 
     y2_pred = torch.multinomial(y_pred, 5, replacement=True)
-    y2_true = torch.multinomial(y_true, 5, replacement=True)
+    y2_true = torch.multinomial(y, 5, replacement=True)
 
     y_pred_combined = torch.cat((y1_pred, y2_pred))
-    y_true_combined = torch.cat((y1_true, y2_true))
+    y_combined = torch.cat((y1_true, y2_true))
 
     ndcg = NDCG()
 
@@ -137,130 +137,65 @@ def test_update(test_case):
 
     result_ignite = ndcg.compute()
 
-    result_sklearn = ndcg_score(y_true_combined.numpy(), y_pred_combined.numpy())
+    result_sklearn = ndcg_score(y_combined.numpy(), y_pred_combined.numpy())
 
     np.testing.assert_allclose(np.array(result_ignite), result_sklearn, rtol=2e-6)
 
-
-def _test_distrib_output(device):
+@pytest.mark.parametrize("metric_device", ["cpu", "process_device"])
+def test_distrib_integration(distributed, metric_device):
+    from ignite.engine import Engine
 
     rank = idist.get_rank()
-
-    def _test(n_epochs, metric_device):
-
-        metric_device = torch.device(metric_device)
-
-        n_iters = 5
-        batch_size = 8
-        n_items = 5
-
-        torch.manual_seed(12 + rank)
-
-        y_true = torch.rand((n_iters * batch_size, n_items)).to(device)
-        y_preds = torch.rand((n_iters * batch_size, n_items)).to(device)
-
-        def update(_, i):
-            return (
-                [v for v in y_preds[i * batch_size : (i + 1) * batch_size, ...]],
-                [v for v in y_true[i * batch_size : (i + 1) * batch_size]],
-            )
-
-        engine = Engine(update)
-
-        ndcg = NDCG(device=metric_device)
-        ndcg.attach(engine, "ndcg")
-
-        data = list(range(n_iters))
-        engine.run(data=data, max_epochs=n_epochs)
-
-        y_true = idist.all_gather(y_true)
-        y_preds = idist.all_gather(y_preds)
-
-        assert (
-            ndcg._device == metric_device
-        ), f"{type(ndcg._device)}:{ndcg._device} vs {type(metric_device)}:{metric_device}"
-
-        assert "ndcg" in engine.state.metrics
-        res = engine.state.metrics["ndcg"]
-        if isinstance(res, torch.Tensor):
-            res = res.cpu().numpy()
-
-        true_res = ndcg_score(y_true.cpu().numpy(), y_preds.cpu().numpy())
-        assert pytest.approx(res) == true_res
-
-    metric_devices = ["cpu"]
-    if device.type != "xla":
-        metric_devices.append(idist.device())
-    for metric_device in metric_devices:
-        for _ in range(2):
-            _test(n_epochs=1, metric_device=metric_device)
-            _test(n_epochs=2, metric_device=metric_device)
-
-
-@pytest.mark.multinode_distributed
-@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
-@pytest.mark.skipif("MULTINODE_DISTRIB" not in os.environ, reason="Skip if not multi-node distributed")
-def test_multinode_distrib_gloo_cpu_or_gpu(distributed_context_multi_node_gloo):
-
+    torch.manual_seed(12 + rank)
+    n_iters = 5
+    batch_size = 8
     device = idist.device()
-    _test_distrib_output(device)
+    if metric_device == "process_device":
+        metric_device = device if device.type != "xla" else "cpu"
+
+    #10 items
+    y = torch.rand((n_iters * batch_size, 10)).to(device)
+    y_preds = torch.rand((n_iters * batch_size, 10)).to(device)
+
+    def update(engine, i):
+        return (
+            y_preds[i * batch_size : (i + 1) * batch_size, ...],
+            y[i * batch_size : (i + 1) * batch_size, ...],
+        )
+
+    engine = Engine(update)
+    NDCG(device=metric_device).attach(engine, "ndcg")
+
+    data = list(range(n_iters))
+    engine.run(data=data, max_epochs=1)
+
+    y_preds = idist.all_gather(y_preds)
+    y = idist.all_gather(y)
+
+    assert "ndcg" in engine.state.metrics
+    res = engine.state.metrics["ndcg"]
+
+    true_res = ndcg_score(y.cpu().numpy(), y_preds.cpu().numpy())
+    
+    tol = 1e-3 if device.type == "xla" else 1e-4  # Isn't better to ask `distributed` about backend info?
+
+    assert pytest.approx(res, abs=tol) == true_res
 
 
-@pytest.mark.multinode_distributed
-@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
-@pytest.mark.skipif("GPU_MULTINODE_DISTRIB" not in os.environ, reason="Skip if not multi-node distributed")
-def test_multinode_distrib_nccl_gpu(distributed_context_multi_node_nccl):
-
+@pytest.mark.parametrize("metric_device", [torch.device("cpu"), "process_device"])
+def test_distrib_accumulator_device(distributed, metric_device):
     device = idist.device()
-    _test_distrib_output(device)
+    if metric_device == "process_device":
+        metric_device = torch.device(device if device.type != "xla" else "cpu")
 
+    ndcg = NDCG(device=metric_device)
 
-@pytest.mark.distributed
-@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
-@pytest.mark.skipif(torch.cuda.device_count() < 1, reason="Skip if no GPU")
-def test_distrib_nccl_gpu(distributed_context_single_node_nccl):
+   
+    assert ndcg._device == metric_device, f"{type(dev)}:{dev} vs {type(metric_device)}:{metric_device}"
 
-    device = idist.device()
-    _test_distrib_output(device)
+    y_pred = torch.rand((2, 10)).to(device)
+    y = torch.rand((2, 10)).to(device)
+    ndcg.update((y_pred, y))
 
-
-@pytest.mark.distributed
-@pytest.mark.skipif(not idist.has_native_dist_support, reason="Skip if no native dist support")
-def test_distrib_gloo_cpu_or_gpu(distributed_context_single_node_gloo):
-
-    device = idist.device()
-    _test_distrib_output(device)
-
-
-@pytest.mark.distributed
-@pytest.mark.skipif(not idist.has_hvd_support, reason="Skip if no Horovod dist support")
-@pytest.mark.skipif("WORLD_SIZE" in os.environ, reason="Skip if launched as multiproc")
-def test_distrib_hvd(gloo_hvd_executor):
-
-    device = torch.device("cpu" if not torch.cuda.is_available() else "cuda")
-    nproc = 4 if not torch.cuda.is_available() else torch.cuda.device_count()
-
-    gloo_hvd_executor(_test_distrib_output, (device,), np=nproc, do_init=True)
-
-
-@pytest.mark.tpu
-@pytest.mark.skipif("NUM_TPU_WORKERS" in os.environ, reason="Skip if NUM_TPU_WORKERS is in env vars")
-@pytest.mark.skipif(not idist.has_xla_support, reason="Skip if no PyTorch XLA package")
-def test_distrib_single_device_xla():
-
-    device = idist.device()
-    _test_distrib_output(device)
-
-
-def _test_distrib_xla_nprocs(index):
-
-    device = idist.device()
-    _test_distrib_output(device)
-
-
-@pytest.mark.tpu
-@pytest.mark.skipif("NUM_TPU_WORKERS" not in os.environ, reason="Skip if no NUM_TPU_WORKERS in env vars")
-@pytest.mark.skipif(not idist.has_xla_support, reason="Skip if no PyTorch XLA package")
-def test_distrib_xla_nprocs(xmp_executor):
-    n = int(os.environ["NUM_TPU_WORKERS"])
-    xmp_executor(_test_distrib_xla_nprocs, args=(), nprocs=n)
+    dev = ndcg.ndcg.device
+    assert dev == metric_device, f"{type(dev)}:{dev} vs {type(metric_device)}:{metric_device}"
