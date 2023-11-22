a5ee7ae79b66afe43552efef6539e56fb1c39359
vfdev <vfdev.5@gmail.com>
2023-11-22 03:16:16 +0100

    Add support for `MPS` Backend [without torch.amp.autocast ] + CI (#3041)
    * Add support for `MPS` Backend [without torch.amp.autocast ] (#2993)
    
    * Add support for "mps" device in ignite.distributed.base
    
    * Made changes in the supervised_trainer API to have mps devices, Added some tests
    
    * autopep8 fix
    
    * Added lint fixes
    
    * Setup ci for mps tests
    
    ---------
    
    Co-authored-by: guptaaryan16 <guptaaryan16@users.noreply.github.com>
    Co-authored-by: vfdev <vfdev.5@gmail.com>
    
    * Fixed mps-tests.yml
    
    * More fixes to mps-tests.yml
    
    * Set up working directory in mps yml
    
    * another try
    
    * another try
    
    * another try
    
    * added version check and skipped mps tests
    
    * code formatting
    
    * another code formatting fix
    
    ---------
    
    Co-authored-by: Aryan Gupta <97878444+guptaaryan16@users.noreply.github.com>
    Co-authored-by: guptaaryan16 <guptaaryan16@users.noreply.github.com>

--- tests/ignite/distributed/utils/test_serial.py
+++ tests/ignite/distributed/utils/test_serial.py
@@ -1,6 +1,8 @@
+import pytest
 import torch
 
 import ignite.distributed as idist
+from ignite.distributed.comp_models.base import _torch_version_le_112
 from tests.ignite.distributed.utils import (
     _sanity_check,
     _test_distrib__get_max_length,
@@ -13,6 +15,9 @@ from tests.ignite.distributed.utils import (
 )
 
 
+@pytest.mark.skipif(
+    _torch_version_le_112 and torch.backends.mps.is_available(), reason="Temporary skip if MPS is available"
+)
 def test_no_distrib(capsys):
     assert idist.backend() is None
     if torch.cuda.is_available():
