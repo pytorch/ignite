b7dd7ea8f22659e08095f53088f723644884427a
Kazuki Adachi <kazuki.adachi.xy@gmail.com>
2024-03-18 20:55:24 +0900

    fix typo

--- ignite/metrics/__init__.py
+++ ignite/metrics/__init__.py
@@ -34,7 +34,7 @@ __all__ = [
     "MeanPairwiseDistance",
     "MeanSquaredError",
     "ConfusionMatrix",
-    "CosineSimilarity"
+    "CosineSimilarity",
     "ClassificationReport",
     "TopKCategoricalAccuracy",
     "Average",
