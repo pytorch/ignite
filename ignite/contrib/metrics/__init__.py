0b1af730a1229f317efd80b9e2dede39b3ab7bd1
root <johnleenimh@gmail.com>
2024-03-27 13:41:33 +0000

    add tests and import fixes

--- ignite/contrib/metrics/__init__.py
+++ ignite/contrib/metrics/__init__.py
@@ -1,6 +1,7 @@
-import ignite.contrib.metrics.regression
-from ignite.contrib.metrics.average_precision import AveragePrecision
-from ignite.contrib.metrics.cohen_kappa import CohenKappa
-from ignite.contrib.metrics.gpu_info import GpuInfo
-from ignite.contrib.metrics.precision_recall_curve import PrecisionRecallCurve
-from ignite.contrib.metrics.roc_auc import ROC_AUC, RocCurve
+import ignite.metrics.regression
+from ignite.metrics import average_precision, cohen_kappa, gpu_info, precision_recall_curve, roc_auc
+from ignite.metrics.average_precision import AveragePrecision
+from ignite.metrics.cohen_kappa import CohenKappa
+from ignite.metrics.gpu_info import GpuInfo
+from ignite.metrics.precision_recall_curve import PrecisionRecallCurve
+from ignite.metrics.roc_auc import ROC_AUC, RocCurve
