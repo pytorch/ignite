0dfeabe40b23659ec8a13c5be06e1d5cb056a088
Kazuki Adachi <kazuki.adachi.xy@gmail.com>
2024-03-30 21:14:42 +0900

    Add PearsonCorrelation metric (#3212)
    * add PearsonCorrelation metric
    
    * match the notation of the docstring with the other metrics
    
    * move PearsonCorrelation metric from contrib.metrics.regression to metrics.regression
    
    * update test for PearsonCorrelation metric
    
    * update test
    
    * modify doc for PearsonCorrelation metric
    
    * fix import
    
    * resolve code formatting issue
    
    * remove loop from test
    
    * Update ignite/metrics/regression/pearson_correlation.py
    
    Co-authored-by: vfdev <vfdev.5@gmail.com>
    
    * Update pearson_correlation.py
    
    * update test for PearsonCorrelation
    
    * Update tests/ignite/metrics/regression/test_pearson_correlation.py
    
    * relax pytest.approx
    
    ---------
    
    Co-authored-by: vfdev <vfdev.5@gmail.com>

--- ignite/metrics/regression/__init__.py
+++ ignite/metrics/regression/__init__.py
@@ -11,5 +11,6 @@ from ignite.metrics.regression.mean_normalized_bias import MeanNormalizedBias
 from ignite.metrics.regression.median_absolute_error import MedianAbsoluteError
 from ignite.metrics.regression.median_absolute_percentage_error import MedianAbsolutePercentageError
 from ignite.metrics.regression.median_relative_absolute_error import MedianRelativeAbsoluteError
+from ignite.metrics.regression.pearson_correlation import PearsonCorrelation
 from ignite.metrics.regression.r2_score import R2Score
 from ignite.metrics.regression.wave_hedges_distance import WaveHedgesDistance
