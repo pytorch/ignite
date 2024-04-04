ba3e78f9ef3ddda6648fc70b051f36d3bbf91a1c
Kazuki Adachi <kazuki.adachi.xy@gmail.com>
2024-04-04 21:17:26 +0900

    update formula of docstring

--- ignite/metrics/mutual_information.py
+++ ignite/metrics/mutual_information.py
@@ -14,7 +14,6 @@ class MutualInformation(Metric):
     between input :math:`X` and prediction :math:`Y`.
 
     .. math::
-
        I(X;Y) &= H(Y) - H(Y|X)
        = H \left( \frac{1}{N}\sum_{i=1}^N \hat{\mathbf{p}}_i \right)
        - \frac{1}{N}\sum_{i=1}^N H(\hat{\mathbf{p}}_i), \\
