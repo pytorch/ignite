a5d3464231f37b86fe4e5ae272fe14bbcb33b27d
Kazuki Adachi <kazuki.adachi.xy@gmail.com>
2024-10-14 16:40:14 +0900

    simplify imports of metric functions (#3292)

--- ignite/metrics/cohen_kappa.py
+++ ignite/metrics/cohen_kappa.py
@@ -71,23 +71,17 @@ class CohenKappa(EpochMetric):
         # initalize weights
         self.weights = weights
 
-        self.cohen_kappa_compute = self.get_cohen_kappa_fn()
-
         super(CohenKappa, self).__init__(
-            self.cohen_kappa_compute,
+            self._cohen_kappa_score,
             output_transform=output_transform,
             check_compute_fn=check_compute_fn,
             device=device,
             skip_unrolling=skip_unrolling,
         )
 
-    def get_cohen_kappa_fn(self) -> Callable[[torch.Tensor, torch.Tensor], float]:
-        """Return a function computing Cohen Kappa from scikit-learn."""
+    def _cohen_kappa_score(self, y_targets: torch.Tensor, y_preds: torch.Tensor) -> float:
         from sklearn.metrics import cohen_kappa_score
 
-        def wrapper(y_targets: torch.Tensor, y_preds: torch.Tensor) -> float:
-            y_true = y_targets.cpu().numpy()
-            y_pred = y_preds.cpu().numpy()
-            return cohen_kappa_score(y_true, y_pred, weights=self.weights)
-
-        return wrapper
+        y_true = y_targets.cpu().numpy()
+        y_pred = y_preds.cpu().numpy()
+        return cohen_kappa_score(y_true, y_pred, weights=self.weights)
