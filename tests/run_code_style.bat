425c262bf2d15ee79c5f9eb97946abb9497a9c8b
vfdev <vfdev.5@gmail.com>
2024-10-17 20:14:55 +0000

    Updated code formatting tools' versions

--- tests/run_code_style.bat
+++ tests/run_code_style.bat
@@ -20,7 +20,7 @@ mypy --config-file mypy.ini
 goto end
 
 :install
-pip install --upgrade flake8 "black==24.3.0" "usort==1.0.8.post1" "ufmt==2.5.1" "mypy"
+pip install --upgrade flake8 "black==24.10.0" "usort==1.0.8.post1" "ufmt==2.7.3" "mypy"
 goto end
 
 :end
