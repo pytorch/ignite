8ed45296dc5539f9d0eba4897056e18affff5b93
HyeSungP <hshs050401@gmail.com>
2025-03-26 23:57:31 +0900

    Apply formatting to previous changes

--- tests/ignite/metrics/test_mean_absolute_error.py
+++ tests/ignite/metrics/test_mean_absolute_error.py
@@ -32,7 +32,7 @@ def test_case(request):
 def test_compute(n_times, test_case, available_device):
     mae = MeanAbsoluteError(device=available_device)
     assert mae._device == torch.device(available_device)
-    
+
     y_pred, y, batch_size = test_case
 
     mae.reset()
