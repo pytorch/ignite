9229e3b2d8e085f957dbf2d06eb97a2fb5716e7b
BanzaiTokyo <banzaitokyo@gmail.com>
2025-04-24 09:30:22 +0000

    add _double_dtype ad dtype when transfrring errors to device

--- ignite/metrics/regression/canberra_metric.py
+++ ignite/metrics/regression/canberra_metric.py
@@ -74,7 +74,7 @@ class CanberraMetric(_BaseRegression):
     def _update(self, output: Tuple[torch.Tensor, torch.Tensor]) -> None:
         y_pred, y = output[0].detach(), output[1].detach()
         errors = torch.abs(y - y_pred) / (torch.abs(y_pred) + torch.abs(y) + 1e-15)
-        self._sum_of_errors += torch.sum(errors).to(self._device)
+        self._sum_of_errors += torch.sum(errors).to(dtype=self._double_dtype, device=self._device)
 
     @sync_all_reduce("_sum_of_errors")
     def compute(self) -> float:
