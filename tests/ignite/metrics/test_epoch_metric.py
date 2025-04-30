38a54584b96b96851f43821a9b0c6760c0d0d1ef
BanzaiTokyo <banzaitokyo@gmail.com>
2025-04-30 13:13:16 +0000

    move tensors to cpu for comparison

--- tests/ignite/metrics/test_epoch_metric.py
+++ tests/ignite/metrics/test_epoch_metric.py
@@ -59,34 +59,33 @@ def test_epoch_metric(available_device):
     assert em._device == torch.device(available_device)
 
     em.reset()
-    output1 = (torch.rand(4, 3, device="cpu"), torch.randint(0, 2, size=(4, 3), dtype=torch.long, device="cpu"))
+    output1 = (torch.rand(4, 3), torch.randint(0, 2, size=(4, 3), dtype=torch.long))
     em.update(output1)
-
-    output2 = (torch.rand(4, 3, device="cpu"), torch.randint(0, 2, size=(4, 3), dtype=torch.long, device="cpu"))
+    output2 = (torch.rand(4, 3), torch.randint(0, 2, size=(4, 3), dtype=torch.long))
     em.update(output2)
 
     if available_device == "cpu":
         assert all([t.device.type == "cpu" for t in em._predictions + em._targets])
-    assert torch.allclose(em._predictions[0], output1[0])
-    assert torch.allclose(em._predictions[1], output2[0])
-    assert torch.allclose(em._targets[0], output1[1])
-    assert torch.allclose(em._targets[1], output2[1])
+    assert torch.equal(em._predictions[0].cpu(), output1[0].cpu())
+    assert torch.equal(em._predictions[1].cpu(), output2[0].cpu())
+    assert torch.equal(em._targets[0].cpu(), output1[1].cpu())
+    assert torch.equal(em._targets[1].cpu(), output2[1].cpu())
     assert em.compute() == 0.0
 
     # test when y and y_pred are (batch_size, 1) that are squeezed to (batch_size, )
     em.reset()
-    output1 = (torch.rand(4, 1, device="cpu"), torch.randint(0, 2, size=(4, 1), dtype=torch.long, device="cpu"))
+    output1 = (torch.rand(4, 1), torch.randint(0, 2, size=(4, 1), dtype=torch.long))
     em.update(output1)
 
-    output2 = (torch.rand(4, 1, device="cpu"), torch.randint(0, 2, size=(4, 1), dtype=torch.long, device="cpu"))
+    output2 = (torch.rand(4, 1), torch.randint(0, 2, size=(4, 1), dtype=torch.long))
     em.update(output2)
 
     if available_device == "cpu":
         assert all([t.device.type == "cpu" for t in em._predictions + em._targets])
-    assert torch.allclose(em._predictions[0], output1[0][:, 0])
-    assert torch.allclose(em._predictions[1], output2[0][:, 0])
-    assert torch.allclose(em._targets[0], output1[1][:, 0])
-    assert torch.allclose(em._targets[1], output2[1][:, 0])
+    assert torch.equal(em._predictions[0].cpu(), output1[0][:, 0].cpu())
+    assert torch.equal(em._predictions[1].cpu(), output2[0][:, 0].cpu())
+    assert torch.equal(em._targets[0].cpu(), output1[1][:, 0].cpu())
+    assert torch.equal(em._targets[1].cpu(), output2[1][:, 0].cpu())
     assert em.compute() == 0.0
 
 
