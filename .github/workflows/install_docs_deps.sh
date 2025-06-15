4231f27e2b5e8267ab2477deeee4765439e3b679
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-06-15 11:47:03 -0500

    Use uv for other workflows

--- .github/workflows/install_docs_deps.sh
+++ .github/workflows/install_docs_deps.sh
@@ -1,6 +1,6 @@
 # remove pkg-resources as it causes failure when installing https://github.com/pytorch-ignite/sphinxcontrib-versioning
-pip uninstall -y pkg-resources setuptools && pip install --upgrade setuptools pip wheel
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu -U
-pip install -r requirements-dev.txt
-pip install -r docs/requirements.txt
-pip install git+https://github.com/pytorch-ignite/sphinxcontrib-versioning.git
+uv pip uninstall pkg-resources setuptools && pip install --upgrade setuptools pip wheel
+uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu -U
+uv pip install -r requirements-dev.txt
+uv pip install -r docs/requirements.txt
+uv pip install git+https://github.com/pytorch-ignite/sphinxcontrib-versioning.git
