b1f4891c3edb73e45d9d16d0879194d62fbf6106
Jeevan Chevula <jeevanchevulaa@gmail.com>
2025-09-09 14:49:24 +0530

    Add tests for has_registered_events method and updated indent issue

--- ignite/engine/engine.py
+++ ignite/engine/engine.py
@@ -250,15 +250,15 @@ class Engine(Serializable):
         self.state._update_attrs()
         
     def has_registered_events(self, event: Any) -> bool:
-    """Check whether engine has a registered event.
+        """Check whether engine has a registered event.
     
-    Args:
-        event: Event to check for registration.
+        Args:
+            event: Event to check for registration.
         
-    Returns:
-        bool: True if the event is registered, False otherwise.
-    """
-    return event in self._allowed_events    
+        Returns:
+            bool: True if the event is registered, False otherwise.
+        """
+        return event in self._allowed_events    
 
     def _handler_wrapper(self, handler: Callable, event_name: Any, event_filter: Callable) -> Callable:
         # signature of the following wrapper will be inspected during registering to check if engine is necessary
