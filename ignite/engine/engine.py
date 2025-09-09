2573d81f2d83af53d09d4be41575d68354d3d0f4
Jeevan Chevula <jeevanchevulaa@gmail.com>
2025-09-09 10:45:32 +0530

    Add has_registered_events method to Engine class

--- ignite/engine/engine.py
+++ ignite/engine/engine.py
@@ -248,6 +248,17 @@ class Engine(Serializable):
                 State.event_to_attr[e] = event_to_attr[e]
         # we need to update state attributes associated with new custom events
         self.state._update_attrs()
+        
+    def has_registered_events(self, event: Any) -> bool:
+    """Check whether engine has a registered event.
+    
+    Args:
+        event: Event to check for registration.
+        
+    Returns:
+        bool: True if the event is registered, False otherwise.
+    """
+    return event in self._allowed_events    
 
     def _handler_wrapper(self, handler: Callable, event_name: Any, event_filter: Callable) -> Callable:
         # signature of the following wrapper will be inspected during registering to check if engine is necessary
