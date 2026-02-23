7151fdbc4a3c42c7e02ea71b7877d65461dd86f5
Atharva <52556680+atharva7-g@users.noreply.github.com>
2026-02-23 19:36:13 +0530

    fix config definition

--- examples/cifar10raytune/main.py
+++ examples/cifar10raytune/main.py
@@ -101,8 +101,6 @@ def tune_cifar(num_samples=10, num_epochs=10, gpus_per_trial=0, cpus_per_trial=2
     data_dir = os.path.abspath(data_dir)
 
     config = {
-        "l1": tune.choice([2**i for i in range(9)]),
-    config = {
         "l1": tune.choice([2**i for i in range(2,9)]),
         "l2": tune.choice([2**i for i in range(2,9)]),
         "lr": tune.loguniform(1e-4, 1e-1),
