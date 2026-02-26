12e8afb4f8aefdc95b0c6b83de110a86a5a4ea90
Manimaran <manimarantech@gmail.com>
2026-02-26 17:56:14 +0530

    Replace typing hints in metrics/gan/fid.py
    Fixes #3481

--- ignite/metrics/gan/fid.py
+++ ignite/metrics/gan/fid.py
@@ -1,5 +1,5 @@
 import warnings
-from typing import Callable, Optional, Sequence, Union
+from collections.abc import Callable, Sequence
 
 import torch
 from packaging.version import Version
@@ -168,10 +168,10 @@ class FID(_BaseInceptionMetric):
 
     def __init__(
         self,
-        num_features: Optional[int] = None,
-        feature_extractor: Optional[torch.nn.Module] = None,
+        num_features: int | None = None,
+        feature_extractor: torch.nn.Module | None = None,
         output_transform: Callable = lambda x: x,
-        device: Union[str, torch.device] = torch.device("cpu"),
+        device: str | torch.device = torch.device("cpu"),
     ) -> None:
         try:
             import numpy as np  # noqa: F401
