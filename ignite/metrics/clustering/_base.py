bff76a6e13314acc93284a1e549de7cfc3a4aa35
Manimaran <manimarantech@gmail.com>
2026-02-26 17:56:14 +0530

    Replace typing hints in metrics/clustering/_base.py
    Fixes #3481

--- ignite/metrics/clustering/_base.py
+++ ignite/metrics/clustering/_base.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+
 
 from torch import Tensor
 
@@ -9,7 +9,7 @@ from ignite.metrics.epoch_metric import EpochMetric
 class _ClusteringMetricBase(EpochMetric):
     required_output_keys = ("features", "labels")
 
-    def _check_shape(self, output: Tuple[Tensor, Tensor]) -> None:
+    def _check_shape(self, output: tuple[Tensor, Tensor]) -> None:
         features, labels = output
         if features.ndimension() != 2:
             raise ValueError("Features should be of shape (batch_size, n_targets).")
@@ -17,7 +17,7 @@ class _ClusteringMetricBase(EpochMetric):
         if labels.ndimension() != 1:
             raise ValueError("Labels should be of shape (batch_size, ).")
 
-    def _check_type(self, output: Tuple[Tensor, Tensor]) -> None:
+    def _check_type(self, output: tuple[Tensor, Tensor]) -> None:
         features, labels = output
         if len(self._predictions) < 1:
             return
