e5d2689864f71c9b2f540be752a46a2d21766d1e
Manimaran <manimarantech@gmail.com>
2026-02-26 21:34:45 +0530

    Replace Optional with | None

--- ignite/handlers/mlflow_logger.py
+++ ignite/handlers/mlflow_logger.py
@@ -265,9 +265,9 @@ class OutputHandler(BaseOutputHandler):
     def __init__(
         self,
         tag: str,
-        metric_names: Optional[str | list[str]] = None,
+        metric_names: str | list[str] | None = None,
         output_transform: Callable | None = None,
-        global_step_transform: Optional[Callable[[Engine, str | Events], int]] = None,
+        global_step_transform: Callable[[Engine, str | Events], int] | None = None,
         state_attributes: list[str] | None = None,
     ) -> None:
         super().__init__(tag, metric_names, output_transform, global_step_transform, state_attributes)
